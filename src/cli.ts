5d1fc06870d054081c85dc6f2e8f38d8c075eb2f
Victor Serain <victor.serain@epitech.eu>
2022-01-04 12:10:37 +0100

    feat: add cli interfaces

---
+++ src/cli.ts
@@ -0,0 +1,20 @@
+import {CliStaticInterface} from "./intefaces/Cli.interface";
+
+const contollers: CliStaticInterface[] = [
+];
+
+const args = process.argv.slice(2);
+
+const Controller = contollers.find(controller => controller.cmdName  === args[0]);
+
+if (!Controller) {
+    throw new Error(`Controller ${args[0]} not found`);
+}
+
+const instance = new Controller() as any;
+
+if (!instance[args[1]]) {
+    throw new Error(`Method ${args[1]} not found in controller ${args[0]}`);
+}
+
+instance[args[1]].call(instance, ...args.slice(2));
