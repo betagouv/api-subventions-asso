ed188a59f6c692e6330114421717b0968aa9005b
Victor Serain <victor.serain@epitech.eu>
2022-01-04 15:57:37 +0100

    feat: add osiris folder entity

---
+++ src/osiris/entities/OsirisFoldersEntity.ts
@@ -0,0 +1,23 @@
+import OsirisActionsNumberColumn from "../@types/OsirisActionsNumberColumn";
+import OsirisAmountsColumn from "../@types/OsirisAmountsColumn";
+import OsirisAssociationColumn from "../@types/OsirisAssociationColumn";
+import OsirisCommentsColumn from "../@types/OsirisCommentsColumn";
+import OsirisEvaluationColumn from "../@types/OsirisEvaluationColumn";
+import OsirisFolderColumn from "../@types/OsirisFolderColumn";
+import OsirisLegalRepresentativeColumn from "../@types/OsirisLegalRepresentativeColumn";
+import OsirisMailingAddressColumn from "../@types/OsirisMailingAddressColumn";
+import OsirisPaymentsColumn from "../@types/OsirisPaymentsColumn";
+
+export default class OsirisFolderEntity {
+    constructor(
+        public readonly folder: OsirisFolderColumn,
+        public readonly association: OsirisAssociationColumn,
+        public readonly mailingAddress: OsirisMailingAddressColumn,
+        public readonly legalRepresentative: OsirisLegalRepresentativeColumn,
+        public readonly actionsNumber: OsirisActionsNumberColumn,
+        public readonly amounts: OsirisAmountsColumn,
+        public readonly payments: OsirisPaymentsColumn,
+        public readonly evaluation: OsirisEvaluationColumn,
+        public readonly comments: OsirisCommentsColumn
+    ) {}
+}
