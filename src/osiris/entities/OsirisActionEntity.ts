77debac750daf0e5a6bdeb7009ce0fb6858a765b
Victor Serain <victor.serain@epitech.eu>
2022-01-05 16:55:40 +0100

    feat: add osiris action entity

---
+++ src/osiris/entities/OsirisActionEntity.ts
@@ -0,0 +1,33 @@
+import { ObjectId } from "mongodb";
+import OsirisActionFolderColumn from "../@types/action/OsirisActionFolderColumn";
+import OsirisActionBeneficiaryAssociationColumn from "../@types/action/OsirisActionBeneficiaryAssociationColumn";
+import OsirisActionSpecificationsColumn from "../@types/action/OsirisActionSpecificationsColumn";
+import OsirisActionAffiliatingFederationColumn from "../@types/action/OsirisActionAffiliatingFederationColumn";
+import OsirisActionBeneficiariesColumn from "../@types/action/OsirisActionBeneficiariesColumn";
+import OsirisActionTerritoriesColumn from "../@types/action/OsirisActionTerritoriesColumn";
+import OsirisActionResourcesColumn from "../@types/action/OsirisActionResourcesColumn";
+import OsirisActionAmountsColumn from "../@types/action/OsirisActionAmountsColumn";
+import OsirisActionCoFinanciersColumn from "../@types/action/OsirisActionCoFinanciersColumn";
+import OsirisActionOtherColumn from "../@types/action/OsirisActionOtherColumn";
+import OsirisActionCustomColumn from "../@types/action/OsirisActionCustomColumn";
+import OsirisActionResourcesEvaluationColumn from "../@types/action/OsirisActionResourcesEvaluationColumn";
+import OsirisActionEvaluationColumn from "../@types/action/OsirisActionEvaluationColumn";
+
+export default class OsirisActionEntity {
+    constructor(
+        public readonly folder: OsirisActionFolderColumn,
+        public readonly beneficiaryAssociation: OsirisActionBeneficiaryAssociationColumn,
+        public readonly specifications: OsirisActionSpecificationsColumn,
+        public readonly affiliatingFederation: OsirisActionAffiliatingFederationColumn,
+        public readonly beneficiaries: OsirisActionBeneficiariesColumn,
+        public readonly territories: OsirisActionTerritoriesColumn,
+        public readonly resources: OsirisActionResourcesColumn,
+        public readonly resourcesEvaluation: OsirisActionResourcesEvaluationColumn,
+        public readonly amount: OsirisActionAmountsColumn,
+        public readonly coFinanciers: OsirisActionCoFinanciersColumn,
+        public readonly other: OsirisActionOtherColumn,
+        public readonly evaluation: OsirisActionEvaluationColumn,
+        public readonly custom: OsirisActionCustomColumn,
+        public _id?: ObjectId
+    ) {}
+}
