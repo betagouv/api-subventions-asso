c5e12e80d05e75c8a2a8b7275fb53eda7057124d
Victor Serain <victor.serain@epitech.eu>
2022-01-06 10:54:20 +0100

    feat: add search endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { connectDB } from './shared/MongoConnection';
 
+import { router as searchRouter, path as searchPath } from "./modules/search/interfaces/http/search.controller";
+
 const appName = 'datasubvention';
 const port = process.env.PORT || 8080;
 
@@ -8,6 +10,8 @@ async function main() {
     await connectDB();
 
     const app = express();
+
+    app.use(searchPath, searchRouter);
 
     app.get('/', (req, res) => {
         res.send('Hello World');
