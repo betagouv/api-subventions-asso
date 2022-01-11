ae048b215e777a2b05067a95e78df464a74bcaa0
Victor Serain <victor.serain@epitech.eu>
2022-01-11 16:39:42 +0100

    feat: add search module

---
+++ src/modules/search/entities/RequestEntity.ts
@@ -0,0 +1,10 @@
+import ILegalInformations from "../@types/ILegalInformations";
+import IProviderInformations from "../@types/IProviderInformations";
+
+export default class RequestEntity {
+    public provider = "datasubvention";
+    public providerInformations: IProviderInformations = {};
+    public data: unknown;
+
+    constructor(public legalInformations: ILegalInformations) {}
+}
