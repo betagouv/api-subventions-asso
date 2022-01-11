984387614552227d5346bd694029c3d30d1eb847
Victor Serain <victor.serain@epitech.eu>
2022-01-11 09:24:11 +0100

    feat: add logoptions and legalcategoriesaccepted in shared

---
+++ src/shared/LegalCategoriesAccepted.ts
@@ -0,0 +1,15 @@
+export const LEGAL_CATEGORIES_ACCEPTED = [ // see file in drive Developpement/Documents utils/cj_juillet_2020.xls or https://www.insee.fr/fr/information/2028129
+    "5195", // Association coopérative inscrite (droit local Alsace Moselle)
+    "7321", // Association syndicale autorisée
+    "7322", // Association foncière urbaine 
+    "7323", // Association foncière de remembrement 
+    "9150", // Association syndicale libre 
+    "9210", // Association non déclarée 
+    "9220", // Association déclarée 
+    "9221", // Association déclarée d'insertion par l'économique
+    "9222", // Association intermédiaire 
+    "9224", // Association d'avocats à responsabilité professionnelle individuelle
+    "9230", // Association déclarée, reconnue d'utilité publique
+    "9260", // Association de droit local (Bas-Rhin, Haut-Rhin et Moselle)
+    "92",   // Association loi 1901 ou assimilé
+]
