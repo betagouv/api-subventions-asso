2cfe652ffc81144180fc814436bd85b0d3776408
Victor Serain <victor.serain@epitech.eu>
2022-01-20 09:19:26 +0100

    feat: update legal categories

--- src/shared/LegalCategoriesAccepted.ts
+++ src/shared/LegalCategoriesAccepted.ts
@@ -1,9 +1,12 @@
-export const LEGAL_CATEGORIES_ACCEPTED = [ // see file in drive Developpement/Documents utils/cj_juillet_2020.xls or https://www.insee.fr/fr/information/2028129
+export const LEGAL_CATEGORIES_ACCEPTED = [ // see files in drive Developpement/Documents utils/Codes Juridiques
     "9210", // Association non déclarée 
     "9220", // Association déclarée 
     "9221", // Association déclarée d'insertion par l'économique
     "9222", // Association intermédiaire 
+    "9223", // Groupement d'employeurs
     "9230", // Association déclarée, reconnue d'utilité publique
+    "9240", // Congrégation
     "9260", // Association de droit local (Bas-Rhin, Haut-Rhin et Moselle)
+    "9300", // Fondation
     "92",   // Association loi 1901 ou assimilé
 ]
