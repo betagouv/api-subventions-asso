4caa1e9b31bc56968031d4f036cfa1b697c5fb36
natoine <antoineseilles@gmail.com>
2022-02-10 10:48:16 +0100

    ortho correction in mail

--- src/modules/mail-notifier/mail-notifier.service.ts
+++ src/modules/mail-notifier/mail-notifier.service.ts
@@ -4,14 +4,14 @@ export class MailNotifierService {
     private provider = ProviderFactory.getProvider();
     
     public async sendTestMail(email: string) {
-        await this.provider.sendMail(email, "Envoie de mail test", 
+        await this.provider.sendMail(email, "Envoi de mail test", 
             `<h1>Data Subvention</h1>
-                <p>Ceci est un mail de test merci de ne pas en prendre compte.</p>
+                <p>Ceci est un mail de test, merci de ne pas en prendre compte.</p>
 
                 <p>Bonne journée, de la part de l'équipe DataSubvention !</p>`
             , `
             Data Subvention
-            Ceci est un mail de test merci de ne pas en prendre compte
+            Ceci est un mail de test, merci de ne pas en prendre compte.
 
             Bonne journée, de la part de l'équipe DataSubvention !
         `);
@@ -23,12 +23,12 @@ export class MailNotifierService {
             "Inscription à la plateforme DataSubvention",
             `<h1>Data Subvention</h1>
             <p>
-                Félicitation, vous venez d'être inscrit à la plateforme DataSubvention !
+                Félicitations, vous êtes inscrit à la plateforme DataSubvention !
             </p>
             <p>
-                Pour acceder à celle-ci vous devez activer votre compte et choisir un mot de passe. Veuilliez vous rendre sur l'outil Postman en cliquant <a href="https://datasubvention.postman.co/workspace/Team-DataSubvention~064238d6-0f48-47f3-8f03-2f4312dbd702/request/9452502-6e7f4900-4f0d-4e36-b29d-aede3be4ff83">ici</a>.
-                <br>Ajouter ceci dans le champ token: ${token}.
-                <br>Et saisiser votre mots de passe dans le champs password.
+                Pour acceder à celle-ci, vous devez activer votre compte et choisir un mot de passe. Veuillez vous rendre sur l'outil Postman en cliquant <a href="https://datasubvention.postman.co/workspace/Team-DataSubvention~064238d6-0f48-47f3-8f03-2f4312dbd702/request/9452502-6e7f4900-4f0d-4e36-b29d-aede3be4ff83">ici</a>.
+                <br>Ajoutez ceci dans le champ token: ${token}.
+                <br>Et saisissez votre mot de passe dans le champ password.
             </p>
 
             <p>Bonne journée et à bientôt sur notre plateforme !</p>
@@ -36,11 +36,11 @@ export class MailNotifierService {
             `,
             `Data Subvention
             
-            Félicitation, vous venez d'être inscrit à la plateforme DataSubvention !
+            Félicitation, vous êtes inscrit à la plateforme DataSubvention !
             
             
-            Pour acceder à celle-ci vous devez activer votre compte et choisir un mot de passe. Veuilliez vous rendre sur l'outil Postman en cliquant sur ce lien : https://datasubvention.postman.co/workspace/Team-DataSubvention~064238d6-0f48-47f3-8f03-2f4312dbd702/request/9452502-6e7f4900-4f0d-4e36-b29d-aede3be4ff83 .
-            Ajouter ceci dans le champ token: ${token}. Et saisiser votre mots de passe dans le champs password.
+            Pour acceder à celle-ci vous devez activer votre compte et choisir un mot de passe. Veuillez vous rendre sur l'outil Postman en cliquant sur ce lien : https://datasubvention.postman.co/workspace/Team-DataSubvention~064238d6-0f48-47f3-8f03-2f4312dbd702/request/9452502-6e7f4900-4f0d-4e36-b29d-aede3be4ff83 .
+            Ajoutez ceci dans le champ token: ${token}. Et saisissez votre mot de passe dans le champ password.
             
 
             Bonne journée et à bientôt sur notre plateforme !
@@ -51,26 +51,26 @@ export class MailNotifierService {
     public async sendForgetPassword(email: string, token: string) {
         await this.provider.sendMail(
             email,
-            "Mots de passe perdu -- DataSubvention",
+            "Mot de passe perdu -- DataSubvention",
             `<h1>Data Subvention</h1>
             <p>
-                Vous avez fait une demande de mots de passe perdu, sur la plateforme DataSubvention.
+                Vous avez fait une demande de mot de passe perdu sur la plateforme DataSubvention.
             </p>
             <p>
-                Pour changer votre mots de passe, veuilliez vous rendre sur l'outil Postman en cliquant <a href="https://datasubvention.postman.co/workspace/Team-DataSubvention~064238d6-0f48-47f3-8f03-2f4312dbd702/request/9452502-6e7f4900-4f0d-4e36-b29d-aede3be4ff83">ici</a>.
-                <br>Ajouter ceci dans le champ token: ${token}. <br>Et saisiser votre mots de passe dans le champs password.
+                Pour changer votre mot de passe, veuillez vous rendre sur l'outil Postman en cliquant <a href="https://datasubvention.postman.co/workspace/Team-DataSubvention~064238d6-0f48-47f3-8f03-2f4312dbd702/request/9452502-6e7f4900-4f0d-4e36-b29d-aede3be4ff83">ici</a>.
+                <br>Ajoutez ceci dans le champ token: ${token}. <br>Et saisissez votre mot de passe dans le champ password.
             </p>
             
-            <p>Bonne journée et à bientôt sur notre plateforme !</p>
+            <p>Bonne journée, et à bientôt sur notre plateforme !</p>
             <p>L'équipe DataSubvention</p>
             `,
             `Data Subvention
             
-            Vous avez fait une demande de mots de passe perdu, sur la plateforme DataSubvention.
+            Vous avez fait une demande de mot de passe perdu sur la plateforme DataSubvention.
             
             
-            Pour changer votre mots de passe, veuilliez vous rendre sur l'outil Postman en cliquant sur ce lien : https://datasubvention.postman.co/workspace/Team-DataSubvention~064238d6-0f48-47f3-8f03-2f4312dbd702/request/9452502-6e7f4900-4f0d-4e36-b29d-aede3be4ff83 .
-            Ajouter ceci dans le champ token: ${token}. Et saisiser votre mots de passe dans le champs password.
+            Pour changer votre mot de passe, veuillez vous rendre sur l'outil Postman en cliquant sur ce lien : https://datasubvention.postman.co/workspace/Team-DataSubvention~064238d6-0f48-47f3-8f03-2f4312dbd702/request/9452502-6e7f4900-4f0d-4e36-b29d-aede3be4ff83 .
+            Ajoutez ceci dans le champ token: ${token}. Et saisissez votre mot de passe dans le champ password.
             
 
             Bonne journée et à bientôt sur notre plateforme !
