43b5f57f776d352e7d2531ad9c16d2b4f3ae18cb
Victor Serain <victor.serain@epitech.eu>
2022-02-17 15:38:39 +0100

    feat: move parse file in parse helpers

--- src/shared/helpers/ParserHelper.ts
+++ src/shared/helpers/ParserHelper.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import xlsx from 'node-xlsx';
 
 import { ParserInfo } from "../../@types/ParserInfo";
 import ParserPath from "../../@types/ParserPath";
@@ -47,4 +48,37 @@ export function findFiles(file: string) {
     } else files.push(file);
 
     return files;
+}
+
+export function csvParse(content: Buffer) {
+    return content
+        .toString()
+        .split("\n") // Select line by line
+        .map(raw => raw.split(/[\t;,]/)
+            .flat()) // Parse column
+}
+
+export function xlsParse(content: Buffer) {
+    const xls = xlsx.parse(content, { raw: true, rawNumbers: true, cellNF:true, dateNF: "165"});
+
+    return xls.map(xlsPage => xlsPage.data.filter((row) => (row as unknown[]).length));
+}
+
+export function ExcelDateToJSDate(serial: number) {
+    const utc_days  = Math.floor(serial - 25569);
+    const utc_value = utc_days * 86400;                                        
+    const date_info = new Date(utc_value * 1000);
+
+    const fractional_day = serial - Math.floor(serial) + 0.0000001;
+
+    let total_seconds = Math.floor(86400 * fractional_day);
+
+    const seconds = total_seconds % 60;
+
+    total_seconds -= seconds;
+
+    const hours = Math.floor(total_seconds / (60 * 60));
+    const minutes = Math.floor(total_seconds / 60) % 60;
+
+    return new Date(Date.UTC(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds));
 }
