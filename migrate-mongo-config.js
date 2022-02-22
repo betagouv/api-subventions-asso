2a8344c1e43d44287525980f63be045cf41955dc
Victor Serain <victor.serain@epitech.eu>
2022-02-22 14:56:04 +0100

    chore: add mongodb migration

---
+++ migrate-mongo-config.js
@@ -0,0 +1,35 @@
+
+require('dotenv/config') // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const mongoConf = require("./build/src/configurations/mongo.conf")
+
+// In this file you can configure migrate-mongo
+const config = {
+    mongodb: {
+        url: mongoConf.MONGO_URL,
+        databaseName: mongoConf.MONGO_DBNAME,
+
+        options: {
+            useNewUrlParser: true, // removes a deprecation warning when connecting
+            useUnifiedTopology: true, // removes a deprecating warning when connecting
+            //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
+            //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
+        }
+    },
+
+    // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
+    migrationsDir: "migrations",
+
+    // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
+    changelogCollectionName: "migrations",
+
+    // The file extension to create migrations and search for in migration dir 
+    migrationFileExtension: ".js",
+
+    // Enable the algorithm to create a checksum of the file contents and use that in the comparison to determin
+    // if the file should be run.  Requires that scripts are coded to be run multiple times.
+    useFileHash: false
+};
+
+// Return the config as a promise
+module.exports = config;
