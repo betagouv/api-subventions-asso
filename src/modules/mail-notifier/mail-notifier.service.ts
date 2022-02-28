3f6313a7ffd2da37b087499c7e465f2957164786
natoine <antoineseilles@gmail.com>
2022-02-28 11:55:43 +0100

    fix(mail-notifier): suppressed tab no test is alright

--- src/modules/mail-notifier/mail-notifier.service.ts
+++ src/modules/mail-notifier/mail-notifier.service.ts
@@ -6,9 +6,9 @@ export class MailNotifierService {
     public async sendTestMail(email: string) {
         await this.provider.sendMail(email, "Envoi de mail test", 
             `<h1>Data Subvention</h1>
-                <p>Ceci est un mail de test, merci de ne pas en prendre compte.</p>
+            <p>Ceci est un mail de test, merci de ne pas en prendre compte.</p>
 
-                <p>Bonne journée, de la part de l'équipe DataSubvention !</p>`
+            <p>Bonne journée, de la part de l'équipe DataSubvention !</p>`
             , `
             Data Subvention
             Ceci est un mail de test, merci de ne pas en prendre compte.
