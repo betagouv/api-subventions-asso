45c199531d2b8956dc70e0a26813b10d26b7d119
Victor Serain <victor.serain@epitech.eu>
2022-03-08 10:12:13 +0100

    chore(front): copy static files after build

--- packages/front/src/shared/config.ts
+++ packages/front/src/shared/config.ts
@@ -1,4 +1,3 @@
-import 'dotenv/config' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 export const DATASUB_URL = process.env["DATASUB_URL"] || "http://localhost:8080";
 export const MONGO_URL = process.env["MONGO_URL"] || `mongodb://localhost:27017/api-subventions-asso`;
