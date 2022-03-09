cfe0e85c7bebcb74d87e64d91f9c06a882ed0fd2
Victor Serain <victor.serain@epitech.eu>
2022-03-09 16:05:05 +0100

    feat(front): add dowload controller and route for download association data

--- packages/front/src/modules/controllers.ts
+++ packages/front/src/modules/controllers.ts
@@ -1,9 +1,11 @@
 import AuthController from "./auth/AuthController";
+import DownloadController from "./download/DownloadController";
 import HomeController from "./home/HomeController";
 import SearchController from "./search/SearchController";
 
 export default [
     HomeController,
     AuthController,
-    SearchController
+    SearchController,
+    DownloadController,
 ]
