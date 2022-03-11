ce0147984cd27d5365885377ec88798b18a9d736
Victor Serain <victor.serain@epitech.eu>
2022-03-11 10:48:32 +0100

    fix(front): fix front controller no detected

--- packages/front/views/index.js
+++ packages/front/views/index.js
@@ -3,5 +3,5 @@ import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 import "@hotwired/turbo"
 
 window.Stimulus = Application.start();
-const context = require.context("./", true, /\.js$/)
+const context = require.context("./", true, /\.js$/);
 Stimulus.load(definitionsFromContext(context));
