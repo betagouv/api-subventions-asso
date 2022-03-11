01cac8ab3e66a87982c7ae1d1220fbbaa127e0a8
Victor Serain <victor.serain@epitech.eu>
2022-03-11 16:24:09 +0100

    feat(api): add cache systeme for data entreprise api

---
+++ packages/api/src/shared/Cache.ts
@@ -0,0 +1,50 @@
+interface CacheValue<T> {
+    validateDate: number,
+    value: T
+}
+
+export default class CacheData<T> {
+
+    private collection = new Map<string, (CacheValue<T>)[]>();
+
+    constructor(private timeToCacheMS: number) {}
+
+    private cleanCache() {
+        const now = new Date().getTime();
+        this.collection.forEach((values, key) => {
+            const newValues = values.filter( value => value.validateDate > now);
+
+            if (!newValues.length) this.collection.delete(key);
+            else this.collection.set(key, newValues);
+        });
+    }
+
+    public add(key: string, value: T) {
+        const values = [
+            {
+                validateDate: new Date().getTime() + this.timeToCacheMS,
+                value
+            }
+        ] as CacheValue<T>[]
+        if (this.collection.has(key)) values.push(...this.collection.get(key) as []);
+
+        this.collection.set(key, values);
+        this.cleanCache();
+    }
+
+    public get(key: string): T[] {
+        if (!this.collection.has(key)) return [];
+        this.cleanCache();
+
+        return (this.collection.get(key) as CacheValue<T>[])
+            .map(v => v.value);
+    }
+
+    public has(key: string): boolean {
+        return !!this.get(key).length;
+    }
+
+    public destroy() {
+        this.collection.clear();
+    }
+}
