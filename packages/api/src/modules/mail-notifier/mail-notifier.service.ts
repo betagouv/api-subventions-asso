ec6f180ac948fb7bfdfe1ae764363fbcff6fbdaa
AlixBouillon <102042851+AlixBouillon@users.noreply.github.com>
2022-03-29 16:15:06 +0200

    V3 - Mises à jour des 2 mails

--- packages/api/src/modules/mail-notifier/mail-notifier.service.ts
+++ packages/api/src/modules/mail-notifier/mail-notifier.service.ts
@@ -33,7 +33,7 @@ export class MailNotifierService {
             Vous aimeriez suivre une démo, pour que nous vous présentions l'outil pas à pas ? Vous pouvez nous écrire à <a href="mailto:contact@datasubvention.beta.gouv.fr">cette adresse</a>, nous nous ferons un plaisir de vous proposer un rendez-vous.
             </p>
             <p>
-                Vous souhaitez accéder directement à la plateforme ? Pour cela, il suffit d'activer votre compte et choisir un mot de passe. Veuillez vous rendre sur la page d'activation en cliquant <a href="${FRONT_OFFICE_URL}/auth/reset-password/${token}?active=true">ici</a>.
+                Vous souhaitez accéder directement à la plateforme ? Pour cela, il suffit d'activer votre compte et de choisir un mot de passe. Veuillez vous rendre sur la page d'activation en cliquant <a href="${FRONT_OFFICE_URL}/auth/reset-password/${token}?active=true">ici</a>.
             </p>
 
             <p>Bonne journée et à bientôt sur notre plateforme !</p>
@@ -43,9 +43,11 @@ export class MailNotifierService {
             
             Bienvenue sur la plateforme DataSubvention !
             
+            Vous êtes inscrit à un test solution ou une démo avec notre équipe dans les jours qui viennent ? Inutile d'aller plus loin, nous prendrons le temps d'activer votre compte ensemble, lors de cet échange.
             
-            Pour accéder à celle-ci vous devez activer votre compte et choisir un mot de passe. Veuillez vous rendre sur la page d'activation en cliquant sur ce lien : ${FRONT_OFFICE_URL}/auth/reset-password/${token}?active=true .
-
+            Vous aimeriez suivre une démo, pour que nous vous présentions l'outil pas à pas ? Vous pouvez nous écrire à l'adresse contact@datasubvention.beta.gouv.fr, nous nous ferons un plaisir de vous proposer un rendez-vous.
+           
+            Vous souhaitez accéder directement à la plateforme ? Pour cela, il suffit d'activer votre compte et de choisir un mot de passe.. Veuillez vous rendre sur la page d'activation en cliquant sur ce lien : ${FRONT_OFFICE_URL}/auth/reset-password/${token}?active=true .
 
             Bonne journée et à bientôt sur notre plateforme !
             L'équipe DataSubvention `
