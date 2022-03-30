b9d18cbfe9b13fec2315856404b777cb6a228d17
Victor Serain <victor.serain@epitech.eu>
2022-03-30 09:18:34 +0200

    feat(api): order etablisements in association

--- packages/api/src/modules/search/search.service.ts
+++ packages/api/src/modules/search/search.service.ts
@@ -1,10 +1,11 @@
 import etablissementService from "../etablissements/etablissements.service";
 import associationsService from "../associations/associations.service";
 
-import { Siret, Rna, Siren } from "../../@types";
+import { Siret, Rna, Siren } from "@api-subventions-asso/dto";
 import demandesSubventionsService from "../demandes_subventions/demandes_subventions.service";
 import rnaSirenService from "../rna-siren/rnaSiren.service";
 import versementsService from "../versements/versements.service";
+import Etablissement from "../etablissements/interfaces/Etablissement";
 
 export class SearchService {
 
@@ -49,11 +50,7 @@ export class SearchService {
         const association = await associationsService.getAssociationBySiren(siren, rna);
 
         if (!association) return null;
-        const etablissements = (await etablissementService.getEtablissementsBySiren(siren) || []).sort(e => {
-            if (!e.ouvert || !e.ouvert[0].value) return 2;
-            if (e.siege && e.siege[0].value) return -1;
-            return 1;
-        });
+        const etablissements = (await etablissementService.getEtablissementsBySiren(siren) || []).sort((etablisementA,etablisementB) => this.scoreEtablisement(etablisementB) - this.scoreEtablisement(etablisementA));
 
         const demandesSubventions = await demandesSubventionsService.getDemandeSubventionsBySiren(siren);
 
@@ -70,6 +67,14 @@ export class SearchService {
         }
 
         return await versementsService.aggregateVersementsByAssoSearch(associationDto);
+    }
+
+    private scoreEtablisement(etablisement: Etablissement) {
+        let score = 0;
+
+        if (etablisement.ouvert && etablisement.ouvert[0].value) score += 1;
+        if (etablisement.siege && etablisement.siege[0].value) score += 10;
+        return score
     }
 }
 
