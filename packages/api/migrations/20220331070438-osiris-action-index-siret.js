817f427f00131b8962cabdaddbdaee79afe7ad8d
Victor Serain <victor.serain@epitech.eu>
2022-03-31 09:12:20 +0200

    chore(api): indexed siret in osiris action

---
+++ packages/api/migrations/20220331070438-osiris-action-index-siret.js
@@ -0,0 +1,41 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { connectDB } = require('../build/src/shared/MongoConnection');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { printAtSameLine } = require("../build/src/shared/helpers/CliHelper");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const migrationManager = require("../build/src/shared/MigrationManager").default;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const repo = require("../build/src/modules/providers/osiris/repository/osiris.repository").default;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const enity = require("../build/src/modules/providers/osiris/entities/OsirisActionEntity").default;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const ParseHelper = require("../build/src/shared/helpers/ParserHelper");
+
+module.exports = {
+    async up(db, client) {
+        console.log("Start Osris action indexed siret migration");
+        await connectDB();
+        await migrationManager.startMigration();
+
+        const cursor = repo.cursorFindActions();
+
+        let counter = 0;
+        while(await cursor.hasNext()) {
+            const doc = await cursor.next();
+            if (!doc) continue;
+            const data = doc.data;
+            doc.indexedInformations = ParseHelper.indexDataByPathObject(enity.indexedInformationsPath, data);
+            await repo.updateAction(doc);
+            counter++;
+            printAtSameLine(counter.toString());
+        }
+
+        await migrationManager.endMigration();
+    },
+
+    async down(db, client) {
+        // TODO write the statements to rollback your migration (if possible)
+        // Example:
+        // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+    }
+};
