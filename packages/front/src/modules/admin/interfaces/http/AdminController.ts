59237ec46a9f08cc574db5bde3d3064f62cfd3b0
Victor Serain <victor.serain@epitech.eu>
2022-04-01 09:04:26 +0200

    refactor(front): clean admin part

--- packages/front/src/modules/admin/interfaces/http/AdminController.ts
+++ packages/front/src/modules/admin/interfaces/http/AdminController.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import User from '../../../../@types/User';
 import Controller from '../../../../decorators/controller.decorator';
 import { Get, Post } from '../../../../decorators/http.methods.decorator';
 import userService from '../../../user/user.service';
@@ -9,8 +10,11 @@ export default class AdminController {
 
     @Get("")
     public async loginView(req: Request, res: Response, next: NextFunction) {
+        if (!req.session.roles || !req.session.roles.includes("admin")) {
+            return res.redirect("/");
+        }
 
-        const result = await adminService.listUsers(req.session.user);
+        const result = await adminService.listUsers(req.session.user as User);
 
         res.render('admin/index', {
             pageTitle: 'Admin',
@@ -22,6 +26,10 @@ export default class AdminController {
 
     @Post("/user/create")
     public async createUser(req: Request, res: Response, next: NextFunction) {
+        if (!req.session.roles || !req.session.roles.includes("admin")) {
+            return res.redirect("/");
+        }
+
         if (!req.body.email) {
             return res.redirect("/admin?error=USER_EMAIL_NOT_FOUND")
         }
