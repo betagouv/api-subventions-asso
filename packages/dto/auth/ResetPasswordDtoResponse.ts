2c9442e8bc05cbeaede40f87d91406099637df7f
Victor Serain <victor.serain@epitech.eu>
2022-04-14 15:31:36 +0200

    feat(dto): add reset password response

---
+++ packages/dto/auth/ResetPasswordDtoResponse.ts
@@ -0,0 +1,26 @@
+import UserDto from "../user/UserDto"
+
+export enum ResetPasswordErrorCodes {
+    RESET_TOKEN_NOT_FOUND = 1,
+    RESET_TOKEN_EXPIRED = 2,
+    USER_NOT_FOUND = 3,
+    PASSWORD_FORMAT_INVALID = 4,
+    INTERNAL_ERROR = 5,
+}
+
+export interface ResetPasswordDtoPositiveResponse {
+    success: true,
+    data: {
+        user: UserDto
+    }
+}
+
+export interface ResetPasswordDtoNegativeResponse {
+    success: false,
+    data: {
+        message: string,
+        code: ResetPasswordErrorCodes
+    }
+}
+
+export type ResetPasswordDtoResponse = ResetPasswordDtoNegativeResponse | ResetPasswordDtoPositiveResponse;
