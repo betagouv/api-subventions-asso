bff4e77932e30304da4ba3df1e275c59e89074ff
Victor Serain <victor.serain@epitech.eu>
2022-04-14 09:31:08 +0200

    chore(api): add mail logger

--- packages/api/src/configurations/mail.conf.ts
+++ packages/api/src/configurations/mail.conf.ts
@@ -5,3 +5,5 @@ export const MAIL_PORT = process.env.MAIL_PORT || "";
 export const MAIL_USER = process.env.MAIL_USER || "";
 
 export const MAIL_PASSWORD = process.env.MAIL_PASSWORD || "";
+
+export const LOG_MAIL = "log@datasubvention.beta.gouv.fr";
