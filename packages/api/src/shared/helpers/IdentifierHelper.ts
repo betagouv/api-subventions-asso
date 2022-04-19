6f2ea64bcdf3aa0cd5c347e793c4f72d5a908ba4
Maxime Silvère Meunier <mxmeunier@gmail.com>
2022-04-19 09:33:18 +0200

    refactor(api): clean code from review

--- packages/api/src/shared/helpers/IdentifierHelper.ts
+++ packages/api/src/shared/helpers/IdentifierHelper.ts
@@ -5,7 +5,7 @@ import { isRna, isSiren, isSiret } from '../Validators';
 
 export function getIdentifierType(id: StructureIdentifiers): string | null {
     if (isRna(id)) return StructureIdentifiersEnum.rna;
-    if (isSiren(id)) {  return StructureIdentifiersEnum.siren }
+    if (isSiren(id)) return StructureIdentifiersEnum.siren
     if (isSiret(id)) return StructureIdentifiersEnum.siret
     else return null;
 }
