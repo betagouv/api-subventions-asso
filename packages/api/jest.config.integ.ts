6632e6cc5d5c8b7367239652e678aa6c5f5c9939
Maxime Silvère Meunier <mxmeunier@gmail.com>
2022-04-27 11:49:54 +0200

    refactor(api): make test:integ runs old unit tests

--- packages/api/jest.config.integ.ts
+++ packages/api/jest.config.integ.ts
@@ -157,7 +157,7 @@ export default Object.assign(baseConfig, {
     // ],
 
     // The regexp pattern or array of patterns that Jest uses to detect test files
-    testRegex: ["\\.spec\\.[jt]s$"],
+    testRegex: ["\\.(spec|test)\\.[jt]s$"],
 
     // This option allows the use of a custom results processor
     // testResultsProcessor: undefined,
