ee1c1cde9ebc06614a6b06348a856d6a17b6aa60
Maxime Silvère Meunier <mxmeunier@gmail.com>
2022-05-06 09:46:57 +0200

    feat(api): rename INotifier import

--- packages/api/src/modules/mail-notifier/providers/provider-factory.ts
+++ packages/api/src/modules/mail-notifier/providers/provider-factory.ts
@@ -1,8 +1,8 @@
-import IProvider from "./@types/INotifier";
+import INotifier from "./@types/INotifier";
 import NodeMailerProvider from "./nodemailer.provider";
 
 export default class ProviderFactory {
-    static getProvider(): IProvider {
+    static getProvider(): INotifier {
         return new NodeMailerProvider();
     }
 }
