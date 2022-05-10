a8a48f3f104fb191858fd1b5b856e9694f6e38b2
Maxime Meunier <mxmeunier@gmail.com>
2022-05-10 10:50:00 +0200

    chore(api): remove unused external module (#231)

--- packages/api/src/modules/external/@types/AssociationSirenInfertace.ts
+++
@@ -1,9 +0,0 @@
-import EtablissementSirenInterface from "./EtablissementSirenInterface";
-
-export default interface AssociationSirenInfertace {
-    siren: string,
-    categorie_juridique: string;
-    nic_siege: string,
-    etablissement_siege: EtablissementSirenInterface
-    etablissements: EtablissementSirenInterface[]
-}
