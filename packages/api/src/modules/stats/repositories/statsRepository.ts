cdd19846e38a77f9c68fa9c99e41f65db9c27655
Victor Serain <victor.serain@epitech.eu>
2022-05-17 10:09:29 +0200

    feat(api): stats allow the exclusion of the admin

--- packages/api/src/modules/stats/repositories/statsRepository.ts
+++ packages/api/src/modules/stats/repositories/statsRepository.ts
@@ -1,15 +1,28 @@
+import { DefaultObject } from '../../../@types';
 import db from '../../../shared/MongoConnection';
 
 export class StatsRepository {
     private readonly collection = db.collection("log");
 
-    public async countUsersByRequestNbOnPeriod(start: Date, end: Date, nbReq: number) {
+    public async countUsersByRequestNbOnPeriod(start: Date, end: Date, nbReq: number, includesAdmin: boolean): Promise<number> {
+        const matchQuery: { $match: DefaultObject } = { 
+            $match: {
+                timestamp: {
+                    $gte: start,
+                    $lte: end 
+                }
+            }
+        };
+        if (!includesAdmin) {
+            matchQuery.$match['meta.req.user.roles'] = { $nin: ["admin"] };
+        }
+
         return (await this.collection.aggregate([
-            { $match: { timestamp: { $gte: start, $lte: end } } } ,
+            matchQuery,
             { $group : { _id : '$meta.req.user.email', nbOfRequest : { $sum : 1} } },
             { $match: { "nbOfRequest": { $gte: nbReq } } },
             { $count: "nbOfUsers" }
-        ]).next())?.nbOfUsers;
+        ]).next())?.nbOfUsers || 0; // If no stats found nbOfUsers is null but whant retrun an number
     }
 }
 
