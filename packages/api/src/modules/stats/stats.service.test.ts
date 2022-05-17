a855ed40de5eef1fa09f20405410e6194e7a3573
Victor Serain <victor.serain@epitech.eu>
2022-05-17 11:31:07 +0200

    test(api): move tests and add test one admin filter is set

---
+++ packages/api/src/modules/stats/stats.service.test.ts
@@ -0,0 +1,43 @@
+import statsService from "./stats.service";
+import statsRepository from './repositories/statsRepository';
+
+describe("StatsService", () => {
+
+    describe("getNbUsersByRequestsOnPeriod()", () => {
+
+        const countUsersByRequestNbOnPeriodMock = jest.spyOn(statsRepository, "countUsersByRequestNbOnPeriod");
+        
+        const TODAY = new Date();
+        const NB_REQUESTS = 5;
+        const START = new Date(TODAY);
+        START.setDate(START.getDate() + -1);
+        const END = new Date(TODAY);
+        END.setDate(END.getDate() + 1);
+
+        beforeEach(() => {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            countUsersByRequestNbOnPeriodMock.mockImplementationOnce((start, end, minReq) => Promise.resolve(7))
+        })
+
+
+        it("should be call repository", async () => {
+            const expected = [START, END, NB_REQUESTS, false];
+            const actual = statsRepository.countUsersByRequestNbOnPeriod;
+            await statsService.getNbUsersByRequestsOnPeriod(START, END, NB_REQUESTS, false);
+            expect(actual).toHaveBeenCalledWith(...expected);
+        })
+
+        it("should be call repository with includesAdmin", async () => {
+            const expected = [START, END, NB_REQUESTS, true];
+            const actual = statsRepository.countUsersByRequestNbOnPeriod;
+            await statsService.getNbUsersByRequestsOnPeriod(START, END, NB_REQUESTS, true);
+            expect(actual).toHaveBeenCalledWith(...expected);
+        })
+
+        it("should be call repository", async () => {
+            const expected = 7;
+            const actual = await statsService.getNbUsersByRequestsOnPeriod(START, END, NB_REQUESTS, false);
+            expect(actual).toBe(expected);
+        })
+    })
+})
