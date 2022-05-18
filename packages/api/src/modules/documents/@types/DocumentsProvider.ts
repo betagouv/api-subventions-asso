347d56998766c42e2abe948ed80082f721704cef
Victor Serain <victor.serain@epitech.eu>
2022-05-18 10:11:44 +0200

    Documents API ASSO (#207)
    * feat(api): add documents modules
    
    * test(api): add tests on document service
    
    * feat(api): add retrieval document on api asso
    
    * test(api): add test in api asso

---
+++ packages/api/src/modules/documents/@types/DocumentsProvider.ts
@@ -0,0 +1,11 @@
+import { Siret, Rna, Siren } from "@api-subventions-asso/dto";
+import Provider from '../../providers/@types/IProvider';
+import Document from "./Document";
+
+export default interface DocumentProvider extends Provider {
+    isDocumentProvider: boolean,
+
+    getDocumentsBySiren(siren: Siren): Promise<Document[] | null>;
+    getDocumentsBySiret(siret: Siret): Promise<Document[] | null>;
+    getDocumentsByRna(rna: Rna): Promise<Document[] | null>;
+}
