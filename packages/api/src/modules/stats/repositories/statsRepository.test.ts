4ca169c295bd30385635ccd7057a3c609da67f59
Victor Serain <victor.serain@epitech.eu>
2022-05-23 16:12:03 +0200

    feat(api): add stats route, for compute median number of requests #243
    * feat(api): add stats route, for compute median number of requests
    
    * test(api): add tests on median requests stats
    
    * refactor(api): review cleaning
    
    * refactor(api): review cleanning

--- packages/api/src/modules/stats/repositories/statsRepository.test.ts
+++ packages/api/src/modules/stats/repositories/statsRepository.test.ts
@@ -32,4 +32,56 @@ describe("StatsRepository", () => {
             expect(mock.mock.calls).toMatchSnapshot()
         })
     })
+
+    describe("countMedianRequestsOnPeriod()", () => {
+        const START = dateFactory(-1);
+        const END = dateFactory(1);
+
+        it('should call mongo with admin filters', async () => {
+            // @ts-expect-error statsRepository.collection is private attribute
+            const mock = jest.spyOn(statsRepository.collection, "aggregate").mockImplementationOnce(() => ({toArray: () => ([])}));
+
+            await statsRepository.countMedianRequestsOnPeriod(START, END, false);
+            
+            expect(mock.mock.calls).toMatchSnapshot()
+        })
+
+        it('should call mongo without admin filters', async () => {
+            // @ts-expect-error statsRepository.collection is private attribute
+            const mock = jest.spyOn(statsRepository.collection, "aggregate").mockImplementationOnce(() => ({toArray: () => ([])}));
+
+            await statsRepository.countMedianRequestsOnPeriod(START, END, true);
+            
+            expect(mock.mock.calls).toMatchSnapshot()
+        })
+
+
+        it('should return median (odd)', async () => {
+            // @ts-expect-error statsRepository.collection is private attribute
+            jest.spyOn(statsRepository.collection, "aggregate").mockImplementationOnce(() => ({toArray: () => ([
+                { nbOfRequest: 1 },
+                { nbOfRequest: 2 },
+                { nbOfRequest: 3 }
+            ])}));
+            const expected = 2;
+            const actual = await statsRepository.countMedianRequestsOnPeriod(START, END, true);
+            
+            expect(actual).toBe(expected)
+        })
+
+
+        it('should return median (pair)', async () => {
+            // @ts-expect-error statsRepository.collection is private attribute
+            jest.spyOn(statsRepository.collection, "aggregate").mockImplementationOnce(() => ({toArray: () => ([
+                { nbOfRequest: 1 },
+                { nbOfRequest: 2 },
+                { nbOfRequest: 3 },
+                { nbOfRequest: 4 }
+            ])}));
+            const expected = 2.5;
+            const actual = await statsRepository.countMedianRequestsOnPeriod(START, END, true);
+            
+            expect(actual).toBe(expected)
+        })
+    })
 })
