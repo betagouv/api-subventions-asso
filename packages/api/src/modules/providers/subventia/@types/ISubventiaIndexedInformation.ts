8aea4037a862e737b92549a2b53e2c1f55fa859c
Victor Serain <victor.serain@epitech.eu>
2022-05-24 16:42:57 +0200

    feat(api): add subventia entity

---
+++ packages/api/src/modules/providers/subventia/@types/ISubventiaIndexedInformation.ts
@@ -0,0 +1,11 @@
+export default interface ISubventiaIndexedInformation {
+    initule: string,
+    description: string,
+    exerciceBudgetaire: number,
+    budgetGlobal: number,
+    montantSollicite?: number,
+    decision?: string,
+    dateDecision?: string,
+    financeurs: string,
+    status: string
+}
