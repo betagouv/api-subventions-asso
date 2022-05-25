29700b5481c99a89c177f1e0e5cebf9d1bc55209
Maxime Silvère Meunier <mxmeunier@gmail.com>
2022-05-25 11:39:29 +0200

    feat(api): add migration script

---
+++ packages/api/migrations/20220525083959-update-fonjep-import-date-and-add-templs-plein.js
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { connectDB } = require('../build/src/shared/MongoConnection');
+const { printAtSameLine } = require("../build/src/shared/helpers/CliHelper");
+const { ObjectId } = require('mongodb');
+
+module.exports = {
+    async up(db, client) {
+        console.log("Connecting to DB...");
+        connectDB();
+        const fonjepCollection = db.collection("fonjep");
+        const cursor = db.collection("fonjep").find({});
+        console.log("Starting fonjep update...");
+        let counter = 0;
+        while (await cursor.hasNext()) {
+            const doc = await cursor.next();
+            if (!doc) continue;
+            const iso = new Date("2022-03-14").toISOString();
+            await fonjepCollection.updateOne(
+                { _id: ObjectId(doc._id) },
+                { $set: 
+                  { 
+                      "indexedInformations.updated_at": new Date(iso), 
+                      "indexedInformations.plein_temps": doc.data.PleinTemps
+                  }
+                }
+            );
+            counter++;
+            printAtSameLine(counter.toString());
+        }
+    },
+
+    async down(db, client) {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+    }
+};
