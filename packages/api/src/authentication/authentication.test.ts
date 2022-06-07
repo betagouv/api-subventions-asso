0f4eb5877326a40a12c6b747a6cd0224b466cf18
Maxime Meunier <mxmeunier@gmail.com>
2022-06-07 10:21:10 +0200

    291 move auth tests (#292)
    * refactor(api): remove eslint warnings (#239)
    
    * Documents API ASSO (#207)
    
    * feat(api): add documents modules
    
    * test(api): add tests on document service
    
    * feat(api): add retrieval document on api asso
    
    * test(api): add test in api asso
    
    * feat(api): stats allow the exclusion of the admin
    
    * docs(api, dto): add doc for route stats request
    
    * test(api): move tests and add test one admin filter is set
    
    * refactor(api): review cleaning
    
    * refactor(api): review cleaning
    
    * chore: add github action on develop (#245)
    
    * chore: add github action on develop
    
    * refactor(api): mv parse in CliController (#242)
    
    * feat(front): add pipe drive chat bot
    
    * feat(api,front,dto): error on login better manage
    
    * test(api): update test auth
    
    * feat(api): add route for getting list of etablissements
    
    * fix(api): quick fix not uuse var date_modif_siren before checking if var exist
    
    * feat(api): add stats route, for compute median number of requests #243
    
    * feat(api): add stats route, for compute median number of requests
    
    * test(api): add tests on median requests stats
    
    * refactor(api): review cleaning
    
    * refactor(api): review cleanning
    
    * feat(front): add tiret - on value not found (#249)
    
    * feat(front): add tiret - on value not found
    
    * feat(front): add tiret - on value not found
    
    * feat(api): add route for getting one etablissement (#255)
    
    * chore(api): manage http error propagation
    
    * feat(api): add route for getting one etablissement
    
    * feat(api): good management of error
    
    * feat(front): add € on amount
    
    * feat(front): add € on amount
    
    * refactor(front): use toLocalString
    
    * 205 async aggregate (#253)
    
    * refactor(api): make asso/etab aggregate async
    
    * refactor(api): remove optional rna in search getBySiren
    
    * refactor(api): factorize associations aggregates
    
    * feat(api): update README (#258)
    
    * fix(api): fix siret as siren in association-name (#263)
    
    * fix(api): fix siret as siren in association-name
    
    * fix(api): replace siret by siren
    
    * feat(api): add migration to fill association-name from osiris (#259)
    
    
    * feat(api): add migration to fill association-name from Osiris
    
    * 248 update fonjep import (#256)
    
    248 update fonjep import (#256)
    
    * 59 split osiris repository (#270)
    
    * Ajout d'un parser subventia (#265)
    
    * chore(api): migration repository has abstract class
    
    * feat(api): add subventia entity
    
    * feat(api): add subventia service and repository
    
    * feat(api): add subventia parser
    
    * feat(api): add subventia cli
    
    * test(api): add tests on subventia
    
    * refactor(api): mv authentication tests
    
    Co-authored-by: Victor Serain <victor.serain@epitech.eu>

---
+++ packages/api/src/authentication/authentication.test.ts
@@ -0,0 +1,114 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as express from "express";
+
+import { expressAuthentication } from "../../src/authentication/authentication";
+import jwt from "jsonwebtoken";
+import userService, { UserServiceError } from "../../src/modules/user/user.service";
+import userRepository from "../../src/modules/user/repositoies/user.repository";
+import { ObjectId } from "mongodb";
+
+describe("expressAuthentication", () => {
+
+    // Spys
+    const verifyMock = jest.spyOn(jwt, "verify");
+    const findByEmailMock = jest.spyOn(userService, "findByEmail");
+    const findJwtByEmailMock = jest.spyOn(userService, "findJwtByEmail");
+    const findJwtMock = jest.spyOn(userRepository, "findJwt");
+    const updateMock = jest.spyOn(userRepository, "update");
+    const SPYS = [verifyMock, findByEmailMock, findByEmailMock, findJwtMock, updateMock];
+
+    const DEFAULT_REQ = {
+        body: {},
+        query: {},
+        headers: {
+            "x-access-token": "token"
+        } as unknown
+    } as express.Request;
+    const SECURITY_NAME = "jwt";
+    // @ts-expect-error: mock
+    const VERIFY_DEFAULT_MOCK = (_, __, cb: (err: null, d: unknown) => void) => {
+        cb(null, { email: "test@beta.gouv.fr" })
+    };
+    const DEFAULT_TOKEN = DEFAULT_REQ.headers["x-access-token"] as string;
+
+    let warn: jest.SpyInstance;
+    beforeAll(() => {
+        warn = jest.spyOn(console, 'warn').mockImplementation();
+        findByEmailMock.mockImplementation((email) => Promise.resolve({ email, roles: ["user"], active: true, _id: new ObjectId() }))
+        findJwtByEmailMock.mockImplementation(() => Promise.resolve({ success: true, jwt: { token: DEFAULT_TOKEN, expirateDate: new Date() } }))
+        findJwtMock.mockImplementation(() => Promise.resolve({ token: DEFAULT_TOKEN, expirateDate: new Date() }))
+        updateMock.mockImplementation((user) => Promise.resolve({ email: user.email, roles: ["user", "admin"], active: true, _id: new ObjectId() }))
+        // @ts-expect-error: mock
+        verifyMock.mockImplementation(VERIFY_DEFAULT_MOCK);
+    });
+
+
+    afterAll(() => {
+        warn.mockRestore();
+        SPYS.forEach(spy => spy.mockRestore());
+    })
+
+    it("should throw an error when security do not use JWT", () => {
+        const req = {} as express.Request;
+        expect(expressAuthentication(req, "NO_JWT_SECURITY")).rejects.toThrowError("Internal server error");
+    });
+
+    it("should throw an error when token not provided", () => {
+        const req = {
+            body: {},
+            query: {},
+            headers: {}
+        } as express.Request;
+        expect(expressAuthentication(req, SECURITY_NAME)).rejects.toThrowError("No token provided");
+    });
+
+    it("should throw error when token parsing failed", () => {
+        // @ts-expect-error: mock
+        verifyMock.mockImplementationOnce((_, __, cb: (err: boolean, d: unknown) => void) => { cb(true, {}) });
+
+        const REQ_WITH_WRONG_TOKEN = {
+            body: {},
+            query: {},
+            headers: {
+                "x-access-token": "WRONG_TOKEN"
+            } as unknown
+        } as express.Request;
+
+        expect(expressAuthentication(REQ_WITH_WRONG_TOKEN, SECURITY_NAME)).rejects.toThrowError("JWT parse error");
+    });
+
+    it("should throw an error when user is not found", async () => {
+        findByEmailMock.mockImplementationOnce(async () => null);
+        await expect(expressAuthentication(DEFAULT_REQ, SECURITY_NAME)).rejects.toThrowError("User not found");
+    });
+
+    it("should throw an error when JWT is not found", async () => {
+        findJwtByEmailMock.mockImplementationOnce(async () => ({ success: false } as UserServiceError));
+        await expect(expressAuthentication(DEFAULT_REQ, SECURITY_NAME)).rejects.toThrowError("JWT is not valid anymore");
+    });
+
+    it("should throw an error when JWT is not corresponding", async () => {
+        const TOKEN = "NOT_CORRESPONDING_TOKEN";
+        findJwtByEmailMock.mockImplementationOnce(() => Promise.resolve({ success: true, jwt: { token: TOKEN, expirateDate: new Date() } }))
+        await expect(expressAuthentication(DEFAULT_REQ, SECURITY_NAME)).rejects.toThrowError("JWT is not valid anymore");
+    });
+
+    it("should throw an error when JWT has expired", async () => {
+        findJwtByEmailMock.mockImplementationOnce(async () => ({ success: true, jwt: { token: DEFAULT_TOKEN, expirateDate: new Date(Date.now() - 1000 * 60 * 60 * 24) } }));
+        await expect(expressAuthentication(DEFAULT_REQ, SECURITY_NAME)).rejects.toThrowError("JWT has expired, please login try again");
+    });
+
+    it("should throw an error when user role is not allowed", async () => {
+        await expect(expressAuthentication(DEFAULT_REQ, SECURITY_NAME, ["admin"])).rejects.toThrowError("JWT does not contain required scope.");
+    });
+
+    it("should throw an error when user is not active", async () => {
+        findByEmailMock.mockImplementationOnce((email) => Promise.resolve({ email, roles: ["user"], active: false, _id: new ObjectId() }))
+        await expect(expressAuthentication(DEFAULT_REQ, SECURITY_NAME, ["admin"])).rejects.toThrowError("User is not active");
+    });
+
+    it("should validate user", async () => {
+        findByEmailMock.mockImplementationOnce((email) => Promise.resolve({ email, roles: ["user", "admin"], active: true, _id: new ObjectId() }))
+        await expect(expressAuthentication(DEFAULT_REQ, SECURITY_NAME, ["admin"])).resolves.toMatchObject({ email: "test@beta.gouv.fr", roles: ["user", "admin"], active: true })
+    });
+});
