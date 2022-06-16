dec18aaaa402fbae7136f552618c9f24e2a70235
Maxime Silvère Meunier <mxmeunier@gmail.com>
2022-06-16 14:27:31 +0200

    feat(api, front): integ svelte and active cors

---
+++ packages/front/svelte/shared/Auth.svelte
@@ -0,0 +1,36 @@
+<script>
+  import axios from "axios";
+  import UserService from "../../src/modules/user/user.service";
+  import { user as userStore } from "../store/auth.store";
+
+  async function getUser() {
+    let user;
+    try {
+      user = (await axios.get("/auth/token")).data;
+    } catch (e) {
+      document.location.href = "/";
+    }
+    userStore.update((oldUser) => Object.assign(oldUser, user));
+  }
+
+  async function getRole(user) {
+    const roles = (await UserService.getRoles(user)).data;
+    userStore.update((oldUser) => Object.assign(oldUser, { roles }));
+    return roles;
+  }
+
+  async function initApp() {
+    await getUser();
+    return await getRole($userStore);
+  }
+
+  const promise = initApp();
+</script>
+
+<div>
+  {#await promise}
+    spinning...
+  {:then user}
+    <slot />
+  {/await}
+</div>
