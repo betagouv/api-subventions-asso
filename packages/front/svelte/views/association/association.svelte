347206cffac57ffd8117c865d788c7a68cc22035
Maxime Silvère Meunier <mxmeunier@gmail.com>
2022-06-20 14:55:36 +0200

    feat(front): add info legale for association view

--- packages/front/svelte/views/association/association.svelte
+++ packages/front/svelte/views/association/association.svelte
@@ -3,6 +3,7 @@
   import axios from "axios";
   import { user as userStore } from "../../store/user.store";
   import Breadcrumb from "../../dsfr/Breadcrumb.svelte";
+  import ProviderValueHelper from "../../../src/shared/helpers/ProviderValueHelper";
 
   export let route;
   const { getApiUrl } = getContext("app");
@@ -14,21 +15,72 @@
 
   onMount(async () => {
     promise = getAssociation();
+    console.log({ promise });
   });
 
-  async function getAssociation() {
+  function getAssociation() {
     const path = `${apiUrl}/${route}`;
-    return await axios.get(path, { headers: { "x-access-token": token } });
+    return axios.get(path, { headers: { "x-access-token": token } }).then((result) => {
+      const association = result.data.association;
+      Object.keys(result.data.association).map((key) => (association[key] = ProviderValueHelper.getValue(association[key])));
+      return association;
+    });
   }
 </script>
 
 <Breadcrumb {segments} />
-<div>
-  {#await promise}
-    <p>...waiting</p>
-  {:then association}
-    <p>Here is your association : {association}</p>
-  {:catch error}
-    <p style="color: red">{error.message}</p>
-  {/await}
-</div>
+{#await promise}
+  <p>Fetching association...</p>
+{:then association}
+  <h1>{association?.denomination}</h1>
+  <div class="grid">
+    <div>
+      <div>
+        <span>RNA</span>
+        <p>{association?.rna}</p>
+      </div>
+      <div>
+        <span>SIREN</span>
+        <p>{association?.siren}</p>
+      </div>
+      <div>
+        <span>SIRET du siège</span>
+        <p>{association?.siren + association?.nic_siege}</p>
+      </div>
+    </div>
+    <div>
+      <span>Objet social</span>
+      <p>{association?.objet_social}</p>
+    </div>
+    <div>
+      <div>
+        <span>Adresse siège</span>
+        <p>{association?.adresse_siege?.code_postal} - {association?.adresse_siege?.commune}</p>
+      </div>
+      <div>
+        <span>Date d'immatriculation</span>
+        <p>{new Date(association?.date_creation).toLocaleDateString()}</p>
+      </div>
+      {#if association?.date_modification}
+        <div>
+          <span>Dernière modification au greffe</span>
+          <p>{new Date(association?.date_modification).toLocaleDateString()}</p>
+        </div>
+      {/if}
+    </div>
+  </div>
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await}
+
+<style>
+  .grid {
+    display: grid;
+    grid-template-rows: auto;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  .grid span {
+    font-weight: 700;
+  }
+</style>
