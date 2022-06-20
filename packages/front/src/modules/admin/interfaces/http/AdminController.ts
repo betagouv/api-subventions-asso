dd1a5dd76a8f8c75030e6bb95d635cc54adfba36
Victor Serain <victor.serain@epitech.eu>
2022-06-20 16:53:30 +0200

    274 admin show user by domaine name (#303)
    * feat(front): refacto admin front views
    
    * feat(front): show users by domain name (admin views)
    
    * refactor(front): ortho

--- packages/front/src/modules/admin/interfaces/http/AdminController.ts
+++ packages/front/src/modules/admin/interfaces/http/AdminController.ts
@@ -1,5 +1,7 @@
+import UserDto from '@api-subventions-asso/dto/user/UserDto';
 import { NextFunction, Request, Response } from 'express';
 import User from '../../../../@types/User';
+import { DefaultObject } from '../../../../@types/utils';
 import Controller from '../../../../decorators/controller.decorator';
 import { Get, Post } from '../../../../decorators/http.methods.decorator';
 import adminService from '../../user.service';
@@ -8,33 +10,88 @@ import adminService from '../../user.service';
 export default class AdminController {
 
     @Get("")
-    public async loginView(req: Request, res: Response, next: NextFunction) {
+    public async adminView(req: Request, res: Response, next: NextFunction) {
+        if (!req.session.roles || !req.session.roles.includes("admin")) {
+            return res.redirect("/");
+        }
+
+        res.render('admin/index', {
+            pageTitle: 'Admin',
+        });
+    }
+
+    @Get("/users/list")
+    public async listUsersView(req: Request, res: Response, next: NextFunction) {
         if (!req.session.roles || !req.session.roles.includes("admin")) {
             return res.redirect("/");
         }
 
         const result = await adminService.listUsers(req.session.user as User);
 
-        res.render('admin/index', {
-            pageTitle: 'Admin',
+        res.render('admin/list-users', {
+            pageTitle: 'Admin - Liste des utilisateurs',
             users: result.type === "SUCCESS" ? result.data : [],
-            error: req.query.error,
-            success: req.query.success
         })
     }
 
-    @Post("/user/create")
+    @Get("/users/domain")
+    public async domainUsersView(req: Request, res: Response, next: NextFunction) {
+        if (!req.session.roles || !req.session.roles.includes("admin")) {
+            return res.redirect("/");
+        }
+
+        const result = await adminService.listUsers(req.session.user as User);
+
+        const users = result.data || [];
+
+        const usersByDomainName = users.reduce((acc, user) => {
+            const domaine = user.email.match(/@.+/)?.toString();
+
+            if (!domaine) return acc;
+
+            if (!acc[domaine]) acc[domaine] = [];
+            acc[domaine].push(user);
+            return acc;
+        }, {} as DefaultObject<UserDto[]>)
+
+
+        res.render('admin/domain', {
+            pageTitle: 'Admin - Nom de domaines',
+            usersByDomainName
+        })
+    }
+
+    @Get("/users/create")
+    public async createUserView(req: Request, res: Response, next: NextFunction) {
+        if (!req.session.roles || !req.session.roles.includes("admin")) {
+            return res.redirect("/");
+        }
+
+        return res.render('admin/create-user', {
+            pageTitle: 'Admin - Création d\'utilisateur',
+        })
+    }
+
+
+    @Post("/users/create")
     public async createUser(req: Request, res: Response, next: NextFunction) {
         if (!req.session.roles || !req.session.roles.includes("admin")) {
             return res.redirect("/");
         }
 
         if (!req.body.email) {
-            return res.redirect("/admin?error=USER_EMAIL_NOT_FOUND")
+            return res.render('admin/create-user', {
+                pageTitle: 'Admin - Création d\'utilisateur',
+                error: "USER_EMAIL_NOT_FOUND",
+            });
         }
         
         const result = await adminService.createUser(req.body.email, req.session.user);
 
-        return res.redirect("/admin?" +(result.type === "SUCCESS" ? "success=true" : "error=API_ERROR"));
+        return res.render('admin/create-user', {
+            pageTitle: 'Admin - Création d\'utilisateur',
+            error: result.type === "SUCCESS" ? null : "API_ERROR",
+            success: result.type === "SUCCESS"
+        })
     }
 }
