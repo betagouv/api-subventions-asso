9b2bc872b8f7e2a84adc7315c5d83c0d4106335f
Victor Serain <victor.serain@epitech.eu>
2022-06-20 11:16:00 +0200

    chore(tools): add osiris data extractor

---
+++ packages/tools/osiris-automation/index.js
@@ -0,0 +1,37 @@
+require('dotenv/config') // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
+const OsirisExtractDownloader = require("./Osiris/OsirisExtractDownloader");
+const OsirisPosibilitiesBuilder = require("./Osiris/OsirisPosibilitiesBuilder");
+const OsirisPuppeteer = require('./OsirisPuppeteer/OsirisPuppeteer');
+
+const OSIRIS_URL = process.env.OSIRIS_URL || "https://osiris.extranet.jeunesse-sports.gouv.fr/";
+const OSIRIS_EMAIL = process.env.OSIRIS_EMAIL || "";
+const OSIRIS_PASSWORD = process.env.OSIRIS_PASSWORD || "";
+const year = process.argv[2] || new Date().getFullYear().toString();
+
+console.log(year);
+
+
+async function startExtractOsiris(cookies, year) {
+    const osirisDownloader = new OsirisExtractDownloader(cookies, false);
+    const osirisPosibilitiesBuilder = new OsirisPosibilitiesBuilder(year, cookies, false);
+
+    const posibilities = await osirisPosibilitiesBuilder.buildPosibilities();
+
+    await posibilities.reduce(async (acc, posibility, i) => {
+        await acc;
+        await osirisDownloader.download(posibility);
+        console.log(`${i}/${posibilities.length}`)
+    }, Promise.resolve());
+} 
+
+
+(async () => {
+    const osirisPuppeteer = new OsirisPuppeteer(OSIRIS_EMAIL, OSIRIS_PASSWORD, OSIRIS_URL);
+
+    const cookies = await osirisPuppeteer.connect();
+
+    await startExtractOsiris(cookies, year);
+    // await osirisPuppeteer.extractDossiers(year);
+    await osirisPuppeteer.close();
+})();
