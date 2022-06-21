51ca9c0439689e97bf2114f33708991bfdc10363
Victor Serain <victor.serain@epitech.eu>
2022-06-21 16:09:53 +0200

    feat(front): add cgu page (#313)

---
+++ packages/front/src/modules/cgu/interface/http/CGUController.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from 'express';
+import Controller from '../../../../decorators/controller.decorator';
+import { Get } from '../../../../decorators/http.methods.decorator';
+
+@Controller("/cgu")
+export default class CGUController {
+
+    @Get("")
+    public cguView(req: Request, res: Response, next: NextFunction) {
+        res.render('cgu/index.ejs', {
+            pageTitle: 'Conditions générales d’utilisation'
+        })
+    }
+}
