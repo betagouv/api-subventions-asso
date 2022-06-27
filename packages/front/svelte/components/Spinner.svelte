e6e81e6a1e5328ecdf127d26b4df93d4164b1085
Victor Serain <victor.serain@epitech.eu>
2022-06-27 14:44:22 +0200

    feat(front): add etablissements in association svelte view (#329)
    * feat(front): add etablissements in association svelte view
    
    * refactor(front): review cleening

---
+++ packages/front/svelte/components/Spinner.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let description = ""
+</script>
+
+<div class="spinner-container">
+    <p>{description}</p>
+    <img src="/static/images/logo-beta.jpg" class="spinner" alt="Chargement en cours">
+</div>
+
+<style> 
+    @keyframes spinner {
+        to {transform: rotate(360deg);}
+    }
+    .spinner {
+        width: 70px;
+        animation: spinner 1.8s ease infinite;
+    }
+    .spinner-container {
+        display: flex;
+        justify-content: space-around;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
