3ea132cf42c4137eccdbc71912ee6f27357a8e3e
Maxime Meunier <mxmeunier@gmail.com>
2022-06-28 11:53:41 +0200

    feat(front): add prettier (#333)
    Format views folder with prettier
    
    Update prettier config

--- packages/front/svelte/dsfr/TabContent.svelte
+++ packages/front/svelte/dsfr/TabContent.svelte
@@ -3,6 +3,11 @@
   export let index;
 </script>
 
-<div id={`tabpanel-${index}-panel`} class={`fr-tabs__panel fr-tabs__panel--${selected ? "selected" : ""}`} role="tabpanel" aria-labelledby={`tabpanel-${index}`} tabindex="0">
+<div
+  id={`tabpanel-${index}-panel`}
+  class={`fr-tabs__panel fr-tabs__panel--${selected ? "selected" : ""}`}
+  role="tabpanel"
+  aria-labelledby={`tabpanel-${index}`}
+  tabindex="0">
   <slot />
 </div>
