e361b67ddfe82d47f086cd45cb89e800294d9b62
Maxime Silvère Meunier <mxmeunier@gmail.com>
2022-07-07 15:32:38 +0200

    feat(api, dto): update DTO and TSOA doc for Open Data routes

--- packages/api/src/modules/open-data/provider/interfaces/http/ProviderController.ts
+++ packages/api/src/modules/open-data/provider/interfaces/http/ProviderController.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Route } from 'tsoa';
+import { Controller, Get, Route, Tags } from 'tsoa';
 import ProvidersInfos from '../../entities/ProvidersInfos';
 import providerService from '../../provider.service';
 
 @Route("open-data/providers")
+@Tags("Open Data")
 export class ProviderController extends Controller {
     @Get("/")
     async getProvidersInfos(): Promise<ProvidersInfos> {
