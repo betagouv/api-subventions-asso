cd562d38bbd4d398fdab5fb8319a4f00365207aa
Victor Serain <victor.serain@epitech.eu>
2022-07-25 11:23:21 +0200

    feat(api): add posibilities to remove user (#364)
    * feat(api): add posibilities to remove user
    
    * feat(front): checkout admin list user in svelte
    
    * fix(front): prettier
    
    * refactor(front): clean for revie

--- packages/api/src/modules/user/interfaces/http/UserController.ts
+++ packages/api/src/modules/user/interfaces/http/UserController.ts
@@ -1,6 +1,7 @@
 import UserDto from '@api-subventions-asso/dto/user/UserDto';
 import { Request as ExRequest } from 'express';
-import { Route, Controller, Tags, Post, Body, Security, Put, Request, Get } from 'tsoa';
+import { ObjectId } from 'mongodb';
+import { Route, Controller, Tags, Post, Body, Security, Put, Request, Get, Delete, Path, Response } from 'tsoa';
 import User, { UserWithoutSecret } from '../../entities/User';
 import userService from '../../user.service';
 
@@ -44,6 +45,24 @@ export class UserController extends Controller {
             this.setStatus(500);
         }
         return result[0];
+    }
+
+    @Delete("/admin/user/:id")
+    @Security("jwt", ['admin'])
+    @Response(204, "Retourne true si l'opération s'est bien exécutée", { success: true })
+    @Response(500, "Retourne false si une erreur est survenue pendant l'exécution", { success: false })
+    public async deleteUser(
+        @Path() id: string
+    ): Promise<{success: boolean }> {
+        const result = await userService.delete({_id: new ObjectId(id)});
+
+        if (!result.success) {
+            this.setStatus(500);
+        }
+        else {
+            this.setStatus(204)
+        }
+        return result;
     }
     
 
---
+++ packages/front/svelte/views/admin/users/AdminUsers.svelte
@@ -0,0 +1,88 @@
+<script>
+    import admin from "../admin.service.js";
+    import { user as userStore } from "../../../store/user.store";
+
+    import Breadcrumb from "../../../dsfr/Breadcrumb.svelte";
+    import Spinner from "../../../components/Spinner.svelte";
+    import ErrorAlert from "../../../components/ErrorAlert.svelte";
+    import StatsUsers from "./composents/StatsUsers.svelte";
+    import SearchUsers from "./composents/SearchUsers.svelte";
+    import TableUsers from "./composents/TableUsers.svelte";
+    import Button from "../../../dsfr/Button.svelte";
+
+    const segments = [
+        { label: "Accueil", url: "/" },
+        { label: "Admin", url: "/admin" },
+        { label: `Liste des utilisateurs` }
+    ];
+
+    let users = [];
+    const currentAdminUser = $userStore;
+
+    if (!currentAdminUser || !currentAdminUser.roles || !currentAdminUser.roles.includes("admin"))
+        document.location.href = "/";
+
+    let promise = new Promise(() => null);
+
+    const loadUsers = () => {
+        promise = admin.getUsers().then(_users => (users = _users)); // Not use then in template beacause var can be declared for bind;
+    };
+
+    loadUsers();
+
+    const downloadCsv = () => {
+        const delimiter = ";";
+        const userCSV = users.map(user =>
+            [
+                user.email,
+                user.roles.join(" - "),
+                user.active ? "Oui" : "Non",
+                user.resetToken ? `/auth/reset-password/${user.resetToken}?active=true` : "",
+                user.resetTokenDate
+            ].join(delimiter)
+        );
+        const header = ["Email", "Roles", "Actif", "Lien d'activation", "Date du token de reset"].join(delimiter);
+
+        const csvContent = [header, ...userCSV].join("\n");
+
+        const encodedUri = encodeURI("data:text/csv;charset=utf-8," + csvContent);
+        const link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", `users-${new Date().toLocaleDateString()}.csv`);
+        document.body.appendChild(link);
+        link.click();
+    };
+</script>
+
+<Breadcrumb {segments} />
+{#await promise}
+    <Spinner description="Chargement des utilisateurs en cours ..." />
+{:then}
+    <div class="fr-grid-row">
+        <Button on:click={downloadCsv}>Téléchager la liste en CSV</Button>
+    </div>
+    <div>
+        <div class="fr-grid-row fr-grid-row--center fr-grid-row--gutters admin_list-users_stats">
+            <StatsUsers {users} />
+        </div>
+
+        <div class="fr-grid-row">
+            <div class="fr-col fr-col-lg-12 fr-grid-row fr-grid-row--center">
+                <h2>Liste des utilisateurs :</h2>
+            </div>
+
+            <SearchUsers bind:users />
+
+            <TableUsers {users} on:userDeleted={loadUsers} />
+        </div>
+    </div>
+{:catch error}
+    <ErrorAlert message={error.message} />
+{/await}
+
+<style>
+    .admin_list-users_stats {
+        margin-top: 20px;
+        margin-bottom: 50px;
+    }
+</style>
