c293bb633fa4a3666a3a307fdb9d526b56af7dcc
Victor Serain <victor.serain@epitech.eu>
2022-08-23 17:14:27 +0200

    fix(api): fix migration

--- packages/api/migrations/20220823095709-addChorusIndexes.js
+++ packages/api/migrations/20220823095709-addChorusIndexes.js
@@ -5,7 +5,7 @@ const { connectDB } = require('../build/src/shared/MongoConnection');
 module.exports = {
   async up(db, client) {
     await connectDB();
-    await db.getCollection('chorus-line').createIndex({ "indexedInformations.siret": 1 });
+    await db.collection('chorus-line').createIndex({ "indexedInformations.siret": 1 });
     // TODO write your migration here.
     // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
     // Example:
