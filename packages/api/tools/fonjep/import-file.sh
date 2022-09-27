a142c7afeda0cd67b10e9ca5f17414b8b6505406
Victor Serain <victor.serain@epitech.eu>
2022-09-27 16:20:41 +0200

    fix(api): fix import fonjep script

--- packages/api/tools/fonjep/import-file.sh
+++ packages/api/tools/fonjep/import-file.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-mkdir /tmp/files
-tar -xf /tmp/uploads/* -C /tmp/files
+echo "\n\n\nThis script accept only xls or xlsx file \n\n\n"
 
-# Est-ce synchrone ? Pour attendre que la collection soit bien drop avant de lancer la suite !=: Reponse: Oui c'est syncrone
-node ./packages/api/build/src/cli.js fonjep rename old_fonjep
-node ./packages/api/build/src/cli.js fonjep parse /tmp/files/$FILE_NAME $IMPORT_DATE
+node  ./packages/api/build/src/cli.js fonjep parse /tmp/uploads $IMPORT_DATE
 
 for file in "./logs/*"; do
     cat $file;
