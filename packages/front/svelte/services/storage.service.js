cd166502af2ef6739f6ddd7a8912664bd00ee1de
Maxime Silvère Meunier <mxmeunier@gmail.com>
2022-10-11 12:33:17 +0200

    feat(front): save in localStorage search history

---
+++ packages/front/svelte/services/storage.service.js
@@ -0,0 +1,7 @@
+export const updateSearchHistory = ({ rna, siren, name, objectSocial }) => {
+    let history = JSON.parse(localStorage.getItem("search-history")) || [];
+    if (history.find(search => search.rna === rna)) return;
+    history.push({ rna, siren, name, objectSocial });
+    if (history.length > 4) history = history.slice(1);
+    localStorage.setItem("search-history", JSON.stringify(history));
+};
