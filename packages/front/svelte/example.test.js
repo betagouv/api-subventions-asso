9a9924ffb1d2fae9999c17d6d181457441c25a50
Maxime Silvère Meunier <mxmeunier@gmail.com>
2022-10-14 12:25:50 +0200

    feat(front): init front unit tests

---
+++ packages/front/svelte/example.test.js
@@ -0,0 +1,7 @@
+describe("front end app unit test example", () => {
+    it("should return true", () => {
+        const expected = true;
+        const actual = true;
+        expect(actual).toBe(expected);
+    });
+});
