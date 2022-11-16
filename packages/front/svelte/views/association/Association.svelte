25ee182899dfcc01c0d11e2927b4bdf4b17bb734
Maxime Meunier <mxmeunier@gmail.com>
2022-11-16 15:34:13 +0100

    feat(front): move Breadcrumbs uses in Router (#608)

--- packages/front/svelte/views/association/Association.svelte
+++ packages/front/svelte/views/association/Association.svelte
@@ -1,28 +1,21 @@
 <script>
     import { isAssociation } from "../../helpers/entrepriseHelper";
     import associationService from "./association.service.js";
-
-    import Breadcrumb from "../../dsfr/Breadcrumb.svelte";
     import Alert from "../../dsfr/Alert.svelte";
-
     import Spinner from "../../components/Spinner.svelte";
     import ErrorAlert from "../../components/ErrorAlert.svelte";
     import InfosLegales from "./components/InfosLegales.svelte";
     import TabsAsso from "./components/TabsAsso.svelte";
     import DataNotFound from "../../components/DataNotFound.svelte";
+    import { segments } from "../../store/url.store"
 
-    export let route = "";
-    const routeSegments = route.split("/");
-    const id = routeSegments[routeSegments.length - 1];
-
+    const id = $segments.at(-1);
     let promise = associationService.getAssociation(id);
 
     const titles = ["Tableau de bord", "Pièces administratives", "Établissements"];
 
-    const segments = [{ label: "Accueil", url: "/" }, { label: `Association (${id})` }];
 </script>
 
-<Breadcrumb {segments} />
 {#await promise}
     <Spinner description="Chargement de l'association {id} en cours ..." />
 {:then association}
