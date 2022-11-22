70b4554f74249518011f2e17a5ad87124239d0bf
Maxime Silvère Meunier <mxmeunier@gmail.com>
2022-11-22 09:58:16 +0100

    fix(api): use real compare method to fix the test

---
+++ packages/api/src/shared/helpers/ArrayHelper.test.ts
@@ -0,0 +1,47 @@
+import * as ArrayHelper from "./ArrayHelper";
+
+describe("ArrayHelper", () => {
+    describe("commpare()", () => {
+        it("should return -1", () => {
+            const expected = -1;
+            const actual = ArrayHelper.compare("a", "b");
+            expect(actual).toEqual(expected)
+        });
+
+        it("should return 1", () => {
+            const expected = 1;
+            const actual = ArrayHelper.compare("b", "a");
+            expect(actual).toEqual(expected)
+        });
+
+        it("should return 0", () => {
+            const expected = 0;
+            const actual = ArrayHelper.compare("a", "a");
+            expect(actual).toEqual(expected)
+        });
+    });
+
+    describe("compareByValue()", () => {
+        it("should return a function", () => {
+            const expected = "function";
+            const actual = typeof ArrayHelper.compareByValueBuilder("provider");
+            expect(actual).toEqual(expected);
+        })
+
+        it("should compare on value", () => {
+            const objA = { provider: "ABC" };
+            const objB = { provider: "BCD" };
+            const expected = [objA, objB];
+            const actual = [objB, objA].sort(ArrayHelper.compareByValueBuilder("provider"));
+            expect(actual).toEqual(expected);
+        });
+
+        it("should compare on deep value", () => {
+            const objA = { siret: { provider: "ABC" } };
+            const objB = { siret: { provider: "BCD" } };
+            const expected = [objA, objB];
+            const actual = [objB, objA].sort(ArrayHelper.compareByValueBuilder("siret.provider"));
+            expect(actual).toEqual(expected);
+        })
+    });
+});
