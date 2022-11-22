031cd129d31411f86b7b135ad618f4b73b7a2d22
Victor Serain <victor.serain@epitech.eu>
2022-11-22 11:22:41 +0100

    fix(api): fix migration duplicate unique id

--- packages/api/migrations/20221116094853-create-index-dauhpin-caches.js
+++ packages/api/migrations/20221116094853-create-index-dauhpin-caches.js
@@ -4,7 +4,7 @@ const { connectDB } = require('../build/src/shared/MongoConnection');
 module.exports = {
     async up(db, client) {
         await connectDB();
-        await db.collection('dauphin-caches').createIndex({ "reference": 1 }, { "unique": true });
+        await db.collection('dauphin-caches').createIndex({ "reference": 1 }, { "unique": true, dropDups:true});
         await db.collection('dauphin-caches').createIndex({ "demandeur.SIRET.SIREN": 1 });
         await db.collection('dauphin-caches').createIndex({ "demandeur.SIRET.complet": 1 });
     },
