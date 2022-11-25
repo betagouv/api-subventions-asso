fe6f31097a29ee7d8d407e6f201e1be4cb0a69b1
Victor Serain <victor.serain@epitech.eu>
2022-11-25 11:39:18 +0100

    feat(front): remplace ejs domain admin view in svelte view (#673)

--- packages/front/svelte/routes.js
+++ packages/front/svelte/routes.js
@@ -2,12 +2,14 @@ import Association from "./views/association/Association.svelte";
 import Etablissement from "./views/etablissement/Etablissement.svelte";
 import AdminUsers from "./views/admin/users/AdminUsers.svelte";
 import Home from "./views/home/Home.svelte";
+import AdminDomains from "./views/admin/domains/AdminDomains.svelte";
 import Admin from "./views/admin/Admin.svelte";
 
 export default {
     "association/:id": () => Association,
     "etablissement/:id": () => Etablissement,
     "admin/users/list": () => AdminUsers,
+    "admin/users/domain": () => AdminDomains,
     "admin": () => Admin,
     "/": () => Home
 };
