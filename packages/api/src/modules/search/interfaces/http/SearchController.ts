9b3e02716ad2f4f4c8b78ca945327d20e9d70f0e
Victor Serain <victor.serain@epitech.eu>
2022-11-29 11:12:14 +0100

    feat(api): remove unused endpoint and services method

--- packages/api/src/modules/search/interfaces/http/SearchController.ts
+++ packages/api/src/modules/search/interfaces/http/SearchController.ts
@@ -1,6 +1,5 @@
-import { Route, Get, Controller, Tags, Security, Response, Deprecated } from 'tsoa';
-import { Siret, Etablissement, GetEtablissementResponseDto, SearchEtablissementSuccessResponseDto } from '@api-subventions-asso/dto';
-import AssociationNameEntity from '../../../association-name/entities/AssociationNameEntity';
+import { Route, Get, Controller, Tags, Security, Response } from "tsoa";
+import AssociationNameEntity from "../../../association-name/entities/AssociationNameEntity";
 
 import searchService from "../../search.service";
 import { ErrorResponse } from "@api-subventions-asso/dto/shared/ResponseStatus";
@@ -9,42 +8,32 @@ import { ErrorResponse } from "@api-subventions-asso/dto/shared/ResponseStatus";
 @Security("jwt")
 @Tags("Search Controller")
 export class SearchController extends Controller {
-    /**
-     * Recherche des demandes de subventions via le siret de l'établissment
-     * 
-     * @summary Cette route va bientôt disparaitre, ne pas l'utiliser
-     * @param siret Identifiant Siret
-     */
-    @Deprecated()
-    @Get("/etablissement/{siret}")
-    @Response<SearchEtablissementSuccessResponseDto>("200")
-    @Response<ErrorResponse>("404")
-    public async findBySiret(
-        siret: Siret,
-    ): Promise<GetEtablissementResponseDto> {
-        const result = await searchService.getBySiret(siret) as Etablissement;
-        if (!result) {
-            this.setStatus(404);
-            return { success: false, message: "Etablissement not found" }
-        } else {
-            return { success: true, etablissement: result };
-        }
+  /**
+   * Recherche une association via son rna, siren et nom partiel ou complet
+   * @summary Recherche une association via son rna, siren et nom partiel ou complet
+   * @param rna_or_siren Identifiant RNA ou Identifiant Siren
+   */
+  @Get("/associations/{input}")
+  @Response<ErrorResponse>("404", "Aucune association retrouvée", {
+    success: false,
+    message: "Could match any association with given input : ${input}",
+  })
+  public async findAssociations(
+    input: string
+  ): Promise<{
+    success: boolean;
+    result?: AssociationNameEntity[];
+    message?: string;
+  }> {
+    const result = await searchService.getAssociationsKeys(input);
+    if (!result || (Array.isArray(result) && result.length == 0)) {
+      this.setStatus(404);
+      return {
+        success: false,
+        message: `Could match any association with given input : ${input}`,
+      };
     }
-
-    /**
-     * Recherche une association via son rna, siren et nom partiel ou complet
-     * @summary Recherche une association via son rna, siren et nom partiel ou complet
-     * @param rna_or_siren Identifiant RNA ou Identifiant Siren
-     */
-    @Get("/associations/{input}")
-    @Response<ErrorResponse>("404", "Aucune association retrouvée", { success: false, message: "Could match any association with given input : ${input}" })
-    public async findAssociations(input: string): Promise<{ success: boolean, result?: AssociationNameEntity[], message?: string }> {
-        const result = await searchService.getAssociationsKeys(input);
-        if (!result || (Array.isArray(result) && result.length == 0)) {
-            this.setStatus(404);
-            return { success: false, message: `Could match any association with given input : ${input}` }
-        }
-        this.setStatus(200);
-        return { success: true, result };
-    }
+    this.setStatus(200);
+    return { success: true, result };
+  }
 }
