b2264414079d6c7a74d17d69328278f100a77fed
Maxime Silvère Meunier <mxmeunier@gmail.com>
2022-12-01 08:39:39 +0100

    feat(api): add unique indexe on osirisId request

--- packages/api/migrations/20221118092644-add-osiris-requests-indexes.js
+++ packages/api/migrations/20221118092644-add-osiris-requests-indexes.js
@@ -7,7 +7,7 @@ module.exports = {
         const collection = db.collection("osiris-requests");
         collection.createIndex({"legalInformations.siret": 1});
         collection.createIndex({"legalInformations.rna": 1});
-        collection.createIndex({"providerInformations.osirisId": 1});
+        collection.createIndex({"providerInformations.osirisId": 1}, { unique: true });
     },
 
     async down(db, client) {
