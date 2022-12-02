efe51db8cf04c6f009dfef9348880559546d35cc
Victor Serain <victor.serain@epitech.eu>
2022-12-02 17:14:37 +0100

    feat(front): add resources subventions

---
+++ packages/front/svelte/resources/subventions/subventions.port.js
@@ -0,0 +1,63 @@
+import { flatenProviderValue } from "../../helpers/dataHelper"; // Voir si il ne vaux mieux pas passer ça en adapteur
+import SSEConnector from "../../shared/SseConnector";
+import Store from "../../shared/Store";
+
+class SubventionsPort {
+    _getSubventionsConnectedStore(identifier, type) {
+        const path = `/sse/${type}/${identifier}/subventions`;
+        const connector = new SSEConnector(path);
+        const store = new Store({
+            status: "inProgress",
+            subventions: [],
+            __meta__: {
+                providerCalls: 0,
+                providerAnswers: 0
+            }
+        });
+
+        connector.on("data", data => {
+            // first message event
+            if (!data.subventions && data.__meta__?.totalProviders) {
+                store.update(state => ({
+                    ...state,
+                    __meta__: {
+                        ...state.__meta__,
+                        providerCalls: data.__meta__.totalProviders
+                    }
+                }));
+            } else if (data.subventions) {
+                store.update(state => ({
+                    ...state,
+                    subventions: state.subventions.concat(data.subventions.map(d => flatenProviderValue(d))),
+                    __meta__: {
+                        ...state.__meta__,
+                        providerAnswers: state.__meta__.providerAnswers + 1
+                    }
+                }));
+            }
+        });
+
+        connector.on("close", () => {
+            store.update(state => {
+                return {
+                    ...state,
+                    status: "close"
+                };
+            });
+        });
+
+        return store;
+    }
+
+    getAssociationSubventionsStore(identifier) {
+        return this._getSubventionsConnectedStore(identifier, "association");
+    }
+
+    getEtablissementSubventionsStore(identifier) {
+        return this._getSubventionsConnectedStore(identifier, "etablissement");
+    }
+}
+
+const subventionsPort = new SubventionsPort();
+
+export default subventionsPort;
