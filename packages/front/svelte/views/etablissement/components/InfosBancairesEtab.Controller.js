fbb6519b9411fc3d17442923e80a5f6bb61b83de
alice.telescoop <alice@telescoop.fr>
2022-12-02 18:22:05 +0100

    refactor(front): formatter as method

--- packages/front/svelte/views/etablissement/components/InfosBancairesEtab.Controller.js
+++ packages/front/svelte/views/etablissement/components/InfosBancairesEtab.Controller.js
@@ -2,17 +2,19 @@ import { writable } from "svelte/store";
 import DateHelper from "../../../../src/shared/helpers/DateHelper";
 import { getDate, getProvider, getValue } from "../../../helpers/providerValueHelper";
 
-function formatBankElement(informationBancaireEtab) {
-    return informationBancaireEtab.flat().map(infoBancaireSourced => ({
-        ...getValue(infoBancaireSourced),
-        date: DateHelper.formatDate(getDate(infoBancaireSourced)),
-        provider: getProvider(infoBancaireSourced)
-    }));
-}
-
 export class InfosBancairesEtabController {
     constructor(informationBancaireEtab) {
-        this.infosBancaires = writable(formatBankElement(informationBancaireEtab));
+        this.infosBancaires = writable(this._formatBankElement(informationBancaireEtab));
         this.headers = ["BIC", "IBAN", "Date de dépôt", "Source de dépôt"];
+    }
+
+    _formatBankElement(informationBancaireEtab) {
+        return (
+            informationBancaireEtab?.flat()?.map(infoBancaireSourced => ({
+                ...getValue(infoBancaireSourced),
+                date: DateHelper.formatDate(getDate(infoBancaireSourced)),
+                provider: getProvider(infoBancaireSourced)
+            })) || []
+        );
     }
 }
