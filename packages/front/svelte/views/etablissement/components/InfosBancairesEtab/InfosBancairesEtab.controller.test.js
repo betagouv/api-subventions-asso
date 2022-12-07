e9d6b684bafb3e80ac8970afd4eefcf9a0678e54
alice.telescoop <alice@telescoop.fr>
2022-12-07 17:39:03 +0100

    test(api): adapt to inforomation_banquaire format

--- packages/front/svelte/views/etablissement/components/InfosBancairesEtab/InfosBancairesEtab.controller.test.js
+++ packages/front/svelte/views/etablissement/components/InfosBancairesEtab/InfosBancairesEtab.controller.test.js
@@ -1,44 +1,34 @@
 import { InfosBancairesEtabController } from "./InfosBancairesEtab.controller";
 
-/* jest.mock("../../../../helpers/dataHelper.js", () => ({
-    ...jest.requireActual("../../../../helpers/dataHelper.js"),
-    valueOrHyphen: jest.fn(v => v),
-    formatPhoneNumber: jest.fn(v => v)
-})); */
-
 describe("InfosBancairesEtabController", () => {
     const infosBanquairesRaw = [
-        [
-            {
-                value: {
-                    iban: "FR1517569000301937696517I48",
-                    bic: "BTOTORPPPAA"
-                },
-                provider: "BASE SIREN <Via API ASSO>",
-                last_update: "2021-11-28T00:00:00.000Z",
-                type: "object"
-            }
-        ],
-        [
-            {
-                value: {
-                    iban: "FR6312739000403863291458F56",
-                    bic: "TOTOFRPPPAA"
-                },
-                provider: "BASE SIREN <Via API ASSO>",
-                last_update: "2021-10-27T00:00:00.000Z",
-                type: "object"
+        {
+            value: {
+                iban: "FR1517569000301937696517I48",
+                bic: "BTOTORPPPAA"
             },
-            {
-                value: {
-                    iban: "FR8912739000506746997936N17",
-                    bic: "TOTOFRPTOTO"
-                },
-                provider: "BASE SIREN <Via API ASSO>",
-                last_update: "2021-12-01T00:00:00.000Z",
-                type: "object"
-            }
-        ]
+            provider: "BASE SIREN <Via API ASSO>",
+            last_update: "2021-11-28T00:00:00.000Z",
+            type: "object"
+        },
+        {
+            value: {
+                iban: "FR6312739000403863291458F56",
+                bic: "TOTOFRPPPAA"
+            },
+            provider: "BASE SIREN <Via API ASSO>",
+            last_update: "2021-10-27T00:00:00.000Z",
+            type: "object"
+        },
+        {
+            value: {
+                iban: "FR8912739000506746997936N17",
+                bic: "TOTOFRPTOTO"
+            },
+            provider: "BASE SIREN <Via API ASSO>",
+            last_update: "2021-12-01T00:00:00.000Z",
+            type: "object"
+        }
     ];
     const flatSourcedInfosBancaires = [
         {
