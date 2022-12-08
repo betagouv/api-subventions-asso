aecb3f1c17a0c42b443a86613d73d5bd22bec640
alice.telescoop <alice@telescoop.fr>
2022-12-08 11:49:59 +0100

    fix(front): mocking types

--- packages/api/src/modules/etablissements/etablissements.service.test.ts
+++ packages/api/src/modules/etablissements/etablissements.service.test.ts
@@ -6,94 +6,95 @@ import subventionsService from "../subventions/subventions.service";
 import versementsService from "../versements/versements.service";
 import { EtablissementAdapter } from "./EtablissementAdapter";
 import etablissementService from "./etablissements.service";
+import { DefaultObject } from "../../@types";
+
+type asyncPrivateMock<T> = jest.SpyInstance<Promise<T>>;
 
 const SIREN = "000000000";
 const SIRET = "000000000000001";
 const ETABLISSEMENT_1 = {
-  siret: [{ value: SIRET }],
-  nic: [{}],
-  versements: {},
-  demandes_subventions: {},
+    siret: [
+        {
+            value: SIRET,
+            provider: "BASE SIREN <Via API ASSO>",
+            last_update: "2022-08-29T00:00:00.000Z" as unknown as Date,
+            type: "string"
+        }
+    ],
+    nic: [],
+    versements: [],
+    demandes_subventions: []
 };
 const ETABLISSEMENT_2 = {
-  siret: [{ value: "000000000000002" }],
-  nic: [{}],
-  versements: {},
-  demandes_subventions: {},
+    siret: [
+        {
+            value: "000000000000002",
+            provider: "BASE SIREN <Via API ASSO>",
+            last_update: "2022-08-29T00:00:00.000Z" as unknown as Date,
+            type: "string"
+        }
+    ],
+    nic: [],
+    versements: [],
+    demandes_subventions: []
 };
 
 describe("EtablissementsService", () => {
-  const toSimplifiedEtablissementMock = jest.spyOn(
-    EtablissementAdapter,
-    "toSimplifiedEtablissement"
-  );
-  //@ts-expect-error: mock private method
-  const aggregateMock = jest.spyOn(etablissementService, "aggregate");
-  //@ts-expect-error: mock private method
-  jest
-    .spyOn(etablissementService, "scoreEtablisement")
-    .mockImplementation(() => 1);
-  //@ts-expect-error: mock
-  jest.spyOn(FormaterHelper, "formatData").mockImplementation((data) => data);
+    const toSimplifiedEtablissementMock = jest.spyOn(EtablissementAdapter, "toSimplifiedEtablissement");
 
-  describe("getVersements", () => {
-    const getVersementsBySiretMock = jest.spyOn(
-      versementsService,
-      "getVersementsBySiret"
-    );
+    //@ts-expect-error: mock private method
+    const aggregateMock = jest.spyOn(etablissementService, "aggregate") as asyncPrivateMock<Etablissement>;
 
-    it("should call versement service", async () => {
-      getVersementsBySiretMock.mockImplementation(async () => []);
+    (
+        jest
+            //@ts-expect-error: mock private method
+            .spyOn(etablissementService, "scoreEtablisement") as asyncPrivateMock<number>
+    ).mockResolvedValue(1);
 
-      await etablissementService.getVersements(SIRET);
+    // @ts-ignore because formatHelper does black magic
+    jest.spyOn(FormaterHelper, "formatData").mockImplementation((data, providerScore) => data);
 
-      expect(getVersementsBySiretMock).toHaveBeenCalledWith(SIRET);
+    describe("getVersements", () => {
+        const getVersementsBySiretMock = jest.spyOn(versementsService, "getVersementsBySiret");
+
+        it("should call versement service", async () => {
+            getVersementsBySiretMock.mockImplementation(async () => []);
+
+            await etablissementService.getVersements(SIRET);
+
+            expect(getVersementsBySiretMock).toHaveBeenCalledWith(SIRET);
+        });
     });
-  });
 
-  describe("getSubventions()", () => {
-    const getDemandesByEtablissementMock = jest.spyOn(
-      subventionsService,
-      "getDemandesByEtablissement"
-    );
+    describe("getSubventions()", () => {
+        const getDemandesByEtablissementMock = jest.spyOn(subventionsService, "getDemandesByEtablissement");
 
-    it("should call DemandeSubventionService.getByAssociation()", async () => {
-      getDemandesByEtablissementMock.mockImplementationOnce(
-        () => new Flux<SubventionsFlux>()
-      );
-      etablissementService.getSubventions(SIREN);
-      expect(getDemandesByEtablissementMock).toHaveBeenCalledWith(SIREN);
+        it("should call DemandeSubventionService.getByAssociation()", async () => {
+            getDemandesByEtablissementMock.mockImplementationOnce(() => new Flux<SubventionsFlux>());
+            etablissementService.getSubventions(SIREN);
+            expect(getDemandesByEtablissementMock).toHaveBeenCalledWith(SIREN);
+        });
     });
-  });
 
-  describe("getDocuments", () => {
-    const getDocumentBySiretMock = jest.spyOn(
-      documentsService,
-      "getDocumentBySiret"
-    );
-    const SIRET = "000000000000000";
+    describe("getDocuments", () => {
+        const getDocumentBySiretMock = jest.spyOn(documentsService, "getDocumentBySiret");
+        const SIRET = "000000000000000";
 
-    it("should call subventions service", async () => {
-      getDocumentBySiretMock.mockImplementation(async () => []);
+        it("should call subventions service", async () => {
+            getDocumentBySiretMock.mockImplementation(async () => []);
 
-      await etablissementService.getDocuments(SIRET);
+            await etablissementService.getDocuments(SIRET);
 
-      expect(getDocumentBySiretMock).toHaveBeenCalledWith(SIRET);
+            expect(getDocumentBySiretMock).toHaveBeenCalledWith(SIRET);
+        });
     });
-  });
 
-  describe("getEtablissementsBySiren", () => {
-    it("should call toSimplifiedEtablissement", async () => {
-      // @ts-expect-error: mock private method
-      aggregateMock.mockImplementationOnce(async () => [
-        ETABLISSEMENT_1,
-        ETABLISSEMENT_2,
-      ]);
-      toSimplifiedEtablissementMock.mockImplementationOnce(
-        (etablissement) => etablissement
-      );
-      await etablissementService.getEtablissementsBySiren(SIREN);
-      expect(toSimplifiedEtablissementMock).toHaveBeenCalledTimes(2);
+    describe("getEtablissementsBySiren", () => {
+        it("should call toSimplifiedEtablissement", async () => {
+            aggregateMock.mockResolvedValueOnce([ETABLISSEMENT_1, ETABLISSEMENT_2]);
+            toSimplifiedEtablissementMock.mockImplementationOnce(etablissement => etablissement);
+            await etablissementService.getEtablissementsBySiren(SIREN);
+            expect(toSimplifiedEtablissementMock).toHaveBeenCalledTimes(2);
+        });
     });
-  });
 });
