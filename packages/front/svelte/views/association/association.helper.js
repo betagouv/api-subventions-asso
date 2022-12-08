68d4caae00b764718a8b94beda2970ea7eeedf8a
Maxime Meunier <mxmeunier@gmail.com>
2022-12-08 14:56:51 +0100

    fix(front): link chorus versements to requests

--- packages/front/svelte/views/association/association.helper.js
+++ packages/front/svelte/views/association/association.helper.js
@@ -54,10 +54,17 @@ export const mapSubventionsAndVersements = ({ subventions, versements }) => {
     return uniformizedElements.sort(sortByDateAsc);
 };
 
-const getYearOfItem = item => {
-    if (item.isSub) return item.annee_demande;
-    if (item.periodeDebut) return new Date(item.periodeDebut).getFullYear();
-    return "";
+// Return year of subvention or versement as a string
+export const getYearOfElement = element => {
+    if (element.isSub) return getSubventionYear(element);
+    if (element.isVersement) return getVersementYear(element);
+};
+
+export const getSubventionYear = subvention => subvention.annee_demande;
+
+export const getVersementYear = versement => {
+    if (versement.periodeDebut) return new Date(versement.periodeDebut).getFullYear();
+    if (versement.dateOperation) return new Date(versement.dateOperation).getFullYear();
 };
 
 const groupByVersementKey = (acc, curr) => {
@@ -67,7 +74,9 @@ const groupByVersementKey = (acc, curr) => {
         return acc;
     }
 
-    const key = curr.versementKey + "-" + getYearOfItem(curr); // Discuter avec maxime pour faire cette manip cotée api
+    const year = getYearOfElement(curr);
+    const yearStr = year ? String(year) : "";
+    const key = `${curr.versementKey}-${yearStr}`; // Discuter avec maxime pour faire cette manip cotée api
 
     if (!acc.withKey[key]) acc.withKey[key] = [];
 
---
+++ packages/front/svelte/views/association/association.helper.test.js
@@ -0,0 +1,75 @@
+import * as AssociationHelper from "./association.helper";
+
+const TODAY = new Date();
+const YESTERDAY = (function (date) {
+    const today = new Date(date);
+    today.setDate(today.getDate() - 1);
+    return today;
+})(TODAY);
+
+console.log(TODAY);
+console.log(YESTERDAY);
+
+const SUBVENTION = {
+    isSub: true,
+    annee_demande: TODAY.getFullYear()
+};
+
+const FONJEP_VERSEMENT = {
+    isVersement: true,
+    periodDebut: TODAY,
+    dateOperation: YESTERDAY
+};
+
+const CHORUS_VERSEMENT = {
+    isVersement: true,
+    dateOperation: YESTERDAY
+};
+
+describe("Association Helper", () => {
+    describe("getYearOfElement()", () => {
+        const TODAY_YEAR = TODAY.getFullYear();
+        const YESTERDAY_YEAR = YESTERDAY.getFullYear();
+
+        it.each`
+            element             | expected
+            ${SUBVENTION}       | ${TODAY_YEAR}
+            ${FONJEP_VERSEMENT} | ${TODAY_YEAR}
+            ${CHORUS_VERSEMENT} | ${YESTERDAY_YEAR}
+        `("should return a year", ({ element, expected }) => {
+            const actual = AssociationHelper.getYearOfElement(element);
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    describe("getSubventionYear()", () => {
+        it("should return a year", () => {
+            const actual = AssociationHelper.getSubventionYear(SUBVENTION);
+            expect(actual).toEqual(SUBVENTION.annee_demande);
+        });
+
+        it("should return undefined", () => {
+            const actual = AssociationHelper.getSubventionYear({ annee_demande: undefined });
+            expect(actual).toEqual(undefined);
+        });
+    });
+
+    describe("getVersementYear()", () => {
+        it.each`
+            versement           | expected
+            ${FONJEP_VERSEMENT} | ${FONJEP_VERSEMENT.periodDebut.getFullYear()}
+            ${CHORUS_VERSEMENT} | ${CHORUS_VERSEMENT.dateOperation.getFullYear()}
+        `("should return a year", ({ versement, expected }) => {
+            const actual = AssociationHelper.getVersementYear(versement);
+            expect(actual).toEqual(expected);
+        });
+
+        it("should return undefined", () => {
+            const actual = AssociationHelper.getVersementYear({
+                periodDebut: undefined,
+                dateOperation: undefined
+            });
+            expect(actual).toEqual(undefined);
+        });
+    });
+});
