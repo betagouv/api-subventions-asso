bab7b379899cd036ff5ec8e9d3322910f19f466e
Maxime Silvère Meunier <mxmeunier@gmail.com>
2022-12-08 14:45:35 +0100

    fix(api): update EtablissementController getDemandeSubventions() tests

--- packages/api/src/modules/etablissements/interfaces/http/EtablissementController.ts
+++ packages/api/src/modules/etablissements/interfaces/http/EtablissementController.ts
@@ -4,7 +4,7 @@ import {
     GetEtablissementResponseDto,
     GetSubventionsResponseDto,
     GetVersementsResponseDto,
-    Siret,
+    Siret
 } from "@api-subventions-asso/dto";
 import { Route, Get, Controller, Tags, Security, Response } from "tsoa";
 import etablissementService from "../../etablissements.service";
@@ -18,22 +18,20 @@ import * as IdentifierHelper from "../../../../shared/helpers/IdentifierHelper";
 @Security("jwt")
 @Tags("Etablissement Controller")
 export class EtablissementController extends Controller {
-  /**
-   * Remonte les informations d'un établissement
-   * @param siret Identifiant Siret
-   */
-  @Get("/{siret}")
-  @Response<ErrorResponse>("400", "Identifiant incorrect", {
-      success: false,
-      message: "You must provide a valid SIRET",
-  })
-  @Response<ErrorResponse>("404", "L'établissement n'a pas été trouvé", {
-      success: false,
-      message: "Etablissement not found",
-  })
-    public async getEtablissement(
-        siret: Siret
-    ): Promise<GetEtablissementResponseDto> {
+    /**
+     * Remonte les informations d'un établissement
+     * @param siret Identifiant Siret
+     */
+    @Get("/{siret}")
+    @Response<ErrorResponse>("400", "Identifiant incorrect", {
+        success: false,
+        message: "You must provide a valid SIRET"
+    })
+    @Response<ErrorResponse>("404", "L'établissement n'a pas été trouvé", {
+        success: false,
+        message: "Etablissement not found"
+    })
+    public async getEtablissement(siret: Siret): Promise<GetEtablissementResponseDto> {
         const type = IdentifierHelper.getIdentifierType(siret);
 
         if (!type || type !== StructureIdentifiersEnum.siret) {
@@ -47,59 +45,61 @@ export class EtablissementController extends Controller {
         return { success: true, etablissement };
     }
 
-  /**
-   * Recherche les demandes de subventions liées à un établisement
-   *
-   * @summary Recherche les demandes de subventions liées à un établisement
-   * @param siret Identifiant Siret
-   */
-  @Get("/{siret}/subventions")
-  @Response<ErrorResponse>("404")
-  public async getDemandeSubventions(
-      siret: Siret
-  ): Promise<GetSubventionsResponseDto> {
-      const data = await etablissementService.getSubventions(siret).toPromise();
-      const subventions = data
-          .map((subFlux) => subFlux.subventions)
-          .flat()
-          .filter((subvention) => subvention) as DemandeSubvention[];
+    /**
+     * Recherche les demandes de subventions liées à un établisement
+     *
+     * @summary Recherche les demandes de subventions liées à un établisement
+     * @param siret Identifiant Siret
+     */
+    @Get("/{siret}/subventions")
+    @Response<ErrorResponse>("404")
+    public async getDemandeSubventions(siret: Siret): Promise<GetSubventionsResponseDto> {
+        try {
+            const data = await etablissementService.getSubventions(siret).toPromise();
+            const subventions = data
+                .map(subFlux => subFlux.subventions)
+                .flat()
+                .filter(subvention => subvention) as DemandeSubvention[];
+            return { success: true, subventions };
+        } catch (e) {
+            this.setStatus(404);
+            return { success: false, message: (e as Error).message };
+        }
+    }
 
-      return { success: true, subventions };
-  }
+    /**
+     * Recherche les versements liées à un établisement
+     *
+     * @summary Recherche les versements liées à un établisement
+     * @param siret Identifiant Siret
+     */
+    @Get("/{siret}/versements")
+    @Response<ErrorResponse>("404")
+    public async getVersements(siret: Siret): Promise<GetVersementsResponseDto> {
+        try {
+            const versements = await etablissementService.getVersements(siret);
+            return { success: true, versements };
+        } catch (e: unknown) {
+            this.setStatus(404);
+            return { success: false, message: (e as Error).message };
+        }
+    }
 
-  /**
-   * Recherche les versements liées à un établisement
-   *
-   * @summary Recherche les versements liées à un établisement
-   * @param siret Identifiant Siret
-   */
-  @Get("/{siret}/versements")
-  @Response<ErrorResponse>("404")
-  public async getVersements(siret: Siret): Promise<GetVersementsResponseDto> {
-      try {
-          const versements = await etablissementService.getVersements(siret);
-          return { success: true, versements };
-      } catch (e: unknown) {
-          this.setStatus(404);
-          return { success: false, message: (e as Error).message };
-      }
-  }
-
-  /**
-   * Recherche les documents liées à un établisement
-   *
-   * @summary Recherche les documents liées à un établisement
-   * @param siret Identifiant Siret
-   */
-  @Get("/{siret}/documents")
-  @Response<ErrorResponse>("404")
-  public async getDocuments(siret: Siret): Promise<GetDocumentsResponseDto> {
-      try {
-          const documents = await etablissementService.getDocuments(siret);
-          return { success: true, documents };
-      } catch (e: unknown) {
-          this.setStatus(404);
-          return { success: false, message: (e as Error).message };
-      }
-  }
+    /**
+     * Recherche les documents liées à un établisement
+     *
+     * @summary Recherche les documents liées à un établisement
+     * @param siret Identifiant Siret
+     */
+    @Get("/{siret}/documents")
+    @Response<ErrorResponse>("404")
+    public async getDocuments(siret: Siret): Promise<GetDocumentsResponseDto> {
+        try {
+            const documents = await etablissementService.getDocuments(siret);
+            return { success: true, documents };
+        } catch (e: unknown) {
+            this.setStatus(404);
+            return { success: false, message: (e as Error).message };
+        }
+    }
 }
