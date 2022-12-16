c43ae61cbb08c4c05f64b050cadf77dbb41af0e9
alice.telescoop <alice@telescoop.fr>
2022-12-16 14:28:58 +0100

    feat(api): simplify async/await

--- packages/api/src/modules/stats/stats.service.ts
+++ packages/api/src/modules/stats/stats.service.ts
@@ -3,16 +3,16 @@ import assoVisitsRepository from "./repositories/associationVisits.repository";
 import { Association } from "@api-subventions-asso/dto";
 
 class StatsService {
-    async getNbUsersByRequestsOnPeriod(start: Date, end: Date, minReq: number, includesAdmin: boolean) {
-        return await statsRepository.countUsersByRequestNbOnPeriod(start, end, minReq, includesAdmin);
+    getNbUsersByRequestsOnPeriod(start: Date, end: Date, minReq: number, includesAdmin: boolean) {
+        return statsRepository.countUsersByRequestNbOnPeriod(start, end, minReq, includesAdmin);
     }
 
-    async getMedianRequestsOnPeriod(start: Date, end: Date, includesAdmin: boolean) {
-        return await statsRepository.countMedianRequestsOnPeriod(start, end, includesAdmin);
+    getMedianRequestsOnPeriod(start: Date, end: Date, includesAdmin: boolean) {
+        return statsRepository.countMedianRequestsOnPeriod(start, end, includesAdmin);
     }
 
-    async getRequestsPerMonthByYear(year: number, includesAdmin: boolean) {
-        return await statsRepository.countRequestsPerMonthByYear(year, includesAdmin);
+    getRequestsPerMonthByYear(year: number, includesAdmin: boolean) {
+        return statsRepository.countRequestsPerMonthByYear(year, includesAdmin);
     }
 
     registerRequest(association: Association) {
