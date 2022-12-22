32901ea60b3bbdd5a1c5e7d88387483a6d9ab817
Maxime Meunier <mxmeunier@gmail.com>
2022-12-22 14:44:15 +0100

    fix(front): display breadcrumb for Etablissement view (#756)

--- packages/front/svelte/services/router.service.js
+++ packages/front/svelte/services/router.service.js
@@ -2,6 +2,7 @@
 export const buildBreadcrumbs = path => {
     const crumbs = [];
     if (path.includes("association")) crumbs.push({ label: `Association (${path.split("/").at(-1)})` });
+    if (path.includes("etablissement")) crumbs.push({ label: `Etablissement (${path.split("/").at(-1)})` });
     else if (path.includes("admin")) {
         crumbs.push({ label: "Admin", url: "/admin" });
         if (path.includes("users/list")) crumbs.push({ label: "Liste des utilisateurs" });
