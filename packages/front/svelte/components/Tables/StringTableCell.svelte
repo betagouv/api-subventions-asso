5e2589a052911e9927b968417331f20ae7e1a682
Maxime Meunier <mxmeunier@gmail.com>
2022-12-23 08:58:26 +0100

    fix(front): v0.19.0 bugs
    fix versement date and cell content position (#758)
    
    reduce asso's exercice filter size (#757)
    
    display breadcrumb for Etablissement view (#756)
    
    display blank line for subvention with no versement (#755)
    init subvention versements to array if undefined

---
+++ packages/front/svelte/components/Tables/StringTableCell.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { isHyphen } from "../../helpers/stringHelper";
+    import TableCell from "./TableCell.svelte";
+
+    export let value;
+    export let primary = false;
+
+    let position = "start";
+
+    if (isHyphen(value)) position = "center";
+</script>
+
+<TableCell {primary} {position}>{value}</TableCell>
