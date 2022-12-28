9363a99ddda9375b7571d7888710949698157d5d
Victor Serain <victor.serain@epitech.eu>
2022-12-28 10:44:16 +0100

    feat(front): change title admin cards

--- packages/front/svelte/views/admin/Admin.svelte
+++ packages/front/svelte/views/admin/Admin.svelte
@@ -11,27 +11,25 @@
 
 <div class="fr-grid-row fr-grid-row--center fr-grid-row--gutters">
     <div class="fr-col fr-col-lg-12">
-        <div class="fr-grid-row fr-grid-row--center fr-grid-row--gutters">
-            <div class="fr-col fr-col-lg-12">
-                <div role="alert" class="fr-alert fr-alert--info fr-alert--sm">
-                    <p>Bienvenue sur l'interface administrateur</p>
-                </div>
-            </div>
+        <div class="fr-grid-row fr-grid-row--left">
+            <h2>Bienvenue sur l'interface administrateur</h2>
         </div>
         <div class="fr-grid-row fr-grid-row--gutters">
-            <Card title="Noms de domaine" url="/admin/users/domain" size="6">
-                Donne une vision sur les noms de domaine et les utilisateurs inscrits via ceux-ci
-            </Card>
-
             <Card title="Statistiques d'usage" url="/admin/stats" size="6">
                 Donne une vision sur les usages des utilisateurs inscrits
             </Card>
 
-            <Card title="Création d'un utilisateur" url="/admin/users/create" size="6">
+            <Card title="Gestion des comptes utilisateurs" url="/admin/users/list" size="6">
+                Vous permet de consulter la liste des utilisateurs
+            </Card>
+
+            <Card title="Créer un compte utilisateur" url="/admin/users/create" size="6">
                 Vous permet de créer un utilisateur
             </Card>
 
-            <Card title="Liste des utilisateurs" url="/admin/users/list" size="6">(ancienne page Admin)</Card>
+            <Card title="Noms de domaine" url="/admin/users/domain" size="6">
+                Donne une vision sur les noms de domaine et les utilisateurs inscrits via ceux-ci
+            </Card>
         </div>
     </div>
 </div>
