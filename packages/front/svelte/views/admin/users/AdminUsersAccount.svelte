ae5a0929beb827d5bc50f3bc7444bcd57f4e49f2
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-01-05 15:27:28 +0100

    feat(front): apply code review

--- packages/front/svelte/views/admin/users/AdminUsersAccount.svelte
+++ packages/front/svelte/views/admin/users/AdminUsersAccount.svelte
@@ -7,24 +7,26 @@
     import ErrorAlert from "../../../components/ErrorAlert.svelte";
     import SearchUsers from "./composents/SearchUsers.svelte";
     import TableUsers from "./composents/TableUsers.svelte";
+    import ActionGroup from "../../../components/ActionGroup.svelte";
     import Button from "../../../dsfr/Button.svelte";
+    import Alert from "../../../dsfr/Alert.svelte";
+    import Input from "../../../dsfr/Input.svelte";
     import { buildCsv, downloadCsv } from "../../../helpers/csvHelper";
     import { PAGE_ADMIN_USERS_ACCOUNT_NAME } from "../admin.constant.js";
 
     let users,
         domains = [];
+    let newDomain,
+        domainError = undefined;
 
-    // const loadUsers = () => {
-    //     usersPromise = adminService.getUsers().then(_users => (users = _users)); // Template bind cannot come from await block (c.f svelt(invalid-binding))
-    // };
-
-    const usersPromise = adminService.getUsers();
-    const domainsPromise = adminService.getUserDomaines();
-    const promises = Promise.all([usersPromise, domainsPromise]).then(results => {
-        console.log(results);
-        users = results[0];
-        domains = results[1];
-    });
+    const addDomain = async () => {
+        try {
+            await adminService.addDomain(newDomain);
+            domainError = false;
+        } catch (e) {
+            domainError = true;
+        }
+    };
 
     const removeUser = event => {
         const id = event.detail;
@@ -33,8 +35,6 @@
         // force child update by affecting a new array
         users = [...users];
     };
-
-    // loadUsers();
 
     const downloadUsersCsv = () => {
         const csvRows = users.map(user => [
@@ -63,6 +63,13 @@
 
         downloadCsv(csvString, `users-${new Date().toLocaleDateString()}`);
     };
+
+    const usersPromise = adminService.getUsers();
+    const domainsPromise = adminService.getUserDomaines();
+    const promises = Promise.all([usersPromise, domainsPromise]).then(results => {
+        users = results[0];
+        domains = results[1];
+    });
 </script>
 
 {#await promises}
@@ -70,6 +77,23 @@
 {:then _promises}
     <h2 class="fr-h2 fr-mb-6w">{PAGE_ADMIN_USERS_ACCOUNT_NAME}</h2>
     <div class="fr-grid-row fr-mb-6w">
+        <div class="fr-col-md-12">
+            {#if domainError === false}
+                <Alert title="Domaine ajouté!" type="success" />
+            {:else if domainError === true}
+                <Alert title="L'ajout du nom de domaine a échoué" />
+            {/if}
+            <div class="fr-mb-2w">
+                <ActionGroup>
+                    <svelte:fragment slot="content">
+                        <Input bind:value={newDomain} label="Ajouter un nom de domaine" />
+                    </svelte:fragment>
+                    <svelte:fragment slot="action">
+                        <Button on:click={addDomain}>Ajouter</Button>
+                    </svelte:fragment>
+                </ActionGroup>
+            </div>
+        </div>
         <div class="fr-col-md-12">
             <Widget title="Noms de domaines :">
                 <ul class="admin-domain-ul">
