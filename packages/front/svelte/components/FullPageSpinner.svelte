08fe1019a0682fb82e9e7e0e038bcba23eb3072b
Maxime Meunier <mxmeunier@gmail.com>
2023-01-09 11:35:26 +0100

    feat(front): add blue banner for asso and etab view (#841)

---
+++ packages/front/svelte/components/FullPageSpinner.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Spinner from "./Spinner.svelte";
+
+    export let description;
+</script>
+
+<div class="fill-space">
+    <Spinner {description} />
+</div>
+
+<style>
+    /* used to avoid footer overlaspsing the blue banner */
+    .fill-space {
+        min-height: 30vh;
+    }
+</style>
