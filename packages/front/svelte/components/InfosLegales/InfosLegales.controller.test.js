5a5fe62fb828539d864f4303b84b6e8abe98732a
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-01-11 10:18:45 +0100

    feat(front): create Association fixture

--- packages/front/svelte/components/InfosLegales/InfosLegales.controller.test.js
+++ packages/front/svelte/components/InfosLegales/InfosLegales.controller.test.js
@@ -1,19 +1,9 @@
 import InfosLegalesController from "./InfosLegales.controller";
+import DEFAULT_ASSOCIATION from "../../views/association/__fixtures__/Association";
 import * as AssociationHelper from "../../views/association/association.helper";
 jest.mock("../../views/association/association.helper", () => ({
-    getAddress: address => address
+    addressToString: address => address
 }));
-
-const DEFAULT_ASSOCIATION = {
-    denomination_rna: "DENOMINATION_RNA",
-    denomination_siren: "DENOMINATION_SIREN",
-    adresse_siege_rna: "Adresse RNA",
-    adresse_siege_siren: "Adresse SIREN",
-    date_creation_rna: new Date("2000-01-01"),
-    date_creation_siren: new Date("2012-01-01"),
-    date_modification_rna: new Date("2021-01-01"),
-    date_modification_siren: new Date("2016-01-01")
-};
 
 function buildPartialAssociation() {
     return { ...DEFAULT_ASSOCIATION };
---
+++ packages/front/svelte/views/association/__fixtures__/Association.js
@@ -0,0 +1,10 @@
+export default {
+    denomination_rna: "DENOMINATION_RNA",
+    denomination_siren: "DENOMINATION_SIREN",
+    adresse_siege_rna: "Adresse RNA",
+    adresse_siege_siren: "Adresse SIREN",
+    date_creation_rna: new Date("2000-01-01"),
+    date_creation_siren: new Date("2012-01-01"),
+    date_modification_rna: new Date("2021-01-01"),
+    date_modification_siren: new Date("2016-01-01")
+};
