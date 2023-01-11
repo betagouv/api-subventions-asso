8e15072e8e3cba892cd441f8553cce3877e22105
Maxime Meunier <mxmeunier@gmail.com>
2023-01-11 09:20:46 +0100

    802 move rna siren diff in modal (#859)

---
+++ packages/front/svelte/components/InfosLegales/MoreInfosLegalesModal.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { data } from "../../store/modal.store";
+
+    import Alert from "../../dsfr/Alert.svelte";
+    import ModalContent from "../../dsfr/ModalContent.svelte";
+    import Table from "../../dsfr/Table.svelte";
+</script>
+
+<ModalContent title="Détail des informations recueillies">
+    <Alert small={true}>Certaines formulations peuvent différer selon les fournisseurs de données</Alert>
+    <Table color="blue-cumulus">
+        <svelte:fragment slot="head">
+            {#each $data.headers as header}
+                <th>{header}</th>
+            {/each}
+        </svelte:fragment>
+        <svelte:fragment slot="body">
+            {#each $data.rows as row}
+                <tr>
+                    {#each row as cell}
+                        <td>{cell}</td>
+                    {/each}
+                </tr>
+            {/each}
+        </svelte:fragment>
+    </Table>
+</ModalContent>
