727366ca21704f64619f4690ab45e8f365f313da
alice.telescoop <alice@telescoop.fr>
2023-01-12 17:03:42 +0100

    chore(front): spelling

--- packages/front/svelte/dsfr/Select.svelte
+++ packages/front/svelte/dsfr/Select.svelte
@@ -17,7 +17,7 @@
         <label class="fr-label" for="select">{label}</label>
     {/if}
     <select bind:value={selected} on:change={onChange} class="fr-select" name="select">
-        <option value="" selected disabled hidden>Selectionnez une option</option>
+        <option value="" selected disabled hidden>Sélectionnez une option</option>
         {#each options as option, index}
             <option value={option.value ? option.value : index}>{option.label ? option.label : option}</option>
         {/each}
