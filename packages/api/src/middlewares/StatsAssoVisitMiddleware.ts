1b951405fe9823d9a27c2777a3066e82c4e54d57
Victor Serain <victor.serain@epitech.eu>
2023-01-13 11:58:05 +0100

    feat(api): add stats asso visits middleware

---
+++ packages/api/src/middlewares/StatsAssoVisitMiddleware.ts
@@ -0,0 +1,19 @@
+import { Response } from "express";
+import { isRequestFromAdmin } from "../shared/helpers/HttpHelper";
+import statsService from "../modules/stats/stats.service";
+import { IdentifiedRequest } from "../@types";
+import { isSiret } from "../shared/Validators";
+import { siretToSiren } from "../shared/helpers/SirenHelper";
+
+const regexPath = new RegExp("/(association|etablissement)/([W0-9]{9,10}|\\d{14})$");
+
+export default async function StatsAssoVisitMiddleware(req: IdentifiedRequest, res: Response) {
+    if (!req.user || res.statusCode !== 200 || isRequestFromAdmin(req)) return;
+    const regexResult = regexPath.exec(req.originalUrl);
+    if (!regexResult || !regexResult[2]) return;
+
+    const id = isSiret(regexResult[2]) ? siretToSiren(regexResult[2]) : regexResult[2];
+    await statsService.addAssociationVisit({ userId: req.user._id, associationIndentifier: id, date: new Date() });
+}
+
+export const StatsAssoVisitRoutesRegex = [regexPath];
