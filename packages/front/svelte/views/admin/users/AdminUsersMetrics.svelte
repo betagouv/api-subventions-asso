b32ba6dfb66b6f81e6843c545d40d8f248d65f7b
alice.telescoop <alice@telescoop.fr>
2023-01-13 12:38:33 +0100

    feat(front): use new widget + some layout

--- packages/front/svelte/views/admin/users/AdminUsersMetrics.svelte
+++ packages/front/svelte/views/admin/users/AdminUsersMetrics.svelte
@@ -1,5 +1,17 @@
 <script>
     import { PAGE_ADMIN_USERS_METRICS_NAME } from "../admin.constant";
+    import Widget from "../../../components/Widget.svelte";
+    import MonthlyUserCountByYear from "./composents/MonthlyUserCountByYear/MonthlyUserCountByYear.svelte";
 </script>
 
-<h1 class="fr-h2">{PAGE_ADMIN_USERS_METRICS_NAME}</h1>
+<h1 class="fr-h2 fr-mb-6w">{PAGE_ADMIN_USERS_METRICS_NAME}</h1>
+<div class="fr-grid-row">
+    <!-- TODO répartition utilisateurs et répartition géographique -->
+</div>
+<div class="fr-grid-row">
+    <div class="fr-col">
+        <Widget title="Nombre d'utilisateurs">
+            <MonthlyUserCountByYear />
+        </Widget>
+    </div>
+</div>
