63a89f877c6f0cecf380df15bed6b36881ec5ea6
alice.telescoop <alice@telescoop.fr>
2023-01-24 14:35:57 +0100

    feat(front): port and service

---
+++ packages/front/svelte/resources/auth/auth.port.js
@@ -0,0 +1,23 @@
+// TODO update import after switch svelte to ts #330
+import { SignupErrorCodes } from "@api-subventions-asso/dto/build/auth/SignupDtoResponse";
+import axios from "axios";
+
+export class AuthPort {
+    BASE_PATH = "/auth";
+
+    signup(email) {
+        const defaultErrorCode = SignupErrorCodes.CREATION_ERROR;
+        const path = `${this.BASE_PATH}/signup`;
+        return axios
+            .post(path, { email })
+            .then(result => result.data.email)
+            .catch(error => {
+                const errorCode = error?.response?.data?.errorCode || defaultErrorCode;
+                throw new Error(errorCode);
+            });
+    }
+}
+
+const authPort = new AuthPort();
+
+export default authPort;
