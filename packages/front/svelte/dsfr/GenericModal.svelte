06941f8cfd72f917a949428878a28651a14e9a62
Victor Serain <victor.serain@epitech.eu>
2023-01-24 10:03:22 +0100

    fix(front): add padding in modal

--- packages/front/svelte/dsfr/GenericModal.svelte
+++ packages/front/svelte/dsfr/GenericModal.svelte
@@ -8,7 +8,7 @@
         <div class="fr-grid-row fr-grid-row--center">
             <div class="fr-col-12 fr-col-md-8 fr-col-lg-8">
                 <div class="fr-modal__body">
-                    <div class="fr-modal__header flex-right">
+                    <div class="fr-modal__header">
                         <Button
                             iconPosition="right"
                             icon="close-line"
@@ -19,9 +19,29 @@
                             Fermer
                         </Button>
                     </div>
-                    <svelte:component this={$modal} />
+                    <div class="child-modal">
+                        <svelte:component this={$modal} />
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </dialog>
+
+<style>
+    .fr-modal__header {
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    .fr-modal__body {
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+        padding: 32px 36px;
+    }
+
+    .child-modal {
+        overflow-x: auto;
+    }
+</style>
