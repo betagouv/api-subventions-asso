d0553ecacc368306b66641d2dedabe065f98806b
alice.telescoop <alice@telescoop.fr>
2023-01-24 16:34:25 +0100

    feat(front): signup component and controller

---
+++ packages/front/svelte/views/auth/signup/Signup.controller.js
@@ -0,0 +1,41 @@
+import Store from "@core/Store";
+import authService from "@resources/auth/auth.service";
+// TODO update import after switch svelte to ts #330
+import { SignupErrorCodes } from "@api-subventions-asso/dto/build/auth/SignupDtoResponse";
+import { getContext } from "svelte";
+
+export default class SignupController {
+    ERROR_MESSAGES = {
+        [SignupErrorCodes.EMAIL_NOT_VALID]: "L'adresse mail fournie ne semble pas être une adresse valide.",
+        [SignupErrorCodes.USER_ALREADY_EXIST]: "Un compte lié à cette adresse mail existe déjà.",
+        [SignupErrorCodes.CREATION_ERROR]: `Une erreur est survenue lors de la création de votre compte. Re-essayer plus tard ou <a href="/contact" target="_blank" rel="noopener noreferrer">contactez-nous</a>`,
+        [SignupErrorCodes.CREATION_RESET_ERROR]: `Une erreur est survenue lors de la création de votre compte. Re-essayer plus tard ou <a href="/contact" target="_blank" rel="noopener noreferrer">contactez-nous</a>`,
+        [SignupErrorCodes.EMAIL_MUST_BE_END_GOUV]: "L'adresse mail fournie ne semble pas être liée au gouvernement."
+    };
+
+    constructor() {
+        this.app = getContext("app");
+        this.pageTitle = `Créer votre compte sur ${this.app.getName()}`;
+        this.email = new Store("");
+        this.signupPromise = new Store(Promise.resolve());
+        this.firstSubmitted = new Store(false);
+    }
+
+    onSubmit() {
+        this.signupPromise.set(authService.signup(this.email.value));
+        this.firstSubmitted.set(true);
+    }
+
+    getErrorMessage(error) {
+        const code = parseInt(error.message);
+        return this._getErrorMessageByCode(code);
+    }
+
+    _getErrorMessageByCode(code) {
+        return this.ERROR_MESSAGES[code] || "Une erreur est survenue lors de la création de votre compte.";
+    }
+
+    get contactEmail() {
+        return this.app.getContact();
+    }
+}
