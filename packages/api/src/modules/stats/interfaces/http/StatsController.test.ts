d344a73072ea448c2fa2efe09802ec0a7ce8c294
Victor Serain <victor.serain@epitech.eu>
2023-01-26 09:50:01 +0100

    refactor(api): review cleaning

--- packages/api/src/modules/stats/interfaces/http/StatsController.test.ts
+++ packages/api/src/modules/stats/interfaces/http/StatsController.test.ts
@@ -58,20 +58,7 @@ describe("StatsController", () => {
         const YEAR_NB = 2022;
         const mockedValue = {
             nombres_utilisateurs_avant_annee: 201,
-            evolution_nombres_utilisateurs: {
-                January: 201,
-                February: 21,
-                March: 20,
-                April: 201,
-                May: 13,
-                June: 201,
-                July: 201,
-                August: 15,
-                September: 201,
-                October: 300,
-                November: 201,
-                December: 1
-            }
+            evolution_nombres_utilisateurs: [201, 21, 20, 201, 13, 201, 201, 15, 201, 300, 201, 1]
         };
 
         it("should call service with args default", async () => {
