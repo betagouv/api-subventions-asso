df0bf82724f45e549d857ac8488f95dea1d144b0
alice.telescoop <alice@telescoop.fr>
2023-01-26 17:52:40 +0100

    test(front): reset pwd port & service

--- packages/front/svelte/resources/auth/auth.service.test.js
+++ packages/front/svelte/resources/auth/auth.service.test.js
@@ -3,10 +3,9 @@ import authService from "@resources/auth/auth.service";
 
 const DEFAULT_ERROR_CODE = 49;
 
-jest.mock("@api-subventions-asso/dto/build/auth/SignupDtoResponse", () => ({
-    SignupErrorCodes: {
-        EMAIL_NOT_VALID: DEFAULT_ERROR_CODE
-    },
+jest.mock("@api-subventions-asso/dto", () => ({
+    SignupErrorCodes: { EMAIL_NOT_VALID: DEFAULT_ERROR_CODE },
+    ResetPasswordErrorCodes: { INTERNAL_ERROR: DEFAULT_ERROR_CODE },
     __esModule: true // this property makes it work
 }));
 
@@ -40,6 +39,40 @@ describe("authService", () => {
             portMock.mockRejectedValueOnce({ message: ERROR_CODE.toString() });
             const expected = ERROR_CODE;
             const actual = authService.signup(EMAIL);
+            expect(actual).rejects.toBe(expected);
+        });
+    });
+
+    describe("resetPassword()", () => {
+        const portMock = jest.spyOn(authPort, "resetPassword");
+        const RES = true;
+        const PASSWORD = "very secret";
+        const TOKEN = "123";
+
+        beforeAll(() => portMock.mockResolvedValue(true));
+        afterAll(() => portMock.mockRestore());
+
+        it("rejects with appropriate code if no token", () => {
+            const test = () => authService.resetPassword();
+            expect(test).rejects.toBe(DEFAULT_ERROR_CODE);
+        });
+
+        it("calls port", async () => {
+            await authService.resetPassword(TOKEN, PASSWORD);
+            expect(portMock).toHaveBeenCalledWith(TOKEN, PASSWORD);
+        });
+
+        it("return result from port if success", async () => {
+            const expected = RES;
+            const actual = await authService.resetPassword(TOKEN, PASSWORD);
+            expect(expected).toBe(actual);
+        });
+
+        it("rejects with error code from port if given", () => {
+            const ERROR_CODE = 5;
+            portMock.mockRejectedValueOnce({ message: ERROR_CODE.toString() });
+            const expected = ERROR_CODE;
+            const actual = authService.resetPassword(TOKEN, PASSWORD);
             expect(actual).rejects.toBe(expected);
         });
     });
