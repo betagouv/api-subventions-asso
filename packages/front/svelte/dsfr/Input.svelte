ceaf776617ddc858d6d88b165d1ff86636670e8c
alice.telescoop <alice@telescoop.fr>
2023-01-30 16:11:29 +0100

    docs(front): comment weird svelte behavior

--- packages/front/svelte/dsfr/Input.svelte
+++ packages/front/svelte/dsfr/Input.svelte
@@ -7,10 +7,22 @@
 
     const name = `input-text-${id}`;
 
+    /*
+    svelte needs to know beforehand the input type to manage reactivity
+    use:typeAction is a workaround
+    cf https://stackoverflow.com/a/71193441
+    * */
     function typeAction(node) {
         node.type = type;
     }
 </script>
 
 <label class="fr-label" for={name}>{label}</label>
-<input class="fr-input" type="text" id={name} {name} bind:value required={required ? "required" : undefined} use:typeAction />
+<input
+    class="fr-input"
+    type="text"
+    id={name}
+    {name}
+    bind:value
+    required={required ? "required" : undefined}
+    use:typeAction />
