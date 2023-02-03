f53f445e1fff40039eac30547397345c6235abf6
alice.telescoop <alice@telescoop.fr>
2023-02-03 13:55:45 +0100

    feat(front): cleanup resetPassword service

--- packages/front/src/modules/auth/AuthService.ts
+++ packages/front/src/modules/auth/AuthService.ts
@@ -30,32 +30,6 @@ export class AuthService {
         }
     }
 
-    async resetPassword(
-        token: string,
-        password: string
-    ): Promise<{ type: "REDIRECT" | "SUCCESS" | "ERROR"; data?: unknown; code?: ResetPasswordErrorCodes }> {
-        try {
-            const data = (await apiDatasubService.resetPassword(token, password)).data;
-
-            if (!isNegativeResponse<ResetPasswordDtoNegativeResponse>(data)) {
-                return { type: "SUCCESS", data: data.user };
-            }
-
-            return {
-                type: "ERROR",
-                code: data.code
-            };
-        } catch (e) {
-            if (axios.isAxiosError(e)) {
-                return {
-                    type: "ERROR",
-                    code: (e as AxiosError<ResetPasswordDtoNegativeResponse>).response?.data.code
-                };
-            }
-            return { type: "ERROR", code: ResetPasswordErrorCodes.INTERNAL_ERROR };
-        }
-    }
-
     async forgetPassword(email: string): Promise<{ type: "REDIRECT" | "SUCCESS" | "ERROR"; data?: unknown }> {
         try {
             const result = await apiDatasubService.forgetPassword(email);
