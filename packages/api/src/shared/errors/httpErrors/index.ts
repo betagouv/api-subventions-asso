4d5f46b1def5606997c5de2d9f39503fc292c479
alice.telescoop <alice@telescoop.fr>
2023-02-13 16:47:19 +0100

    feat(api): include error code in Error interface

--- packages/api/src/shared/errors/httpErrors/index.ts
+++ packages/api/src/shared/errors/httpErrors/index.ts
@@ -2,3 +2,5 @@ export * from "./BadRequestError";
 export * from "./InternalServerError";
 export * from "./NotFoundError";
 export * from "./ConflictError";
+export * from "./ForbiddenError";
+export * from "./UnauthorizedError";
