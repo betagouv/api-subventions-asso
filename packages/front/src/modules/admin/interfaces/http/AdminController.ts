237ac24ada509575de98910e62d9227ada167321
Victor Serain <victor.serain@epitech.eu>
2023-02-15 16:30:27 +0100

    fix(front): fix access on admin page

--- packages/front/src/modules/admin/interfaces/http/AdminController.ts
+++ packages/front/src/modules/admin/interfaces/http/AdminController.ts
@@ -8,58 +8,32 @@ import adminService from "../../user.service";
 
 @Controller("/admin")
 export default class AdminController {
-    private checkRoleAndUseSvelte(req: Request, res: Response) {
-        if (!req.session.user.roles || !req.session.user.roles.includes("admin")) {
-            return res.redirect("/");
-        }
-
+    private useSvelte(res: Response) {
         res.sendFile(path.join(__dirname, "../../../../../static/svelte-index.html"));
     }
 
     @Get("")
     public adminView(req: Request, res: Response, next: NextFunction) {
-        return this.checkRoleAndUseSvelte(req, res);
+        return this.useSvelte(res);
     }
 
     @Get("/users/list")
     public listUsersView(req: Request, res: Response, next: NextFunction) {
-        return this.checkRoleAndUseSvelte(req, res);
+        return this.useSvelte(res);
     }
 
     @Get("/users/metrics")
     public domainUsersView(req: Request, res: Response, next: NextFunction) {
-        return this.checkRoleAndUseSvelte(req, res);
+        return this.useSvelte(res);
     }
 
     @Get("/users/create")
     public createUserView(req: Request, res: Response, next: NextFunction) {
-        return this.checkRoleAndUseSvelte(req, res);
+        return this.useSvelte(res);
     }
 
     @Get("/stats")
     public statsView(req: Request, res: Response, next: NextFunction) {
-        return this.checkRoleAndUseSvelte(req, res);
-    }
-
-    @Post("/users/create")
-    public async createUser(req: Request, res: Response, next: NextFunction) {
-        if (!req.session.user.roles || !req.session.user.roles.includes("admin")) {
-            return res.redirect("/");
-        }
-
-        if (!req.body.email) {
-            return res.render("admin/create-user", {
-                pageTitle: "Admin - Création d'utilisateur",
-                error: "USER_EMAIL_NOT_FOUND"
-            });
-        }
-
-        const result = await adminService.createUser(req.body.email, req.session.user);
-
-        return res.render("admin/create-user", {
-            pageTitle: "Admin - Création d'utilisateur",
-            error: result.type === "SUCCESS" ? null : "API_ERROR",
-            success: result.type === "SUCCESS"
-        });
+        return this.useSvelte(res);
     }
 }
