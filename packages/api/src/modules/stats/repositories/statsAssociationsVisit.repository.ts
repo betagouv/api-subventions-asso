e6508c9100a947d7ee7cfc3ec6069a7176d009f0
Victor Serain <victor.serain@epitech.eu>
2023-02-17 16:51:28 +0100

    feat(api): add endpoint user by request

--- packages/api/src/modules/stats/repositories/statsAssociationsVisit.repository.ts
+++ packages/api/src/modules/stats/repositories/statsAssociationsVisit.repository.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { AssociationIdentifiers } from "../../../@types";
 import MigrationRepository from "../../../shared/MigrationRepository";
 import AssociationVisitEntity from "../entities/AssociationVisitEntity";
@@ -13,6 +14,7 @@ export class StatsAssociationsVisitRepository extends MigrationRepository<Associ
     async createIndexes() {
         await this.collection.createIndex({ date: 1 });
         await this.collection.createIndex({ associationIdentifier: 1 });
+        await this.collection.createIndex({ userId: 1 });
     }
 
     findGroupedByAssociationIdentifierOnPeriod(start: Date, end: Date) {
@@ -20,6 +22,28 @@ export class StatsAssociationsVisitRepository extends MigrationRepository<Associ
             .aggregate([
                 {
                     $match: {
+                        date: {
+                            $gte: start,
+                            $lte: end
+                        }
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$associationIdentifier",
+                        visits: { $addToSet: "$$ROOT" }
+                    }
+                }
+            ])
+            .toArray() as Promise<{ _id: AssociationIdentifiers; visits: AssociationVisitEntity[] }[]>;
+    }
+
+    findByUserIdAndPeriodGroupedByAssociationIdentifier(userId: ObjectId, start: Date, end: Date) {
+        return this.collection
+            .aggregate([
+                {
+                    $match: {
+                        userId,
                         date: {
                             $gte: start,
                             $lte: end
