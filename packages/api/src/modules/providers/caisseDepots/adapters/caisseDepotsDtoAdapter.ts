6de6a582d5c3ec3347649a060a40170039e06e48
alice.telescoop <alice@telescoop.fr>
2023-03-01 14:46:20 +0100

    chore(api): homogenize name

--- packages/api/src/modules/providers/caisseDepots/adapters/caisseDepotsDtoAdapter.ts
+++ packages/api/src/modules/providers/caisseDepots/adapters/caisseDepotsDtoAdapter.ts
@@ -1,10 +1,10 @@
 import { DemandeSubvention } from "@api-subventions-asso/dto";
 import ProviderValueFactory from "../../../../shared/ProviderValueFactory";
 import caisseDepotsService from "../caisseDepots.service";
-import CaisseDepotsSubvDto from "../dto/CaisseDepotsSubvDto";
+import CaisseDepotsSubventionDto from "../dto/CaisseDepotsSubventionDto";
 
 export default class CaisseDepotsDtoAdapter {
-    public static toDemandeSubvention(dto: CaisseDepotsSubvDto): DemandeSubvention {
+    public static toDemandeSubvention(dto: CaisseDepotsSubventionDto): DemandeSubvention {
         const lastUpdateDate = new Date(dto.timestamp);
         const toPV = ProviderValueFactory.buildProviderValueAdapter(caisseDepotsService.provider.name, lastUpdateDate);
         const amount = toPV(dto.fields.montant);
