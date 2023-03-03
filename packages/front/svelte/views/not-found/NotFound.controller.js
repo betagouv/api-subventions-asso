2f6583f489cde2e183d97939d3cc707bd08570c2
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-03-03 12:15:38 +0100

    fix(front): add padding and margin

--- packages/front/svelte/views/not-found/NotFound.controller.js
+++ packages/front/svelte/views/not-found/NotFound.controller.js
@@ -11,7 +11,6 @@ export default class NotFoundController {
     }
 
     openMail() {
-        console.log(window.location);
         window.location = `mailto:${this.app.getContact()}?subject=Page%20non%20trouvée%20&body=Bonjour%2C%0A%0Aje%20n%27arrive%20pas%20%C3%A0%20acc%C3%A9der%20%C3%A0%20la%20page%20%3A%20&html=true`;
     }
 }
