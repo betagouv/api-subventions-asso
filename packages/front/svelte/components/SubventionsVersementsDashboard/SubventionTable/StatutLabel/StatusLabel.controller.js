8c2174b5e77db63644b7023c45ece4d27ccdc17e
alice.telescoop <alice@telescoop.fr>
2023-03-15 12:22:28 +0100

    feat(front): status label badge component & controller

---
+++ packages/front/svelte/components/SubventionsVersementsDashboard/SubventionTable/StatutLabel/StatusLabel.controller.js
@@ -0,0 +1,18 @@
+import { ApplicationStatus } from "@api-subventions-asso/dto";
+
+export class StatusLabelController {
+    static cssClassByLabel = {
+        [ApplicationStatus.GRANTED]: "fr-badge--success",
+        [ApplicationStatus.REFUSED]: "fr-badge--error",
+        [ApplicationStatus.PENDING]: "fr-badge--info",
+        [ApplicationStatus.INELIGIBLE]: "fr-badge--warning fr-badge--beige-gris-galet"
+    };
+
+    /**
+     * @param label: ApplicationStatus
+     */
+    constructor(label) {
+        this.label = label;
+        this.classes = StatusLabelController.cssClassByLabel[label] || "";
+    }
+}
