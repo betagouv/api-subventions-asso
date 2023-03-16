eef731bfdb2f388a7642bd015d8bcb46eb840850
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-03-16 12:01:48 +0100

    fix(dto): revert ResetPasswordErrorCodes removal

--- packages/dto/auth/ResetPasswordDtoResponse.ts
+++ packages/dto/auth/ResetPasswordDtoResponse.ts
@@ -1,5 +1,13 @@
 import { UserWithResetTokenDto } from "../user";
 
+export enum ResetPasswordErrorCodes {
+    RESET_TOKEN_NOT_FOUND = 1,
+    RESET_TOKEN_EXPIRED = 2,
+    USER_NOT_FOUND = 3,
+    PASSWORD_FORMAT_INVALID = 4,
+    INTERNAL_ERROR = 5
+}
+
 export interface ResetPasswordDtoPositiveResponse {
     user: UserWithResetTokenDto;
 }
