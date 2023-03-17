8c4d89a77364d35731df9221288836d358962142
Victor Serain <victor.serain@epitech.eu>
2023-03-17 14:49:39 +0100

    feat(api): add dbo to subvention dto adapter

--- packages/api/src/modules/providers/demarchesSimplifiees/adapters/DemarchesSimplifieesEntityAdapter.ts
+++ packages/api/src/modules/providers/demarchesSimplifiees/adapters/DemarchesSimplifieesEntityAdapter.ts
@@ -13,27 +13,29 @@ export class DemarchesSimplifieesEntityAdapter {
         entity: DemarchesSimplifieesDataEntity,
         mapper: DemarchesSimplifieesMapperEntity
     ): DemandeSubvention {
-        const updatedDate = new Date(entity.demande.dateDerniereModification);
-
+        // Used for decembre month (décembre to decembre)
+        const cleanDate = dateString => new Date(dateString.replace("é", "e"));
         const toPv = ProviderValueFactory.buildProviderValueAdapter(
             demarchesSimplifieesService.provider.name,
-            updatedDate
+            new Date(entity.demande.dateDerniereModification)
         );
 
         const subvention: DefaultObject<ProviderValue> = {
             siret: toPv(entity.siret)
         };
 
-        const cleanDate = dateString => new Date(dateString.replace("é", "e"));
-
         mapper.schema.forEach(property => {
             let value = lodash.get(entity, property.from);
+            if (value === undefined) return;
+            console.log(value);
             if (isString(value) && value.length === 0) return;
             else if (isValidDate(new Date(cleanDate(value)))) value = cleanDate(value);
             else if (!isNaN(parseFloat(value))) value = parseFloat(value);
             lodash.set(subvention, property.to, toPv(value));
         });
-        subvention.annee_demande = toPv((subvention.date_debut.value as Date).getFullYear());
+
+        // Manual assing because not only one year
+        if (subvention.date_debut) subvention.annee_demande = toPv((subvention.date_debut.value as Date).getFullYear());
 
         return subvention as unknown as DemandeSubvention;
     }
