92a8559106c4f8c29374c7b77550d532e3e5a1d2
alice.telescoop <alice@telescoop.fr>
2023-03-21 09:48:35 +0100

    refactor(api): use local helper to convert to provider value

--- packages/api/src/modules/providers/chorus/adapters/ChorusAdapter.ts
+++ packages/api/src/modules/providers/chorus/adapters/ChorusAdapter.ts
@@ -7,88 +7,29 @@ export default class ChorusAdapter {
     static PROVIDER_NAME = "Chorus";
 
     public static toVersement(entity: WithId<ChorusLineEntity>): VersementChorus {
+        const toPv = value =>
+            ProviderValueAdapter.toProviderValue(
+                value,
+                ChorusAdapter.PROVIDER_NAME,
+                entity.indexedInformations.dateOperation
+            );
+        const toPvOrUndefined = value => (value ? toPv(value) : undefined);
         return {
             id: entity._id.toString(),
-            ej: ProviderValueAdapter.toProviderValue(
-                entity.indexedInformations.ej,
-                ChorusAdapter.PROVIDER_NAME,
-                entity.indexedInformations.dateOperation
-            ),
-            versementKey: ProviderValueAdapter.toProviderValue(
-                entity.indexedInformations.ej,
-                ChorusAdapter.PROVIDER_NAME,
-                entity.indexedInformations.dateOperation
-            ),
-            siret: ProviderValueAdapter.toProviderValue(
-                entity.indexedInformations.siret,
-                ChorusAdapter.PROVIDER_NAME,
-                entity.indexedInformations.dateOperation
-            ),
-            amount: ProviderValueAdapter.toProviderValue(
-                entity.indexedInformations.amount,
-                ChorusAdapter.PROVIDER_NAME,
-                entity.indexedInformations.dateOperation
-            ),
-            dateOperation: ProviderValueAdapter.toProviderValue(
-                entity.indexedInformations.dateOperation,
-                ChorusAdapter.PROVIDER_NAME,
-                entity.indexedInformations.dateOperation
-            ),
-            centreFinancier: ProviderValueAdapter.toProviderValue(
-                entity.indexedInformations.centreFinancier,
-                ChorusAdapter.PROVIDER_NAME,
-                entity.indexedInformations.dateOperation
-            ),
-            domaineFonctionnel: ProviderValueAdapter.toProviderValue(
-                entity.indexedInformations.domaineFonctionnel,
-                ChorusAdapter.PROVIDER_NAME,
-                entity.indexedInformations.dateOperation
-            ),
-            codeBranche: ProviderValueAdapter.toProviderValue(
-                entity.indexedInformations.codeBranche,
-                ChorusAdapter.PROVIDER_NAME,
-                entity.indexedInformations.dateOperation
-            ),
-            branche: ProviderValueAdapter.toProviderValue(
-                entity.indexedInformations.branche,
-                ChorusAdapter.PROVIDER_NAME,
-                entity.indexedInformations.dateOperation
-            ),
-            numeroDemandePayment: entity.indexedInformations.numeroDemandePayment
-                ? ProviderValueAdapter.toProviderValue(
-                      entity.indexedInformations.numeroDemandePayment,
-                      ChorusAdapter.PROVIDER_NAME,
-                      entity.indexedInformations.dateOperation
-                  )
-                : undefined,
-            numeroTier: entity.indexedInformations.numeroTier
-                ? ProviderValueAdapter.toProviderValue(
-                      entity.indexedInformations.numeroTier,
-                      ChorusAdapter.PROVIDER_NAME,
-                      entity.indexedInformations.dateOperation
-                  )
-                : undefined,
-            activitee: entity.indexedInformations.activitee
-                ? ProviderValueAdapter.toProviderValue(
-                      entity.indexedInformations.activitee,
-                      ChorusAdapter.PROVIDER_NAME,
-                      entity.indexedInformations.dateOperation
-                  )
-                : undefined,
-            compte: entity.indexedInformations.compte
-                ? ProviderValueAdapter.toProviderValue(
-                      entity.indexedInformations.compte,
-                      ChorusAdapter.PROVIDER_NAME,
-                      entity.indexedInformations.dateOperation
-                  )
-                : undefined,
-            type: entity.indexedInformations.typeOperation
-                ? ProviderValueAdapter.toProviderValue(
-                      entity.indexedInformations.typeOperation,
-                      ChorusAdapter.PROVIDER_NAME,
-                      entity.indexedInformations.dateOperation
-                  )
-                : undefined
+            ej: toPv(entity.indexedInformations.ej),
+            versementKey: toPv(entity.indexedInformations.ej),
+            siret: toPv(entity.indexedInformations.siret),
+            amount: toPv(entity.indexedInformations.amount),
+            dateOperation: toPv(entity.indexedInformations.dateOperation),
+            centreFinancier: toPv(entity.indexedInformations.centreFinancier),
+            domaineFonctionnel: toPv(entity.indexedInformations.domaineFonctionnel),
+            codeBranche: toPv(entity.indexedInformations.codeBranche),
+            branche: toPv(entity.indexedInformations.branche),
+            numeroDemandePayment: toPvOrUndefined(entity.indexedInformations.numeroDemandePayment),
+            numeroTier: toPvOrUndefined(entity.indexedInformations.numeroTier),
+            activitee: toPvOrUndefined(entity.indexedInformations.activitee),
+            compte: toPvOrUndefined(entity.indexedInformations.compte),
+            type: toPvOrUndefined(entity.indexedInformations.typeOperation)
         };
     }
 }
