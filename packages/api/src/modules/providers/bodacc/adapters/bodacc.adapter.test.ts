9b3d39f5a26d49610881529129c16536b5b9c5f1
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-03-23 17:28:54 +0100

    fix(api): mock new Date in bodacc adapter test

--- packages/api/src/modules/providers/bodacc/adapters/bodacc.adapter.test.ts
+++ packages/api/src/modules/providers/bodacc/adapters/bodacc.adapter.test.ts
@@ -2,6 +2,8 @@ import { BodaccDto } from "../dto/BodaccDto";
 import BodaccAdapter from "./bodacc.adapter";
 
 describe("Bodacc Adapter", () => {
+    jest.useFakeTimers().setSystemTime(new Date("2022-01-01"));
+
     const SIREN = "123456789";
     const RECORD = {
         record: {
