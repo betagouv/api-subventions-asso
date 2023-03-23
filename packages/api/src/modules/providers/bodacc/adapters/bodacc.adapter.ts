3e240d3821b1ab079349754a6be259c93349705f
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-03-23 17:25:47 +0100

    feat(api): add bodacc service and adapter

---
+++ packages/api/src/modules/providers/bodacc/adapters/bodacc.adapter.ts
@@ -0,0 +1,15 @@
+import { Association } from "@api-subventions-asso/dto";
+import ProviderValueFactory from "../../../../shared/ProviderValueFactory";
+import bodaccService from "../bodacc.service";
+import { BodaccDto } from "../dto/BodaccDto";
+
+export default class BodaccAdapter {
+    static toAssociation(dto: BodaccDto) {
+        const toPVs = ProviderValueFactory.buildProviderValuesAdapter(bodaccService.provider.name, new Date());
+
+        return {
+            siren: toPVs(dto.records[0].record.fields.registre[1]),
+            bodacc: dto.records.map(record => toPVs(record))
+        } as Association;
+    }
+}
