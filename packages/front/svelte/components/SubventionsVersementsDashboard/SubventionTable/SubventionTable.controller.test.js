0a6cc9a3baad90f62b03f128464d1bbdc15d8e53
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-03-27 12:55:50 +0200

    refactor(front): create SubventionsAdapter to manage subvention display

--- packages/front/svelte/components/SubventionsVersementsDashboard/SubventionTable/SubventionTable.controller.test.js
+++ packages/front/svelte/components/SubventionsVersementsDashboard/SubventionTable/SubventionTable.controller.test.js
@@ -1,6 +1,3 @@
-import * as dataHelper from "../../../helpers/dataHelper";
-
-jest.mock("../../../helpers/dataHelper");
 jest.mock("../helper");
 jest.mock("../../../store/modal.store", () => ({
     modal: { update: jest.fn() },
@@ -9,35 +6,9 @@ jest.mock("../../../store/modal.store", () => ({
 import * as modalStore from "../../../store/modal.store";
 import SubventionTableController from "./SubventionTable.controller";
 import SubventionInfoModal from "@components/SubventionsVersementsDashboard/Modals/SubventionInfoModal.svelte";
+import SubventionsAdapter from "@resources/subventions/subventions.adapter";
 
 describe("SubventionTableController", () => {
-    const ELEMENT = {
-        subvention: [{ dispositif: "ABC DISPOSITIF", serviceInstructeur: "SERVICE INST." }]
-    };
-
-    describe("_extractTableDataFromElement()", () => {
-        const mockGetProjectName = jest.spyOn(SubventionTableController, "getProjectName");
-        beforeAll(() => mockGetProjectName.mockImplementation(name => name));
-        it("should return an object with properties", () => {
-            const expected = [
-                "serviceInstructeur",
-                "dispositif",
-                "projectName",
-                "montantsDemande",
-                "montantsAccorde",
-                "status",
-                "showAmount"
-            ];
-            const actual = Object.keys(SubventionTableController._extractTableDataFromElement(ELEMENT));
-            expect(actual).toEqual(expected);
-        });
-
-        it("should call valueOrHyphen() multiple time", () => {
-            SubventionTableController._extractTableDataFromElement(ELEMENT);
-            expect(dataHelper.valueOrHyphen).toHaveBeenCalledTimes(4);
-        });
-    });
-
     describe("extractHeaders()", () => {
         it("return an array of header", () => {
             const actual = SubventionTableController.extractHeaders();
@@ -46,9 +17,9 @@ describe("SubventionTableController", () => {
     });
 
     describe("extractRows()", () => {
-        const mockExtractTableDataFromElement = jest.spyOn(SubventionTableController, "_extractTableDataFromElement");
+        const mockToSubvention = jest.spyOn(SubventionsAdapter, "toSubvention");
         beforeAll(() =>
-            mockExtractTableDataFromElement.mockImplementation(
+            mockToSubvention.mockImplementation(
                 jest.fn(() => ({
                     serviceInstructeur: undefined,
                     dispositif: undefined,
@@ -58,15 +29,15 @@ describe("SubventionTableController", () => {
                 }))
             )
         );
-        afterAll(() => mockExtractTableDataFromElement.mockRestore());
-        it("should call _extractTableDataFromElement for each element in array", () => {
+        afterAll(() => mockToSubvention.mockRestore());
+        it("should call SubventionsAdapter.toSubvention for each element in array", () => {
             SubventionTableController.extractRows([{ subvention: {} }, { subvention: {} }]);
-            expect(mockExtractTableDataFromElement).toHaveBeenCalledTimes(2);
+            expect(mockToSubvention).toHaveBeenCalledTimes(2);
         });
 
-        it("should not call _extractTableDataFromElement if no subvention", () => {
+        it("should not call SubventionsAdapter.toSubvention if no subvention", () => {
             SubventionTableController.extractRows([{ subvention: {} }, {}]);
-            expect(mockExtractTableDataFromElement).toHaveBeenCalledTimes(1);
+            expect(mockToSubvention).toHaveBeenCalledTimes(1);
         });
 
         it("should return an array", () => {
