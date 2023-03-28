14359c3f2f734096e1fd1922bd7165337581e3b1
alice.telescoop <alice@telescoop.fr>
2023-03-28 16:47:26 +0200

    feat(front): use svelte contact page

--- packages/front/svelte/routes.js
+++ packages/front/svelte/routes.js
@@ -11,6 +11,7 @@ import ForgetPwd from "./views/auth/forget-pwd/ForgetPwd.svelte";
 import Signup from "./views/auth/signup/Signup.svelte";
 import Login from "./views/auth/login/Login.svelte";
 import NotFound from "./views/not-found/NotFound.svelte";
+import Contact from "./views/contact/Contact.svelte";
 
 export default {
     "/404": {
@@ -39,6 +40,10 @@ export default {
     },
     "/": {
         component: () => Home
+    },
+    contact: {
+        component: () => Contact,
+        disableAuth: true
     },
     "auth/signup": {
         component: () => Signup,
