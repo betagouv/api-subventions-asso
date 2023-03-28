416c27adc83d17f0a438b96165af910d17ec7f2b
alice.telescoop <alice@telescoop.fr>
2023-03-28 11:29:09 +0200

    refactor(front): establishment card should not be a card

--- packages/front/svelte/components/EstablishmentPreview/EstablishmentPreviewController.js
+++ packages/front/svelte/components/EstablishmentPreview/EstablishmentPreviewController.js
@@ -2,12 +2,10 @@ import etablissementService from "../../views/etablissement/etablissement.servic
 import { addressToString } from "../../views/association/association.helper";
 import { valueOrHyphen } from "@helpers/dataHelper";
 
-export class EstablismentCardController {
-    constructor(assoName, etab) {
+export class EstablishmentPreviewController {
+    constructor(etab) {
         this.siret = valueOrHyphen(etab.siret);
         this.status = etablissementService.getEtablissementStatus(etab);
-        this.assoName = assoName;
         this.address = valueOrHyphen(addressToString(etab.adresse));
-        this.url = etab.siret ? `/etablissement/${etab.siret}` : undefined;
     }
 }
