224698b59d2591a828c202e5cdaefd8271e59b54
alice.telescoop <alice@telescoop.fr>
2023-03-28 11:33:57 +0200

    style(front): flexible establishment preview component

--- packages/front/svelte/components/EstablishmentPreview/EstablishmentPreview.svelte
+++ packages/front/svelte/components/EstablishmentPreview/EstablishmentPreview.svelte
@@ -2,20 +2,21 @@
     import { EstablishmentPreviewController } from "@components/EstablishmentPreview/EstablishmentPreviewController";
 
     export let establishment;
+    export let small = false;
 
     const { siret, status, address } = new EstablishmentPreviewController(establishment);
 </script>
 
-<p>
+<p class:fr-mb-0={small}>
     {status}
     <br />
     SIRET : {siret}
     <br />
 </p>
-<div class="flex">
-    <span class="fr-col-md-1 fr-mr-1w fr-icon-map-pin-2-line color" />
-    <span class="fr-col-md-11 fr-text--bold">{address}</span>
-</div>
+<p>
+    {#if !small}<span class="fr-mr-1w fr-icon-map-pin-2-line color" />{/if}
+    <span class:fr-text--bold={!small}>{address}</span>
+</p>
 
 <style>
     .color {
