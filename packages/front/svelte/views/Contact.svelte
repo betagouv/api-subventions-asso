ea348f75ee1043d63d2c2c3e323559560814f6bd
alice.telescoop <alice@telescoop.fr>
2023-03-28 17:42:55 +0200

    refactor(front): lighter contact component

---
+++ packages/front/svelte/views/Contact.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { getContext } from "svelte";
+
+    const { getContact } = getContext("app");
+</script>
+
+<h1>Contactez-nous</h1>
+
+<p>
+    Pour nous contacter, vous pouvez envoyer un message à l'adresse
+    <a title="Contactez-nous" href="mailto:{getContact()}" target="_blank" rel="noopener noreferrer">{getContact()}</a>
+    .
+</p>
