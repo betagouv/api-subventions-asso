669fdbb2c14ef0187024c06248273f832519b36c
alice.telescoop <alice@telescoop.fr>
2023-03-28 16:51:10 +0200

    feat(front): contact page breadcrumbs

--- packages/front/svelte/services/router.service.js
+++ packages/front/svelte/services/router.service.js
@@ -9,6 +9,7 @@ import {
 export const buildBreadcrumbs = path => {
     const crumbs = [];
     if (path.includes("404")) crumbs.push({ label: "Page introuvable" });
+    if (path.includes("contact")) crumbs.push({ label: "Contactez-nous" });
     if (path.includes("association")) crumbs.push({ label: `Association (${path.split("/").at(-1)})` });
     if (path.includes("etablissement")) crumbs.push({ label: `Établissement (${path.split("/").at(-1)})` });
     else if (path.includes("admin")) {
