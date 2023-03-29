49ead5a2076ab3889b01b862fd07b33696b7a70c
alice.telescoop <alice@telescoop.fr>
2023-03-29 17:38:01 +0200

    chore(front): remove unused import & spelling

--- packages/front/svelte/resources/subventions/subventions.adapter.test.js
+++ packages/front/svelte/resources/subventions/subventions.adapter.test.js
@@ -1,7 +1,6 @@
 import SubventionsAdapter from "./subventions.adapter";
 import * as dataHelper from "@helpers/dataHelper";
 jest.mock("@helpers/dataHelper");
-import * as textHelper from "@helpers/textHelper";
 jest.mock("@helpers/textHelper", () => ({
     capitalizeFirstLetter: value => value
 }));
@@ -39,7 +38,7 @@ describe("Subventions Adapter", () => {
     });
 
     describe("_getProjectName", () => {
-        it("return concatened names", () => {
+        it("return concatenated names", () => {
             const expected = "A. - B. - C.";
             const actual = SubventionsAdapter._getProjectName(SUBVENTION);
             expect(actual).toEqual(expected);
