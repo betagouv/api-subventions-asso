e1387d6592693a77755583afaf88e84f8f800165
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-03-29 11:34:55 +0200

    fix(front): use getLastVersementsDate from helper

--- packages/front/svelte/resources/versements/versements.adapter.js
+++ packages/front/svelte/resources/versements/versements.adapter.js
@@ -1,29 +1,17 @@
 import { valueOrHyphen, numberToEuro } from "@helpers/dataHelper";
 import { withTwoDigitYear } from "@helpers/dateHelper";
+import { getLastVersementsDate } from "@components/SubventionsVersementsDashboard/helper";
 
 export default class VersementsAdapter {
     static toVersement(versements) {
         return {
             totalAmount: numberToEuro(this._countTotalVersement(versements)),
             centreFinancier: valueOrHyphen(versements[0]?.centreFinancier),
-            lastVersementDate: valueOrHyphen(withTwoDigitYear(this._getLastVersementsDate(versements)))
+            lastVersementDate: valueOrHyphen(withTwoDigitYear(getLastVersementsDate(versements)))
         };
     }
 
     static _countTotalVersement(versements) {
         return versements.reduce((acc, versement) => acc + versement.amount, 0);
     }
-
-    static _getLastVersementsDate = versements => {
-        const orderedVersements = versements.sort((versementA, versementB) => {
-            const dateA = new Date(versementA.dateOperation);
-            const dateB = new Date(versementB.dateOperation);
-
-            return dateB.getTime() - dateA.getTime();
-        });
-
-        if (!orderedVersements.length) return null;
-
-        return new Date(orderedVersements[0].dateOperation);
-    };
 }
