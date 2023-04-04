919023b44516f7a4fb40fb18260164db781cb68b
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-04-04 12:21:49 +0200

    feat(front): display bop in application table

--- packages/front/svelte/resources/versements/versements.adapter.test.js
+++ packages/front/svelte/resources/versements/versements.adapter.test.js
@@ -17,6 +17,7 @@ describe("Versements Adapter", () => {
 
     describe("toVersement()", () => {
         const mockCountTotalVersement = jest.spyOn(VersementsAdapter, "_countTotalVersement");
+        const mockFormatBop = jest.spyOn(VersementsAdapter, "_formatBop");
 
         const mocks = [mockCountTotalVersement];
 
@@ -26,7 +27,7 @@ describe("Versements Adapter", () => {
 
         it("should return an object with properties", () => {
             const actual = Object.keys(VersementsAdapter.toVersement(VERSEMENTS));
-            expect(actual).toEqual(["totalAmount", "centreFinancier", "lastVersementDate"]);
+            expect(actual).toEqual(["totalAmount", "centreFinancier", "lastVersementDate", "bop"]);
         });
 
         it("should call getLastVersementsDate()", () => {
@@ -39,9 +40,14 @@ describe("Versements Adapter", () => {
             expect(mockCountTotalVersement).toHaveBeenCalledTimes(1);
         });
 
-        it("should call valueOrHyphen() twice", () => {
+        it("should call _formatBop()", () => {
             VersementsAdapter.toVersement(VERSEMENTS);
-            expect(dataHelper.valueOrHyphen).toHaveBeenCalledTimes(2);
+            expect(mockFormatBop).toHaveBeenCalledTimes(1);
+        });
+
+        it("should call valueOrHyphen() 3 times", () => {
+            VersementsAdapter.toVersement(VERSEMENTS);
+            expect(dataHelper.valueOrHyphen).toHaveBeenCalledTimes(3);
         });
 
         it("should call withTwoYearDigit() ", () => {
@@ -60,6 +66,29 @@ describe("Versements Adapter", () => {
         it("return sum of versements", () => {
             const expected = 100;
             const actual = VersementsAdapter._countTotalVersement(VERSEMENTS);
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    describe("_fromatBop()", () => {
+        it("should remove first character", () => {
+            const BOP = "0163";
+            const expected = "163";
+            const actual = VersementsAdapter._formatBop(BOP);
+            expect(actual).toEqual(expected);
+        });
+
+        it("should return untouched bop", () => {
+            const BOP = 1267;
+            const expected = BOP;
+            const actual = VersementsAdapter._formatBop(BOP);
+            expect(actual).toEqual(expected);
+        });
+
+        it("should return undefined", () => {
+            const BOP = undefined;
+            const expected = undefined;
+            const actual = VersementsAdapter._formatBop(BOP);
             expect(actual).toEqual(expected);
         });
     });
