1f89eaa7033385bdbeeacb0012597967435e13a8
alice.telescoop <alice@telescoop.fr>
2023-04-05 13:09:37 +0200

    feat(api): new cron scheduler example

--- packages/api/src/modules/example/interfaces/cron/example.cron.controller.ts
+++ packages/api/src/modules/example/interfaces/cron/example.cron.controller.ts
@@ -1,19 +1,32 @@
-import { AsyncIntervalCron, IntervalCron } from "../../../../decorators/cronController.decorator";
+import { AsyncCron, AsyncIntervalCron, Cron, IntervalCron } from "../../../../decorators/cronController.decorator";
 import { CronController } from "../../../../@types/cron";
 
 export class ExampleCronController implements CronController {
     name = "cronExample";
     static testParity = 0;
 
-    @IntervalCron({ seconds: 10 }, false)
-    test() {
+    @IntervalCron({ minutes: 1 }, false)
+    intervalTest() {
         console.log("hello world");
     }
 
-    @AsyncIntervalCron({ seconds: 10 }, false)
-    asyncTest() {
+    @AsyncIntervalCron({ minutes: 1 }, false)
+    asyncIntervalTest() {
         ExampleCronController.testParity += 1;
         if (ExampleCronController.testParity % 2) return Promise.reject("odd number: task fails");
+        console.log("even number: test successes");
+        return Promise.resolve();
+    }
+
+    @Cron({ cronExpression: "* * * * *" })
+    test() {
+        console.log("good morning world");
+    }
+
+    @AsyncCron({ cronExpression: "* * * * *" })
+    asyncTest() {
+        ExampleCronController.testParity += 1;
+        if (ExampleCronController.testParity % 2) return Promise.reject("odd number: cron task fails");
         console.log("even number: test successes");
         return Promise.resolve();
     }
