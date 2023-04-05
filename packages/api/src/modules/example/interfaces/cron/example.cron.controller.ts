65dca7a5ce502b216d5f3c1d3dfb55e08b0095ca
alice.telescoop <alice@telescoop.fr>
2023-04-05 12:47:05 +0200

    feat(api): new cron scheduler format

--- packages/api/src/modules/example/interfaces/cron/example.cron.controller.ts
+++ packages/api/src/modules/example/interfaces/cron/example.cron.controller.ts
@@ -1,16 +1,16 @@
-import { AsyncCron, Cron } from "../../../../decorators/cronController.decorator";
+import { AsyncIntervalCron, IntervalCron } from "../../../../decorators/cronController.decorator";
 import { CronController } from "../../../../@types/cron";
 
 export class ExampleCronController implements CronController {
     name = "cronExample";
     static testParity = 0;
 
-    @Cron({ seconds: 10 }, false)
+    @IntervalCron({ seconds: 10 }, false)
     test() {
         console.log("hello world");
     }
 
-    @AsyncCron({ seconds: 10 }, false)
+    @AsyncIntervalCron({ seconds: 10 }, false)
     asyncTest() {
         ExampleCronController.testParity += 1;
         if (ExampleCronController.testParity % 2) return Promise.reject("odd number: task fails");
