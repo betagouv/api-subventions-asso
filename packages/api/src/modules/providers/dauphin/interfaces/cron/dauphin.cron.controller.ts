eb10896c0df3d107e4e255b63627359e7544a278
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-04-20 10:12:31 +0200

    feat(api): create dauphin cron

---
+++ packages/api/src/modules/providers/dauphin/interfaces/cron/dauphin.cron.controller.ts
@@ -0,0 +1,13 @@
+import { AsyncCron } from "../../../../../decorators/cronController.decorator";
+import { CronController } from "../../../../../@types/cron";
+import dauphinService from "../../dauphin.service";
+
+export class DauphinCronController implements CronController {
+    name = "dauphinCron";
+
+    // every day at 20PM
+    @AsyncCron({ cronExpression: "0 20 * * *" })
+    updateDauphinCache() {
+        return dauphinService.fetchAndSaveApplicationsFromDate(new Date());
+    }
+}
