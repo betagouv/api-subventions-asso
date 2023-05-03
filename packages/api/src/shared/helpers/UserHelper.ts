e20e324dd5e28d138abe65eba6aeb0387e9d9e91
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-05-03 10:35:51 +0200

    feat(api): use service instead of repository in helper

--- packages/api/src/shared/helpers/UserHelper.ts
+++ packages/api/src/shared/helpers/UserHelper.ts
@@ -1,12 +1,12 @@
 import { WithId } from "mongodb";
 import UserDto from "@api-subventions-asso/dto/user/UserDto";
 import UserDbo from "../../modules/user/repositories/dbo/UserDbo";
-import statsAssociationsVisitRepository from "../../modules/stats/repositories/statsAssociationsVisit.repository";
+import statsService from "../../modules/stats/stats.service";
 import { ONE_DAY_MS } from "./DateHelper";
 
 export const isUserActif = async (user: WithId<UserDbo> | UserDbo | UserDto) => {
     const INACTIVE_THRESHOLD = 7 * ONE_DAY_MS;
-    const lastSearchDate = await statsAssociationsVisitRepository.getLastSearchDate(user._id);
+    const lastSearchDate = await statsService.getUserLastSearchDate(user._id);
     if (!lastSearchDate) return false;
 
     return Date.now() - lastSearchDate.getTime() <= INACTIVE_THRESHOLD;
