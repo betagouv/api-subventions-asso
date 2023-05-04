5feb9c2924a5d0ffecf773e21039ab4f1490f895
Victor Serain <victor.serain@epitech.eu>
2023-05-04 16:28:24 +0200

    Revert "feat(api): add database management"

--- packages/api/src/modules/providers/fonjep/repositories/fonjep.versement.repository.ts
+++ packages/api/src/modules/providers/fonjep/repositories/fonjep.versement.repository.ts
@@ -1,16 +1,9 @@
 import { Siren, Siret } from "@api-subventions-asso/dto";
-import { Collection } from "mongodb";
 import MigrationRepository from "../../../../shared/MigrationRepository";
 import FonjepVersementEntity from "../entities/FonjepVersementEntity";
 
 export class FonjepVersementRepository extends MigrationRepository<FonjepVersementEntity> {
     readonly collectionName = "fonjepVersement";
-    private tmpCollectionEnabled = false;
-
-    async createIndexes() {
-        await this.collection.createIndex({ "indexedInformations.code_poste": 1 });
-        await this.collection.createIndex({ "legalInformations.siret": 1 });
-    }
 
     create(entity: FonjepVersementEntity) {
         return this.collection.insertOne(entity);
@@ -30,31 +23,6 @@ export class FonjepVersementRepository extends MigrationRepository<FonjepVerseme
                 "legalInformations.siret": new RegExp(`^${siren}\\d{5}`),
             })
             .toArray();
-    }
-
-    useTemporyCollection(active) {
-        this.tmpCollectionEnabled = active;
-    }
-
-    async applyTemporyCollection() {
-        this.useTemporyCollection(false);
-        await this.collection.rename(this.collectionName + "-OLD");
-        this.useTemporyCollection(true);
-        await this.collection.rename(this.collectionName);
-
-        this.useTemporyCollection(false);
-
-        await this.createIndexes();
-
-        await this.db.collection(this.collectionName + "-OLD").drop();
-    }
-
-    protected get collection(): Collection<FonjepVersementEntity> {
-        if (this.tmpCollectionEnabled) {
-            return this.db.collection<FonjepVersementEntity>(this.collectionName + "-tmp-collection");
-        }
-
-        return super.collection;
     }
 }
 
