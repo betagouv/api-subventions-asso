1f0c492a1ca9da679f975e20f24db14cda737aa3
alice.telescoop <alice@telescoop.fr>
2023-05-15 14:38:59 +0200

    fix(api): workaround tsoa typing error

--- packages/api/src/modules/documents/interfaces/http/documentController.ts
+++ packages/api/src/modules/documents/interfaces/http/documentController.ts
@@ -1,4 +1,3 @@
-import { IncomingMessage } from "http";
 import { Route, Get, Controller, Tags, Security, Response } from "tsoa";
 import { HttpErrorInterface } from "../../../../shared/errors/httpErrors/HttpError";
 import documentService from "../../documents.service";
@@ -13,7 +12,8 @@ export class DocumentController extends Controller {
      */
     @Get("dauphin/{encodedDocPath}")
     @Response<HttpErrorInterface>("404")
-    public async getDauphinDocument(encodedDocPath): Promise<IncomingMessage> {
+    // tsoa workaround https://github.com/lukeautry/tsoa/issues/340#issuecomment-518229063
+    public async getDauphinDocument(encodedDocPath): Promise<unknown> {
         const stream = await documentService.getDauphinDocumentStream(decodeURIComponent(encodedDocPath));
         this.setHeader("Content-Type", stream.headers["content-type"] || "application/octet-stream");
         this.setHeader("Content-Disposition", "inline");
