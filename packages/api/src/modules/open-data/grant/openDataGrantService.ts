9c6710bc02816d6fc3dd8ddbb72077ae3af43ce3
alice.telescoop <alice@telescoop.fr>
2023-06-01 11:00:50 +0200

    feat(api): open data grant routes

---
+++ packages/api/src/modules/open-data/grant/openDataGrantService.ts
@@ -0,0 +1,12 @@
+import { StructureIdentifiers } from "../../../@types";
+import grantService from "../../grant/grant.service";
+
+class OpenDataGrantService {
+    getByStructure(identifier: StructureIdentifiers) {
+        return grantService.getCommonGrants(identifier);
+    }
+}
+
+const openDataGrantService = new OpenDataGrantService();
+
+export default openDataGrantService;
