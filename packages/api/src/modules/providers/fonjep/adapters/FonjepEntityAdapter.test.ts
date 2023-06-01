7326d61b4ce00e269a7e542efa6647448ec1cd00
alice.telescoop <alice@telescoop.fr>
2023-06-01 13:59:15 +0200

    test(api): fonjep adapter common dto

--- packages/api/src/modules/providers/fonjep/adapters/FonjepEntityAdapter.test.ts
+++ packages/api/src/modules/providers/fonjep/adapters/FonjepEntityAdapter.test.ts
@@ -21,4 +21,22 @@ describe("FonjepEntityAdapter", () => {
             expect(actual).toMatchSnapshot();
         });
     });
+
+    describe("toCommon", () => {
+        it("returns proper result", () => {
+            const INPUT = {
+                indexedInformations: {
+                    date_versement: new Date("2022-02-02"),
+                    dispositif: "DISPOSITIF",
+                    annee_demande: 2023,
+                    montant_paye: 42,
+                    service_instructeur: "INSTRUCTEUR",
+                },
+                legalInformations: { siret: "123456789" },
+                data: { MontantSubvention: 43 },
+            };
+            const actual = FonjepEntityAdapter.toCommon(INPUT);
+            expect(actual).toMatchSnapshot();
+        });
+    });
 });
