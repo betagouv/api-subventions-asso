ac239413d4d95310a04b35b8d42e6a92e495a2e5
alice.telescoop <alice@telescoop.fr>
2023-06-05 14:11:57 +0200

    refactor(api): extract adaptation code demarches simplifiees

--- packages/api/src/modules/providers/demarchesSimplifiees/adapters/DemarchesSimplifieesEntityAdapter.ts
+++ packages/api/src/modules/providers/demarchesSimplifiees/adapters/DemarchesSimplifieesEntityAdapter.ts
@@ -10,6 +10,30 @@ import { DefaultObject } from "../../../../@types";
 import { stringIsFloat } from "../../../../shared/helpers/StringHelper";
 
 export class DemarchesSimplifieesEntityAdapter {
+    private static mapSchema<T>(
+        entity: DemarchesSimplifieesDataEntity,
+        mapper: DemarchesSimplifieesMapperEntity,
+        schemaId: string,
+    ): T {
+        const subvention = { siret: entity.siret };
+
+        mapper[schemaId].forEach(property => {
+            let value = lodash.get(entity, property.from);
+            const valueDate = [
+                moment(value, "DD MMMM YYYY", "fr", true).toDate(), // Use moment for read French date
+                moment(value, true), // use moment with strict params true, to force the reading of a date in js format and not an interpretation
+            ].find(date => isValidDate(date));
+
+            if (value === undefined || value === "") return;
+            else if (stringIsFloat(value)) value = parseFloat(value);
+            else value = valueDate || value;
+
+            lodash.set(subvention, property.to, value);
+        });
+
+        return subvention as unknown as T;
+    }
+
     static toSubvention(
         entity: DemarchesSimplifieesDataEntity,
         mapper: DemarchesSimplifieesMapperEntity,
@@ -19,29 +43,13 @@ export class DemarchesSimplifieesEntityAdapter {
             new Date(entity.demande.dateDerniereModification),
         );
 
-        const subvention: DefaultObject<ProviderValue> = {
-            siret: toPv(entity.siret),
-        };
-
-        mapper.schema.forEach(property => {
-            let value = lodash.get(entity, property.from);
-            const valueDate = [
-                moment(value, "DD MMMM YYYY", "fr", true).toDate(), // Use moment for read french date
-                moment(value, true) // use moment with strict params true, to force the reading of a date in js format and not an interpretation
-            ].find(date =>
-                isValidDate(date),
-            );
-
-            if (value === undefined || value === "") return;
-            else if (stringIsFloat(value)) value = parseFloat(value);
-            else value = valueDate || value;
-
-            lodash.set(subvention, property.to, toPv(value));
-        });
+        const subvention: DefaultObject = DemarchesSimplifieesEntityAdapter.mapSchema(entity, mapper, "schema");
 
         // DS doesn't have an attribute with only year, so we get year from the start date
-        if (subvention.date_debut && subvention.date_debut.value && isValidDate(subvention.date_debut.value))
-            subvention.annee_demande = toPv((subvention.date_debut.value as Date).getFullYear());
+        if (subvention.date_debut && subvention.date_debut && isValidDate(subvention.date_debut))
+            subvention.annee_demande = (subvention.date_debut as Date).getFullYear();
+
+        Object.keys(subvention).map(key => (subvention[key] = toPv(subvention[key])));
 
         return subvention as unknown as DemandeSubvention;
     }
