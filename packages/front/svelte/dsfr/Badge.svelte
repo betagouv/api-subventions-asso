216ecd29b9da4e9998ab90584b501bf091051cdc
alice.telescoop <alice@telescoop.fr>
2023-06-08 14:28:46 +0200

    feat(front): dsfr badge and badgeGroup svelte component

---
+++ packages/front/svelte/dsfr/Badge.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let type = false; // "success" | "error" | "info" | "warning" | "new"
+    export let noIcon = false;
+    export let small = false;
+    export let color = false;
+    export let label = "";
+
+    let otherClasses = "fr-badge ";
+
+    if (type) otherClasses += `fr-badge--${type} `;
+    if (color) otherClasses += `fr-badge--${color} `;
+</script>
+
+<p class={otherClasses} class:fr-badge--sm={small} class:fr-badge--no-icon={noIcon}>{label}</p>
