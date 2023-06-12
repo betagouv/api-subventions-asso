dc6966db2d8daecd49e9156c15cef805bfc4a4cc
alice.telescoop <alice@telescoop.fr>
2023-06-12 15:54:14 +0200

    feat(api,front): set XFO header prevent being in frame

--- packages/api/src/middlewares/headersMiddleware.ts
+++ packages/api/src/middlewares/headersMiddleware.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from "express";
 const headers = {
     "Content-Security-Policy": "default-src 'none'",
     "Strict-Transport-Security": "max-age=63072000; includeSubDomains; preload",
+    "X-Frame-Options": "DENY",
 };
 
 export const headersMiddleware = (req: Request, res: Response, next: NextFunction) => {
--- packages/front/src/middlewares/headers.middleware.ts
+++ packages/front/src/middlewares/headers.middleware.ts
@@ -20,6 +20,7 @@ function toCspHeaderValue(dict: Record<string, string>) {
 const headers = {
     "Content-Security-Policy": toCspHeaderValue(cspHeaderValueByDirective),
     "Strict-Transport-Security": "max-age=63072000; includeSubDomains; preload",
+    "X-Frame-Options": "DENY",
 };
 
 export const headersMiddleware = (req: Request, res: Response, next: NextFunction) => {
