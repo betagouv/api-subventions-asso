574c21c92a9d48432115dba6aec85d203da68957
alice.telescoop <alice@telescoop.fr>
2023-06-12 15:41:22 +0200

    feat(api): csp header

--- packages/api/src/middlewares/headersMiddleware.ts
+++ packages/api/src/middlewares/headersMiddleware.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
-const headers = {};
+const headers = {
+    "Content-Security-Policy": "default-src 'none'",
+};
 
 export const headersMiddleware = (req: Request, res: Response, next: NextFunction) => {
     for (const [headerKey, headerValue] of Object.entries(headers)) {
