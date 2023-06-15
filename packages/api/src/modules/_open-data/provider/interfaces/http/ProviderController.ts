965df9751ffcbdacf55b3d6cf9db9dab0ea1eff5
alice.telescoop <alice@telescoop.fr>
2023-06-15 16:44:11 +0200

    feat(api): french open-data provider route

--- packages/api/src/modules/_open-data/provider/interfaces/http/ProviderController.ts
+++ packages/api/src/modules/_open-data/provider/interfaces/http/ProviderController.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Route, Tags } from "tsoa";
 import ProvidersInfos from "../../entities/ProvidersInfos";
 import providerService from "../../provider.service";
 
-@Route("open-data/providers")
+@Route("open-data/fournisseurs")
 @Tags("Open Data")
 export class ProviderController extends Controller {
     @Get("/")
