e5c50476cc73ed83f24f0ab1d7ea620750d8ead8
alice.telescoop <alice@telescoop.fr>
2023-06-21 17:58:02 +0200

    feat(api): etalab scdl dbo

---
+++ packages/api/src/modules/providers/datagouv/@types/ScdlEtalabDbo.ts
@@ -0,0 +1,22 @@
+import { Siret } from "@api-subventions-asso/dto";
+import { ObjectId } from "mongodb";
+
+export type ScdlEtalabDbo = {
+    // in English as it is internal code
+    _id: ObjectId;
+    allocatorName: string;
+    allocatorSiret: string;
+    conventionDate: Date;
+    decisionReference: string;
+    associationName: string;
+    associationSiret: Siret;
+    object: string;
+    amount: number; // granted amount
+    paymentConditions: string;
+    paymentStartDate: Date;
+    paymentEndDate: Date;
+    idRAE?: string;
+    UeNotification: boolean;
+    grantPercentage: number;
+    extractId: ObjectId;
+};
