4850a09a074101db93924f607b5a936e96edb341
alice.telescoop <alice@telescoop.fr>
2023-06-26 17:01:39 +0200

    feat(api): simpler scdl parser

--- packages/api/src/modules/providers/datagouv/dataGouvScdlGrantParser.ts
+++ packages/api/src/modules/providers/datagouv/dataGouvScdlGrantParser.ts
@@ -1,63 +1,32 @@
-import fs from "fs";
 import { ObjectId } from "mongodb";
 import * as ParseHelper from "../../../shared/helpers/ParserHelper";
 import { isSiret } from "../../../shared/Validators";
 import { isEmptyRow } from "../../../shared/helpers/ParserHelper";
-import { SaveCallback } from "./@types";
 import { ScdlGrantDbo } from "./@types/ScdlGrantDbo";
 import ScdlGrantEntity from "./entities/ScdlGrantEntity";
 
 export default class DataGouvScdlGrantParser {
-    static parseCsv(filePath: string, save: SaveCallback<ScdlGrantDbo>, extractId: ObjectId): Promise<void> {
-        return new Promise((resolve, reject) => {
-            let totalEntities = 0;
-            let header: null | string[] = null;
+    static parseCsv(chunk: Buffer, extractId: ObjectId) {
+        let parsedChunk = ParseHelper.csvParse(chunk as Buffer);
+        const header: string[] = parsedChunk[0];
+        parsedChunk = parsedChunk.slice(1);
 
-            const stream = fs.createReadStream(filePath);
+        const storableChunk: ScdlGrantDbo[] = [];
 
-            const streamPause = () => stream.pause();
-            const streamResume = () => stream.resume();
+        for (const csvRow of parsedChunk) {
+            if (isEmptyRow(csvRow)) continue;
 
-            let logNumber = 1;
-            let logTime = new Date();
+            const parsedData = ParseHelper.linkHeaderToData(header, csvRow);
 
-            stream.on("data", async chunk => {
-                let parsedChunk = ParseHelper.csvParse(chunk as Buffer);
+            const storableData = ParseHelper.indexDataByPathObject(
+                ScdlGrantEntity.InformationsPath,
+                parsedData,
+            ) as unknown as ScdlGrantDbo;
 
-                if (totalEntities > 500000 * logNumber) {
-                    logNumber++;
-                    console.log(`\n ${(new Date().getTime() - logTime.getTime()) / 1000} sec`);
-                    logTime = new Date();
-                }
-
-                if (!header) {
-                    header = parsedChunk[0];
-                    parsedChunk = parsedChunk.slice(1);
-                }
-
-                for (const csvRow of parsedChunk) {
-                    if (isEmptyRow(csvRow)) return;
-
-                    totalEntities++;
-                    const parsedData = ParseHelper.linkHeaderToData(header as string[], csvRow);
-
-                    const storableData = ParseHelper.indexDataByPathObject(
-                        ScdlGrantEntity.InformationsPath,
-                        parsedData,
-                    ) as unknown as ScdlGrantDbo;
-
-                    if (!storableData.associationSiret || !isSiret(storableData.associationSiret)) return;
-                    storableData.extractId = extractId;
-
-                    await save(storableData, streamPause, streamResume);
-                }
-            });
-
-            stream.on("error", err => reject(err));
-
-            stream.on("end", () => {
-                resolve();
-            });
-        });
+            if (!storableData.associationSiret || !isSiret(storableData.associationSiret)) continue;
+            storableData.extractId = extractId;
+            storableChunk.push(storableData);
+        }
+        return storableChunk;
     }
 }
