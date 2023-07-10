6e948af2077e2ce3fd9c6e1dfebec8c61c2ba734
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-07-10 11:59:50 +0200

    feat(front): multi step form static

---
+++ packages/front/svelte/components/MultiStepForm/ExampleMultiStepForm.svelte
@@ -0,0 +1,12 @@
+<script>
+    import MultiStepForm from "./MultiStepForm.svelte";
+    import Step1 from "./Step1/Step1.svelte";
+
+    const steps = [Step1];
+</script>
+
+<MultiStepForm {steps} />
+
+<style>
+    /* your styles go here */
+</style>
