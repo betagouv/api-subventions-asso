edf204d8657ca73184811d11dc7032690cebae43
Victor Serain <victor.serain@epitech.eu>
2023-07-11 15:57:01 +0200

    feat(front): add modal on user deleted action

---
+++ packages/front/svelte/views/user/profile/components/ConfirmDeleteUserModal.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { action } from "../../../../store/modal.store";
+
+    import Alert from "../../../../dsfr/Alert.svelte";
+    import Button from "../../../../dsfr/Button.svelte";
+    import ModalContent from "../../../../dsfr/ModalContent.svelte";
+
+    const deleteUser = () => {
+        action.value();
+    };
+
+    let temporyDisabled = true;
+
+    setTimeout(() => (temporyDisabled = false), 2000);
+</script>
+
+<ModalContent title="Souhaitez-vous vraiment supprimer votre compte ?">
+    <div class="fr-container">
+        <Alert>
+            Cette action est définitive, pour réutiliser Data.Subvention, vous devrez créer un nouveau compte.
+        </Alert>
+        <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--center">
+            <Button on:click={deleteUser} ariaControls="fr-modal" disabled={temporyDisabled} type="tertiary">
+                Je comfirme la suppression de mon compte
+            </Button>
+            <Button styleClass="fr-col-offset-1" ariaControls="fr-modal">Annuler</Button>
+        </div>
+    </div>
+</ModalContent>
