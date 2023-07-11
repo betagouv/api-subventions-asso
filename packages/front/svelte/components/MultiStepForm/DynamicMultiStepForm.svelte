3894feb2b63efe8cee508049eb16a6d6e85ef521
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-07-11 07:38:33 +0200

    feat(front): make the form dynamic

---
+++ packages/front/svelte/components/MultiStepForm/DynamicMultiStepForm.svelte
@@ -0,0 +1,18 @@
+<script>
+    import MultiStepForm from "./MultiStepForm.svelte";
+    import Step1 from "./Steps/Step1.svelte";
+    import Step2 from "./Steps/Step2.svelte";
+    import Step3 from "./Steps/Step3.svelte";
+
+    function onSubmit(values) {
+        console.log("multi step submit", values);
+    }
+</script>
+
+Dynamic Multi Step Form
+
+<MultiStepForm steps={[Step1, Step2, Step3]} {onSubmit} />
+
+<style>
+    /* your styles go here */
+</style>
---
+++ packages/front/svelte/components/MultiStepForm/Steps/Step1.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Input from "@dsfr/Input.svelte";
+
+    export let values = {
+        firstname: "",
+        lastname: "",
+    };
+</script>
+
+{Object.values(values)}
+<Input label="Firstname" bind:value={values.firstname} />
+<Input label="Lastname" bind:value={values.lastname} />
+
+<style>
+</style>
