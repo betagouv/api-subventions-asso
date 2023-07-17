d80a72705adcd5d07f3a7bbbf27e6d1a84435945
alice.telescoop <alice@telescoop.fr>
2023-07-17 14:00:03 +0200

    refactor(front): use request service in association port

--- packages/front/svelte/resources/associations/association.port.test.js
+++ packages/front/svelte/resources/associations/association.port.test.js
@@ -1,47 +1,47 @@
-import axios from "axios";
 import associationPort from "./association.port";
+import requestsService from "@services/requests.service";
 
-jest.mock("axios");
+jest.mock("@services/requests.service");
 
 describe("AssociationPort", () => {
-    axios.get.mockImplementation(() => ({ catch: jest.fn() }));
+    requestsService.get.mockImplementation(() => ({ catch: jest.fn() }));
     const SIREN = "SIREN";
 
     describe("incExtractData", () => {
-        it("should call axios with association in path", async () => {
+        it("should call requestsService with association in path", async () => {
             const expected = `/association/${SIREN}/extract-data`;
             await associationPort.incExtractData(SIREN);
-            expect(axios.get).toHaveBeenCalledWith(expected);
+            expect(requestsService.get).toHaveBeenCalledWith(expected);
         });
     });
 
     describe("getByIdentifier", () => {
-        it("calls axios get", async () => {
+        it("calls requestsService get", async () => {
             const expected = `/association/${SIREN}`;
             await associationPort.getByIdentifier(SIREN);
-            expect(axios.get).toHaveBeenCalledWith(expected);
+            expect(requestsService.get).toHaveBeenCalledWith(expected);
         });
 
-        it("return association from axios result", async () => {
+        it("return association from requestsService result", async () => {
             const expected = "";
             const RES = { data: { association: expected } };
-            axios.get.mockResolvedValueOnce(RES);
+            requestsService.get.mockResolvedValueOnce(RES);
             const actual = await associationPort.getByIdentifier(SIREN);
             expect(actual).toBe(expected);
         });
     });
 
     describe("search", () => {
-        it("calls axios get", async () => {
+        it("calls requestsService get", async () => {
             const expected = `/search/associations/${SIREN}`;
             await associationPort.search(SIREN);
-            expect(axios.get).toHaveBeenCalledWith(expected);
+            expect(requestsService.get).toHaveBeenCalledWith(expected);
         });
 
-        it("return association list from axios result", async () => {
+        it("return association list from requestsService result", async () => {
             const expected = [];
             const RES = { data: { result: expected } };
-            axios.get.mockResolvedValueOnce(RES);
+            requestsService.get.mockResolvedValueOnce(RES);
             const actual = await associationPort.search(SIREN);
             expect(actual).toBe(expected);
         });
