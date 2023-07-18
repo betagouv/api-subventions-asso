ba8cb8520e86ad0c06ef743835bc42fdb959524e
alice.telescoop <alice@telescoop.fr>
2023-07-18 10:54:55 +0200

    refactor(front): requestsService in document port

--- packages/front/svelte/resources/documents/documents.port.test.js
+++ packages/front/svelte/resources/documents/documents.port.test.js
@@ -1,23 +1,23 @@
-import axios from "axios";
 import documentPort from "./documents.port";
+import requestsService from "@services/requests.service";
 
-jest.mock("axios");
+jest.mock("@services/requests.service");
 
 describe("DocumentsPort", () => {
-    beforeAll(() => axios.get.mockImplementation(() => Promise.resolve({ data: "" })));
+    beforeAll(() => requestsService.get.mockImplementation(() => Promise.resolve({ data: "" })));
     const DOC_URL = "/path";
 
     describe("search", () => {
-        it("calls axios get", async () => {
+        it("calls requestsService get", async () => {
             const expected = [DOC_URL, { responseType: "blob" }];
             await documentPort.getDauphinBlob(DOC_URL);
-            expect(axios.get).toHaveBeenCalledWith(...expected);
+            expect(requestsService.get).toHaveBeenCalledWith(...expected);
         });
 
-        it("return documents list from axios result", async () => {
+        it("return documents list from requestsService result", async () => {
             const expected = [];
             const RES = { data: expected };
-            axios.get.mockResolvedValueOnce(RES);
+            requestsService.get.mockResolvedValueOnce(RES);
             const actual = await documentPort.getDauphinBlob(DOC_URL);
             expect(actual).toBe(expected);
         });
