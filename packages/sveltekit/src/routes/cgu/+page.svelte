c8f28f2dfaa5b410adbb15f04799011f07a35bde
alice.telescoop <alice@telescoop.fr>
2023-07-20 16:24:18 +0200

    refactor(front): cgu in static

--- packages/sveltekit/src/routes/cgu/+page.svelte
+++ packages/sveltekit/src/routes/cgu/+page.svelte
@@ -1,5 +1,5 @@
 <iframe
     title="Conditions Générales d'Utilisation de Data.Subventions"
     class="cgu-iframe"
-    src="/static/files/CGU_Data.Subvention.pdf#toolbar=0"
+    src="/files/CGU_Data.Subvention.pdf#toolbar=0"
     width="100%" />
