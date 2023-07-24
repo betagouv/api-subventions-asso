734bdd111bd5469836a5b6ad82b5c5ef865e5b3c
alice.telescoop <alice@telescoop.fr>
2023-07-24 18:45:42 +0200

    refactor(front): proper error page

---
+++ packages/sveltekit/src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+  import NotFound from "$lib/components/errors/NotFound.svelte";
+  import { page } from "$app/stores";
+</script>
+
+{#if $page.status === 404}
+  <NotFound />
+{:else}
+  <h1>{$page.status}</h1>
+  <p>{$page.error?.message}</p>
+{/if}
