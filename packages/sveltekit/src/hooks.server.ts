aad6431fd989f686acad8e1e58835bc45b615cab
alice.telescoop <alice@telescoop.fr>
2023-07-25 16:56:18 +0200

    refactor(front): other headers in hooks

---
+++ packages/sveltekit/src/hooks.server.ts
@@ -0,0 +1,11 @@
+import type { Handle } from '@sveltejs/kit';
+
+export const handle = (async ({ event, resolve }) => {
+    event.setHeaders({
+        "Strict-Transport-Security": "max-age=63072000; includeSubDomains; preload",
+    "X-Frame-Options": "SAMEORIGIN",
+    "X-Content-Type-Options": "nosniff",
+    "Cache-Control": "max-age 1800",
+    })
+    return resolve(event)
+}) satisfies Handle;
