0e8ba2435ee7681a30ff21e3d46a0b4688775c05
alice.telescoop <alice@telescoop.fr>
2023-08-14 09:45:47 +0200

    feat(dto): job type enum

--- packages/dto/index.ts
+++ packages/dto/index.ts
@@ -8,6 +8,7 @@ export * from "./auth/LoginDtoResponse";
 export * from "./auth/ResetPasswordDtoResponse";
 export * from "./auth/SignupDtoResponse";
 export * from "./auth/TokenValidationDtoResponse";
+export * from "./auth/subscriptionForm/agentJobType";
 export * from "./search/DemandeSubvention";
 export * from "./shared/ProviderValue";
 export * from "./shared/Rna";
