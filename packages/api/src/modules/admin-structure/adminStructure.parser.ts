8b7d0e59a4a9f116378dbca188f8bf25d32415b4
alice.telescoop <alice@telescoop.fr>
2023-08-14 16:45:55 +0200

    docs(api): explain parser

--- packages/api/src/modules/admin-structure/adminStructure.parser.ts
+++ packages/api/src/modules/admin-structure/adminStructure.parser.ts
@@ -3,6 +3,15 @@ import { BadRequestError } from "../../shared/errors/httpErrors";
 import * as ParseHelper from "../../shared/helpers/ParserHelper";
 import AdminStructureEntity from "./entities/AdminStructureEntity";
 
+/*
+ * This parser is used to populate admin-structures collection from this doc :
+ * https://docs.google.com/spreadsheets/d/1zAjTLwur9jw0VMlMYi77rLgw4F1PbCtII3Z86fFYBGo/edit
+ * through an xls export
+ * The collection is a list of the administration structures and serves as choice list
+ * for step 3 question "Quell est votre administration"
+ * The choice is actually the last column but they are meant to be filtered by columns 1
+ * and 2 (column 2 is only used if column 1 choice is "DECONCENTRATED_ADMIN")
+ */
 export default class AdminStructureParser {
     public static parseXls(content: Buffer): AdminStructureEntity[] {
         const data = ParseHelper.xlsParse(content)[0]; // single page
