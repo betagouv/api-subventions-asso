66f484faeff7b960e3bc15235cf465039455cba9
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-08-22 13:19:38 +0200

    test: test goToUrl call

--- packages/front/src/routes/user/profile/Profile.controller.test.js
+++ packages/front/src/routes/user/profile/Profile.controller.test.js
@@ -1,5 +1,7 @@
 import { ProfileController } from "./Profile.controller";
 import userService from "$lib/resources/users/user.service";
+import { goToUrl } from "$lib/services/router.service";
+vi.mock("$lib/services/router.service");
 vi.mock("$lib/resources/users/user.service");
 
 describe("ProfileController", () => {
@@ -11,6 +13,11 @@ describe("ProfileController", () => {
         it("should call userService.deleteCurrentUser()", async () => {
             await controller.deleteUser();
             expect(userService.deleteCurrentUser).toHaveBeenCalledTimes(1);
+        });
+
+        it("should call goToUrl()", async () => {
+            await controller.deleteUser();
+            expect(goToUrl).toHaveBeenCalledTimes(1);
         });
     });
 });
