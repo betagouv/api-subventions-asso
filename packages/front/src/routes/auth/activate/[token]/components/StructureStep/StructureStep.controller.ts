471064532264b0bf2499ff8a0f380957641ac9b6
alice.telescoop <alice@telescoop.fr>
2023-08-29 10:39:16 +0200

    feat(front): operator sub step

--- packages/front/src/routes/auth/activate/[token]/components/StructureStep/StructureStep.controller.ts
+++ packages/front/src/routes/auth/activate/[token]/components/StructureStep/StructureStep.controller.ts
@@ -1,6 +1,7 @@
 import { AgentTypeEnum, AgentJobTypeEnum } from "dto";
 import type { SvelteComponent } from "svelte";
 import ExampleSubStep from "./ExampleSubStep/ExampleSubStep.svelte";
+import OperatorSubStep from "./OperatorSubStep/OperatorSubStep.svelte";
 import Dispatch from "$lib/core/Dispatch";
 import Store from "$lib/core/Store";
 import { isPhoneNumber } from "$lib/helpers/stringHelper";
@@ -51,7 +52,7 @@ export default class StructureStepController {
 
     private static subStepByAgentType: Record<AgentTypeEnum, typeof SvelteComponent | undefined> = {
         [AgentTypeEnum.CENTRAL_ADMIN]: ExampleSubStep,
-        [AgentTypeEnum.OPERATOR]: ExampleSubStep,
+        [AgentTypeEnum.OPERATOR]: OperatorSubStep,
         [AgentTypeEnum.TERRITORIAL_COLLECTIVITY]: ExampleSubStep,
         [AgentTypeEnum.DECONCENTRATED_ADMIN]: ExampleSubStep,
     };
@@ -66,7 +67,7 @@ export default class StructureStepController {
         for (const inputName of Object.keys(this.validators)) {
             this.dirty[inputName] = false;
         }
-        this.dispatch("valid") // no required field so default is valid
+        this.dispatch("valid"); // no required field so default is valid
     }
 
     onUpdate(values: Record<string, unknown>, changedKey: string) {
