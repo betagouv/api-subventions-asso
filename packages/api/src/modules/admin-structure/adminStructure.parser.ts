50302b7d8d6f0211544f3080ad1e28bba45dd627
alice.telescoop <alice@telescoop.fr>
2023-08-31 12:04:20 +0200

    feat(api): parse structures territory level with enum not initial string

--- packages/api/src/modules/admin-structure/adminStructure.parser.ts
+++ packages/api/src/modules/admin-structure/adminStructure.parser.ts
@@ -1,4 +1,4 @@
-import { AgentTypeEnum } from "dto";
+import { AdminTerritorialLevel, AgentTypeEnum } from "dto";
 import { BadRequestError } from "../../shared/errors/httpErrors";
 import * as ParseHelper from "../../shared/helpers/ParserHelper";
 import AdminStructureEntity from "./entities/AdminStructureEntity";
@@ -8,16 +8,23 @@ import AdminStructureEntity from "./entities/AdminStructureEntity";
  * https://docs.google.com/spreadsheets/d/1zAjTLwur9jw0VMlMYi77rLgw4F1PbCtII3Z86fFYBGo/edit
  * through an xls export
  * The collection is a list of the administration structures and serves as choice list
- * for step 3 question "Quell est votre administration"
- * The choice is actually the last column but they are meant to be filtered by columns 1
+ * for step 3 question "Quelle est votre administration"
+ * The choice is actually the last column, but they are meant to be filtered by columns 1
  * and 2 (column 2 is only used if column 1 choice is "DECONCENTRATED_ADMIN")
  */
 export default class AdminStructureParser {
-    private static readonly csvToEnum = {
+    private static readonly csvToAgentTypeEnum: { [key: string]: AgentTypeEnum } = {
         Opérateur: AgentTypeEnum.OPERATOR,
         "Administration centrale": AgentTypeEnum.CENTRAL_ADMIN,
         "Collectivité territoriale": AgentTypeEnum.TERRITORIAL_COLLECTIVITY,
         "Administration déconcentrée": AgentTypeEnum.DECONCENTRATED_ADMIN,
+    };
+
+    private static readonly csvToTerritoryLevel: { [key: string]: AdminTerritorialLevel | "" } = {
+        Départementale: AdminTerritorialLevel.DEPARTMENTAL,
+        Régionale: AdminTerritorialLevel.REGIONAL,
+        "Collectivité d'outre-mer à statut particulier": AdminTerritorialLevel.OVERSEAS,
+        Autre: "",
     };
 
     public static parseXls(content: Buffer): AdminStructureEntity[] {
@@ -26,16 +33,29 @@ export default class AdminStructureParser {
 
         const entities: AdminStructureEntity[] = [];
         for (const row of rows) {
-            entities.push(new AdminStructureEntity(AdminStructureParser.exportNameToAgentType(row[0]), row[1], row[2]));
+            entities.push(
+                new AdminStructureEntity(
+                    AdminStructureParser.exportNameToAgentType(row[0]),
+                    AdminStructureParser.exportNameToTerritoryLevel(row[1]),
+                    row[2],
+                ),
+            );
         }
 
         return entities;
     }
 
-    public static exportNameToAgentType(value: string) {
-        if (AdminStructureParser.csvToEnum[value]) return AdminStructureParser.csvToEnum[value];
+    private static exportNameToAgentType(value: string) {
+        if (AdminStructureParser.csvToAgentTypeEnum[value]) return AdminStructureParser.csvToAgentTypeEnum[value];
         throw new BadRequestError(
-            `Valeur de première colonne "${value}" non conforme, mettre à jour \`csvNameToAgentType\` dans \`adminStructure.parser\`.`,
+            `Valeur de première colonne "${value}" non conforme, mettre à jour \`csvNameToAgentType\` dans \`adminStructure.parser\` ou la valeur dans le document source.`,
+        );
+    }
+
+    private static exportNameToTerritoryLevel(value: string) {
+        if (AdminStructureParser.csvToTerritoryLevel[value]) return AdminStructureParser.csvToTerritoryLevel[value];
+        throw new BadRequestError(
+            `Valeur de seconde colonne "${value}" non conforme, mettre à jour \`csvNameToTerritoryLevel\` dans \`adminStructure.parser\` ou la valeur dans le document source.`,
         );
     }
 }
