26040ce2e5a49c1cb399cf66ca64cd08d9bb563c
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-09-05 10:32:31 +0200

    feat(api): add import type env in desploy osiris file tool

--- packages/api/tools/osiris/deploy-files-container.sh
+++ packages/api/tools/osiris/deploy-files-container.sh
@@ -3,7 +3,7 @@
 mkdir /tmp/files
 tar -xf /tmp/uploads/* -C /tmp/files
 
-node ./packages/api/build/src/cli.js osiris parse requests /tmp/files $TMP_YEAR_OF_FILE
+node ./packages/api/build/src/cli.js osiris parse $IMPORT_TYPE /tmp/files $TMP_YEAR_OF_FILE
 
 for file in "./logs/*"; do
     cat $file;
