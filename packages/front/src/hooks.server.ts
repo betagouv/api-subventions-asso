f1b7d9f81fd726f2ee092d0062d8b16db22c5144
alice.telescoop <alice@telescoop.fr>
2023-09-07 17:35:17 +0200

    feat(front): basic sentry setup

--- packages/front/src/hooks.server.ts
+++ packages/front/src/hooks.server.ts
@@ -1,6 +1,13 @@
+import { sequence } from "@sveltejs/kit/hooks";
+import * as Sentry from "@sentry/sveltekit";
 import type { Handle } from "@sveltejs/kit";
 
-export const handle = (async ({ event, resolve }) => {
+Sentry.init({
+    dsn: "https://5d47efb730804541a53e916f0bd27cea@sentry.incubateur.net/101",
+    tracesSampleRate: 1,
+});
+
+export const handle = sequence(Sentry.sentryHandle(), (async ({ event, resolve }) => {
     event.setHeaders({
         "Strict-Transport-Security": "max-age=63072000; includeSubDomains; preload",
         "X-Frame-Options": "SAMEORIGIN",
@@ -8,4 +15,5 @@ export const handle = (async ({ event, resolve }) => {
         "Cache-Control": "max-age 1800",
     });
     return resolve(event);
-}) satisfies Handle;
+}) satisfies Handle);
+export const handleError = Sentry.handleErrorWithSentry();
--- packages/front/vite.config.ts
+++ packages/front/vite.config.ts
@@ -1,8 +1,15 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [sentrySvelteKit({
+        sourceMapsUploadOptions: {
+            org: "betagouv",
+            project: "data-subvention-front",
+            url: "https://sentry.incubateur.net/"
+        }
+    }), sveltekit()],
     test: {
         include: ["src/**/*.{test,spec}.{js,ts}"],
         globals: true,
