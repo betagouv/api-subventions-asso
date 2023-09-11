f1dcd8e44b0aea5fce30188b8de357f45dbd4511
alice.telescoop <alice@telescoop.fr>
2023-09-11 14:41:55 +0200

    feat(front): dsfr skiplink component

---
+++ packages/front/src/lib/dsfr/SkipLinks.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let links: { label: string; anchor: string }[];
+</script>
+
+<div class="fr-skiplinks">
+    <nav class="fr-container" aria-label="Accès rapide">
+        <ul class="fr-skiplinks__list">
+            {#each links as link}
+                <li>
+                    <a class="fr-link" href="#{link.anchor}">{link.label}</a>
+                </li>
+            {/each}
+        </ul>
+    </nav>
+</div>
