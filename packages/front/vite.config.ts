86e5345a9b5040d0b35d3c44d27ebc25859ab505
alice.telescoop <alice@telescoop.fr>
2023-09-11 10:47:40 +0200

    fix(front): get all env vars in vite

--- packages/front/vite.config.ts
+++ packages/front/vite.config.ts
@@ -1,29 +1,35 @@
 import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-    plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "betagouv",
-            project: "data-subvention-front",
-            url: "https://sentry.incubateur.net/",
-            authToken: process.env.SENTRY_AUTH_TOKEN,
-        }
-    }), sveltekit()],
-    test: {
-        include: ["src/**/*.{test,spec}.{js,ts}"],
-        globals: true,
-        environment: "jsdom",
-        clearMocks: true,
-        setupFiles: ["src/setuptest.ts"],
-    },
-    optimizeDeps: {
-        include: ["dto"],
-    },
-    build: {
-        commonjsOptions: {
-            include: [/dto/, /node_modules/],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        plugins: [
+            sentrySvelteKit({
+                sourceMapsUploadOptions: {
+                    org: "betagouv",
+                    project: "data-subvention-front",
+                    url: "https://sentry.incubateur.net/",
+                    authToken: env.SENTRY_AUTH_TOKEN || env.SENTRY_AUTH_TOKEN,
+                },
+            }),
+            sveltekit(),
+        ],
+        test: {
+            include: ["src/**/*.{test,spec}.{js,ts}"],
+            globals: true,
+            environment: "jsdom",
+            clearMocks: true,
+            setupFiles: ["src/setuptest.ts"],
         },
-    },
+        optimizeDeps: {
+            include: ["dto"],
+        },
+        build: {
+            commonjsOptions: {
+                include: [/dto/, /node_modules/],
+            },
+        },
+    };
 });
