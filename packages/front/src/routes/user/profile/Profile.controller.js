89d9a2083c3750ab827ce642e47303fed61ee306
alice.telescoop <alice@telescoop.fr>
2023-09-14 18:51:25 +0200

    refactor(front): group agentType options in service

--- packages/front/src/routes/user/profile/Profile.controller.js
+++ packages/front/src/routes/user/profile/Profile.controller.js
@@ -1,17 +1,10 @@
 import { AgentTypeEnum } from "dto";
 import Store from "$lib/core/Store";
 import userService from "$lib/resources/users/user.service";
+import subscriptionFormService from "$lib/resources/auth/subscriptionForm/subscriptionFormService.js";
 
 export class ProfileController {
-    agentTypeOptions = [
-        { value: AgentTypeEnum.CENTRAL_ADMIN, label: "Agent public d’une administration centrale (État)" },
-        {
-            value: AgentTypeEnum.DECONCENTRATED_ADMIN,
-            label: "Agent public d’une administration déconcentrée (État)",
-        },
-        { value: AgentTypeEnum.TERRITORIAL_COLLECTIVITY, label: "Agent public d’une collectivité territoriale" },
-        { value: AgentTypeEnum.OPERATOR, label: "Agent public d’un opérateur de l’État" },
-    ];
+    agentTypeOptions = subscriptionFormService.agentTypeOptions;
 
     constructor() {
         this.deleteError = new Store(false);
