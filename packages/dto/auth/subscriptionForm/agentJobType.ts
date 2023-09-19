1969dcf7a9a7f82e235bea923f53aa5ee6e3d5fb
alice.telescoop <alice@telescoop.fr>
2023-09-19 17:52:41 +0200

    feat(api,dto,front): new job type

--- packages/dto/auth/subscriptionForm/agentJobType.ts
+++ packages/dto/auth/subscriptionForm/agentJobType.ts
@@ -2,5 +2,6 @@ export enum AgentJobTypeEnum {
     ADMINISTRATOR = "ADMINISTRATOR",
     EXPERT = "EXPERT",
     SERVICE_HEAD = "SERVICE_HEAD",
+    CONTROLLER = "CONTROLLER",
     OTHER = "OTHER",
 }
