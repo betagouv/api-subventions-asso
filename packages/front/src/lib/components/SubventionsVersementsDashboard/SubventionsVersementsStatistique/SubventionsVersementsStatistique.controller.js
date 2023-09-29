fe3509f2727624e0fe3e25345d22340d32d67c3c
alice.telescoop <alice@telescoop.fr>
2023-09-29 11:18:24 +0200

    refactor(front): cleaned

--- packages/front/src/lib/components/SubventionsVersementsDashboard/SubventionsVersementsStatistique/SubventionsVersementsStatistique.controller.js
+++ packages/front/src/lib/components/SubventionsVersementsDashboard/SubventionsVersementsStatistique/SubventionsVersementsStatistique.controller.js
@@ -1,13 +1,11 @@
-import Store from "../../../core/Store";
+import Store from "$lib/core/Store";
 
 import { numberToEuro, valueOrHyphen } from "$lib/helpers/dataHelper";
 
 export default class SubventionsVersementsStatistiqueController {
     constructor() {
         this.elements = [];
-
         this.versementsAmount = new Store("-");
-        this.subventionsPercent = new Store(0);
     }
 
     updateElements(elements) {
@@ -21,22 +19,7 @@ export default class SubventionsVersementsStatistiqueController {
         }, 0);
     }
 
-    _computeSubventionsPercent() {
-        const { amountGranted, amountRequested } = this.elements.reduce(
-            (acc, element) => {
-                if (!element?.subvention?.montants?.accorde || !element?.subvention?.montants?.demande) return acc;
-                acc.amountGranted += element.subvention.montants.accorde || 0;
-                acc.amountRequested += element.subvention.montants.demande || 0;
-                return acc;
-            },
-            { amountGranted: 0, amountRequested: 0 },
-        );
-        if (amountRequested === 0) return null;
-        return ((amountGranted / amountRequested) * 100).toFixed(0);
-    }
-
     update() {
         this.versementsAmount.set(valueOrHyphen(numberToEuro(this._computeVersementsAmount())));
-        this.subventionsPercent.set(this._computeSubventionsPercent());
     }
 }
