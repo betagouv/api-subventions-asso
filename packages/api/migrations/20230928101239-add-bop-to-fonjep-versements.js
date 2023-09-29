9875231e9c39615163e86c61f8ea39d8f16b7309
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-09-29 10:56:42 +0200

    feat(api): clean migration

--- packages/api/migrations/20230928101239-add-bop-to-fonjep-versements.js
+++ packages/api/migrations/20230928101239-add-bop-to-fonjep-versements.js
@@ -1,11 +1,7 @@
 const { default: fonjepService } = require("../build/src/modules/providers/fonjep/fonjep.service");
 
 module.exports = {
-    async up(db, client) {
-        // TODO write your migration here.
-        // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
-        // Example:
-
+    async up(db) {
         const cursor = await db.collection("fonjepVersement").find({});
 
         while (await cursor.hasNext()) {
@@ -21,11 +17,5 @@ module.exports = {
             );
             await db.collection("fonjepVersement").updateOne({ _id: versement._id }, { $set: versement });
         }
-    },
-
-    async down(db, client) {
-        // TODO write the statements to rollback your migration (if possible)
-        // Example:
-        // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
     },
 };
