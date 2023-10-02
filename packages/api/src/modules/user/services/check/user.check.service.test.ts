82b708e614cc8447c6a63e84e456a019457723aa
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-10-02 16:28:28 +0200

    refactor(api): split passwordValidator from user service

--- packages/api/src/modules/user/services/check/user.check.service.test.ts
+++ packages/api/src/modules/user/services/check/user.check.service.test.ts
@@ -1,7 +1,42 @@
 import userCheckService from "./user.check.service";
 
 describe("user check service", () => {
-    it("should test", () => {
-        expect(true).toBeTruthy();
+    describe("passwordValidator", () => {
+        it("should accept #", () => {
+            const actual = userCheckService.passwordValidator("Aa12345#");
+            expect(actual).toEqual(true);
+        });
+
+        it("should reject because no number in password", () => {
+            expect(userCheckService.passwordValidator("AAAAAAAaaaaaa;;;;")).toBe(false);
+        });
+
+        it("should reject because no char (Uppercase) in password", () => {
+            expect(userCheckService.passwordValidator("11111aaaaaa;;;;")).toBe(false);
+        });
+
+        it("should reject because no char (Lowercase) in password", () => {
+            expect(userCheckService.passwordValidator("11111AAAAA;;;;")).toBe(false);
+        });
+
+        it("should reject because no special char in password", () => {
+            expect(userCheckService.passwordValidator("11111AAAAAaaaaaa")).toBe(false);
+        });
+
+        it("should reject because length is to short in password", () => {
+            expect(userCheckService.passwordValidator("Aa1;")).toBe(false);
+        });
+
+        it("should reject because length is to big in password", () => {
+            expect(
+                userCheckService.passwordValidator(
+                    "Aa1;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                ),
+            ).toBe(false);
+        });
+
+        it("should accept", () => {
+            expect(userCheckService.passwordValidator("SUPER;test::12345678")).toBe(true);
+        });
     });
 });
--- packages/api/src/modules/user/services/check/user.check.service.ts
+++ packages/api/src/modules/user/services/check/user.check.service.ts
@@ -1,4 +1,10 @@
-export class UserCheckService {}
+import { REGEX_PASSWORD } from "../../user.constant";
+
+export class UserCheckService {
+    passwordValidator(password: string): boolean {
+        return REGEX_PASSWORD.test(password);
+    }
+}
 
 const userCheckService = new UserCheckService();
 export default userCheckService;
