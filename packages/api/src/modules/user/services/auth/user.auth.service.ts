68763cbad2db24c6b8cddd41e2745653d1c9bb3a
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-10-02 14:18:18 +0200

    refactor(api): split getHashPassword

--- packages/api/src/modules/user/services/auth/user.auth.service.ts
+++ packages/api/src/modules/user/services/auth/user.auth.service.ts
@@ -1,4 +1,10 @@
-export class UserAuthService {}
+import bcrypt from "bcrypt";
+
+export class UserAuthService {
+    public async getHashPassword(password: string) {
+        return bcrypt.hash(password, 10);
+    }
+}
 
 const userAuthService = new UserAuthService();
 export default userAuthService;
