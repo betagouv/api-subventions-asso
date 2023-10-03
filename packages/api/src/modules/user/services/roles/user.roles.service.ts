cfed96be04f77c7969ec1d4269a9c8bd70ac1d54
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-10-03 15:28:11 +0200

    refactor(api): split getRoles from user service

--- packages/api/src/modules/user/services/roles/user.roles.service.ts
+++ packages/api/src/modules/user/services/roles/user.roles.service.ts
@@ -1,4 +1,10 @@
-export class UserRolesService {}
+import { UserDto } from "dto";
+
+export class UserRolesService {
+    public getRoles(user: UserDto) {
+        return user.roles;
+    }
+}
 
 const userRolesService = new UserRolesService();
 export default userRolesService;
