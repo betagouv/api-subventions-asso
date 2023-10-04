91a8e94d0c0d7193b444b315f88ebb5d6d5af495
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-10-04 13:03:09 +0200

    refactor(api): split getUsersWithStats from user service

--- packages/api/src/modules/user/services/stats/user.stats.service.ts
+++ packages/api/src/modules/user/services/stats/user.stats.service.ts
@@ -1,4 +1,7 @@
+import { UserWithStatsDto } from "dto";
 import userRepository from "../../repositories/user.repository";
+import userAssociationVisitJoiner from "../../../stats/joiners/UserAssociationVisitsJoiner";
+import { getMostRecentDate } from "../../../../shared/helpers/DateHelper";
 
 export class UserStatsService {
     public countTotalUsersOnDate(date, withAdmin = false) {
@@ -7,6 +10,23 @@ export class UserStatsService {
 
     public findByPeriod(begin: Date, end: Date, withAdmin = false) {
         return userRepository.findByPeriod(begin, end, withAdmin);
+    }
+
+    public async getUsersWithStats(includesAdmin = false): Promise<UserWithStatsDto[]> {
+        const usersWithAssociationVisits = await userAssociationVisitJoiner.findUsersWithAssociationVisits(
+            includesAdmin,
+        );
+        const userWithStats = usersWithAssociationVisits.map(user => {
+            const stats = {
+                lastSearchDate: getMostRecentDate(user.associationVisits.map(visit => visit.date)),
+                searchCount: user.associationVisits.length,
+            };
+            // remove associationVisits
+            const { associationVisits: _associationVisits, ...userDbo } = user;
+            return { ...userDbo, stats } as UserWithStatsDto;
+        });
+
+        return userWithStats;
     }
 }
 
