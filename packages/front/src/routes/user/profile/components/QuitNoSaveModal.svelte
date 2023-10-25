445f206f1c83fe56bbe21d051857f0ec8f8c64f8
alice.telescoop <alice@telescoop.fr>
2023-10-25 14:45:43 +0200

    fix(front): classical modal to prevent and allow leaving unsasved profile

---
+++ packages/front/src/routes/user/profile/components/QuitNoSaveModal.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { action } from "$lib/store/modal.store";
+
+    import ModalContent from "$lib/dsfr/ModalContent.svelte";
+    import ModalFooter from "$lib/dsfr/ModalFooter.svelte";
+</script>
+
+<ModalContent title="Souhaitez-vous quitter sans enregistrer ?">
+    Vous avez des modifications non enregistrées.
+</ModalContent>
+<ModalFooter confirmLabel="Quitter la page" confirmAction={$action} />
