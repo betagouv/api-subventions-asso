14a5d66c1e6a750bd65aae356be2dfc749bef89c
Victor Serain <victor.serain@epitech.eu>
2023-10-31 09:40:42 +0100

    feat(api): new archi for history unite legal

--- packages/api/src/modules/providers/datagouv/use-cases/UpdateHistoryUniteLegalUseCase.ts
+++
@@ -1,9 +0,0 @@
-import DecompressHistoryUniteLegalUseCase from "./DecompressHistoryUniteLegalUseCase";
-import DownloadHistoryUniteLegalUseCase from "./DownloadHistoryUniteLegalUseCase";
-import ParseHistoryUniteLegalUseCase from "./ParseHistoryUniteLegalUseCase";
-
-export default async function UpdateHistoryUniteLegalUseCase() {
-    const archivePath = await DownloadHistoryUniteLegalUseCase();
-    const filePath = await DecompressHistoryUniteLegalUseCase(archivePath);
-    await ParseHistoryUniteLegalUseCase.run(filePath, new Date());
-}
