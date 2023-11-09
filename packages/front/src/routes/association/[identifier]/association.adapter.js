7b8bdb3425807a13715f2c8cfa0f4338c9efe2b0
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-11-09 12:11:29 +0100

    refactor(front): remove unused file

--- packages/front/src/routes/association/[identifier]/association.adapter.js
+++
@@ -1,9 +0,0 @@
-import { getDate, getProvider, flattenProviderValue } from "$lib/helpers/providerValueHelper";
-
-export const toDocumentComponent = document => {
-    return {
-        ...flattenProviderValue(document),
-        provider: getProvider(document.nom),
-        date: new Date(getDate(document.nom)),
-    };
-};
