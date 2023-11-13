e71ca5fd71b55509b45f63bbba86a049e1132e77
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-11-13 09:09:16 +0100

    feat(front): remove searchbar search result

--- packages/front/src/routes/(home)/+page.svelte
+++ packages/front/src/routes/(home)/+page.svelte
@@ -1,9 +1,7 @@
 <script>
-    import ResultCard from "./components/ResultCard.svelte";
     import { HomeController } from "./Home.controller";
     import Alert from "$lib/dsfr/Alert.svelte";
     import AssociationCard from "$lib/components/AssociationCard/AssociationCard.svelte";
-    import Spinner from "$lib/components/Spinner.svelte";
     import Messages from "$lib/components/Messages/Messages.svelte";
     import SearchBar from "$lib/components/SearchBar/SearchBar.svelte";
 
@@ -11,9 +9,7 @@
     const { query } = data;
 
     const ctrl = new HomeController(query);
-    const { input, isLoading, currentSearch, searchResult, searchHistory } = ctrl;
-
-    $: input && ctrl.onInput($input);
+    const { input, searchHistory } = ctrl;
 </script>
 
 <Messages />
@@ -37,44 +33,7 @@
     </div>
 </div>
 
-{#if $isLoading}
-    <div class="fr-grid-row fr-grid-row--center fr-grid-row--gutters">
-        <div class="fr-col-12 fr-col-md-12">
-            <div class="fr-card fr-card--no-arrow">
-                <div class="fr-card__body">
-                    <Spinner description="Recherche en cours" />
-                </div>
-            </div>
-        </div>
-    </div>
-{:else if $currentSearch}
-    {#await $currentSearch.promise then _result}
-        {#if $searchResult.length}
-            <div class="fr-grid-row fr-grid-row--center fr-grid-row--gutters search-result">
-                {#each $searchResult as association}
-                    <ResultCard {association} searchValue={$input} />
-                {/each}
-            </div>
-        {:else}
-            <div class="fr-mt-2w" />
-            <Alert type="info" small={true}>
-                <p>
-                    Si la recherche par nom ne donne pas de résultats, vous pouvez essayer avec le RNA, SIREN ou SIRET
-                </p>
-            </Alert>
-            <Alert small={true}>
-                <p>Nous n'avons trouvé aucun résultat pour votre recherche</p>
-            </Alert>
-        {/if}
-    {:catch _error}
-        <div class="fr-mt-2w" />
-        <Alert type="error" small={true}>
-            <p>Une erreur est survenue</p>
-        </Alert>
-    {/await}
-{/if}
-
-{#if !$isLoading && !$searchResult.length && $searchHistory.length}
+{#if $searchHistory.length}
     <div class="fr-grid-row fr-grid-row--center fr-mt-12v fr-mb-6v">
         <h2 class="fr-h4">Vos dernières recherches</h2>
     </div>
@@ -95,10 +54,5 @@
         /* margin: auto; */
         /* justify-content: space-around; */
         max-width: 792px;
-    }
-
-    .search-result {
-        max-height: 50vh;
-        overflow: auto;
     }
 </style>
--- packages/front/src/routes/(home)/Home.controller.js
+++ packages/front/src/routes/(home)/Home.controller.js
@@ -1,54 +1,13 @@
-import { debounce } from "lodash/function";
 import { getSearchHistory } from "$lib/services/searchHistory.service";
-import { isRna, isSiren, isSiret, isStartOfSiret } from "$lib/helpers/validatorHelper";
+import { isRna, isSiren, isSiret } from "$lib/helpers/validatorHelper";
 import Store from "$lib/core/Store";
-import associationService from "$lib/resources/associations/association.service";
 import { goto } from "$app/navigation";
 
 export class HomeController {
     constructor(query = {}) {
-        this.isLoading = new Store(false);
-        this.searchResult = new Store([]);
         this.searchHistory = getSearchHistory();
         this.input = new Store("");
-        this._searchCache = new Map();
-        this.currentSearch = new Store(null);
         this.successMessage = this._getSuccessMessage(query.success);
-    }
-
-    onInput(input) {
-        console.log("coucou");
-        if (input.length < 3) return;
-
-        this._debouncedInitSearch();
-        this._debouncedPerformSearch(input);
-    }
-
-    _initSearch() {
-        if (!this.isLoading.value) this.isLoading.set(true);
-    }
-
-    _debouncedInitSearch = debounce(() => this._initSearch());
-
-    _performSearch(text) {
-        text = this._removeSpaceFromIdentifier(text);
-
-        // nothing to do if we are already searching for the same text
-        if (text === this.currentSearch.value?.text) return;
-
-        this.currentSearch.set({
-            promise: this._searchAndCache(text).then(result => this._finishSearch(text, result)),
-            text,
-        });
-    }
-
-    _debouncedPerformSearch = debounce(text => this._performSearch(text), 1000);
-
-    _finishSearch(text, result) {
-        if (text !== this.currentSearch.value?.text) return;
-        this.isLoading.set(false);
-        this.searchResult.set(result || []);
-        return result;
     }
 
     onSubmit() {
@@ -56,22 +15,9 @@ export class HomeController {
             goto(`/association/${this.input.value}`);
         } else if (isSiret(this.input.value)) {
             goto(`/etablissement/${this.input.value}`);
-        } else if (this.searchResult.value?.length) {
-            goto(`/association/${this.searchResult.value[0].rna || this.searchResult.value[0].siren}`);
+        } else {
+            goto(`/search/${this.input.value}`);
         }
-    }
-
-    async _searchAndCache(searchedText) {
-        if (this._searchCache.has(searchedText)) return this._searchCache.get(searchedText);
-        const result = (await associationService.search(searchedText)).slice(0, 20);
-        this._searchCache.set(searchedText, result);
-        return result;
-    }
-
-    _removeSpaceFromIdentifier(text) {
-        const textWithoutSpace = text.replaceAll(" ", "");
-        if (isStartOfSiret(textWithoutSpace) || isRna(textWithoutSpace)) return textWithoutSpace;
-        return text;
     }
 
     _getSuccessMessage(successKey) {
