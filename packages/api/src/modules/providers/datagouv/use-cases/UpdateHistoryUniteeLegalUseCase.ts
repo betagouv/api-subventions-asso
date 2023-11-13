ad98fa75257b9115ac7fb670ea94c974a0296ad7
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-11-13 16:27:45 +0100

    fix(api): bad merge

--- packages/api/src/modules/providers/datagouv/use-cases/UpdateHistoryUniteeLegalUseCase.ts
+++
@@ -1,9 +0,0 @@
-import DecompressHistoryUniteeLegalUseCase from "./DecompressHistoryUniteeLegalUseCase";
-import DownloadHistoryUniteeLegalUseCase from "./DownloadHistoryUniteeLegalUseCase";
-import ParseHistoryUniteeLegalUseCase from "./ParseHistoryUniteeLegalUseCase";
-
-export default async function UpdateHistoryUniteeLegalUseCase() {
-    const archivePath = await DownloadHistoryUniteeLegalUseCase();
-    const filePath = await DecompressHistoryUniteeLegalUseCase(archivePath);
-    await ParseHistoryUniteeLegalUseCase(filePath, new Date());
-}
