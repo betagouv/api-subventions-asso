d1fd0ca99e883f0200ed003a5d99990956aa7a8b
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-11-15 11:04:43 +0100

    refactor(api): extract rename empty header function

--- packages/api/src/modules/providers/chorus/chorus.parser.ts
+++ packages/api/src/modules/providers/chorus/chorus.parser.ts
@@ -43,6 +43,25 @@ export default class ChorusParser {
         }, [] as ChorusLineEntity[]);
     }
 
+    // CHORUS exports have "double columns" sharing the same header (only the header for the first column is defined)
+    // Because it is always a code followed by its corresponding label we replace the header by two distinct headers :
+    // LABEL + CODE | LABEL
+    private static renameEmptyHeaders(headerRow) {
+        const header: string[] = [];
+        for (let i = 0; i < headerRow.length; i++) {
+            // if header not defined, we take the previous one
+            if (!headerRow[i]) {
+                const name = header[i - 1] as string;
+                // we add CODE at the end of the previous header
+                header[i - 1] = `${name} CODE`;
+                header.push(name.replace("&#32;", " ").trim());
+            } else {
+                header.push(headerRow[i].replace(/&#32;/g, " ").trim());
+            }
+        }
+        return header;
+    }
+
     static parseXls(content: Buffer, validator: (entity: ChorusLineEntity) => boolean) {
         console.log("Open and read file ...");
         const pages = ParseHelper.xlsParse(content);
@@ -50,17 +69,10 @@ export default class ChorusParser {
         console.log("Read file end");
 
         const headerRow = page[0] as string[];
-        const header: string[] = [];
 
-        for (let i = 0; i < headerRow.length; i++) {
-            if (!headerRow[i]) {
-                const name = header[i - 1] as string;
-                header[i - 1] = `${name} CODE`;
-                header.push(name.replace("&#32;", " ").trim());
-            } else {
-                header.push(headerRow[i].replace(/&#32;/g, " ").trim());
-            }
-        }
+        const header = ChorusParser.renameEmptyHeaders(headerRow);
+
+        // rename empty header
 
         const data = page.slice(1) as string[][];
         return data.reduce((entities, row, index) => {
