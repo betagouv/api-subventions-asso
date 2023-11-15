4e3fbb8f1b945d0c06bbdddb1a4336f36e1462c1
Maxime Silvère Meunier <mxmeunier@gmail.com>
2023-11-15 11:07:15 +0100

    fix(api): updateLogUserId migration default import in commonjs

--- packages/api/migrations/20231113162302-log-add-user-id.js
+++ packages/api/migrations/20231113162302-log-add-user-id.js
@@ -1,4 +1,4 @@
-const updateLogUserId = require("../build/src/modules/stats/migrations/userId.migration");
+const { default: updateLogUserId } = require("../build/src/modules/stats/migrations/userId.migration");
 module.exports = {
     async up(db) {
         await updateLogUserId(db);
