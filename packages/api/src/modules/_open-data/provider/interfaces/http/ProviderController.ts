eae3afe72b56dcfb69cadd6d63bd800222d87b15
natoine <antoineseilles@gmail.com>
2023-11-16 14:56:54 +0100

    docs: add description on opendata http methods

--- packages/api/src/modules/_open-data/provider/interfaces/http/ProviderController.ts
+++ packages/api/src/modules/_open-data/provider/interfaces/http/ProviderController.ts
@@ -5,6 +5,11 @@ import providerService from "../../provider.service";
 @Route("open-data/fournisseurs")
 @Tags("Open Data")
 export class ProviderController extends Controller {
+    /**
+     * Récupérer la liste de tous nos fournisseurs de données
+     *
+     * @summary Récupérer la liste de tous nos fournisseurs de données
+     */
     @Get("/")
     async getProvidersInfos(): Promise<ProvidersInfos> {
         return await providerService.getProvidersInfos();
