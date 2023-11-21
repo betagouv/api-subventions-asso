a25fb78e3d5b97cc023e761cf61e0fb53e0d646d
alice.telescoop <alice@telescoop.fr>
2023-11-21 17:54:48 +0100

    feat(front): display cp instead of establishment

--- packages/front/src/lib/resources/subventions/subventions.adapter.js
+++ packages/front/src/lib/resources/subventions/subventions.adapter.js
@@ -1,19 +1,22 @@
 import { valueOrHyphen, numberToEuro } from "$lib/helpers/dataHelper";
 import { capitalizeFirstLetter } from "$lib/helpers/stringHelper";
+import { mapSiretPostCodeStore } from "$lib/store/association.store";
 
 export default class SubventionsAdapter {
     /**
      * @param {siret, subvention, versements} element Object that links an application to its related payments
      */
     static toSubvention(element) {
-        const siret = element.siret;
+        const siret = element.siret?.toString();
         const subvention = element.subvention;
         let dispositif = subvention.dispositif;
         let serviceInstructeur = subvention.service_instructeur;
         let projectName = this._getProjectName(subvention);
 
         return {
-            establishment: valueOrHyphen(siret),
+            establishmentPostcode: valueOrHyphen(
+                mapSiretPostCodeStore.value.get(siret)?.replace(/(\d{2})(\d{3})/, "$1 $2"),
+            ),
             serviceInstructeur: valueOrHyphen(serviceInstructeur),
             dispositif: valueOrHyphen(dispositif),
             projectName: valueOrHyphen(projectName),
