858936f2eb1d1f5619dc6d545d49cb71067d8bc4
alice.telescoop <alice@telescoop.fr>
2023-12-19 15:27:09 +0100

    fix(front): get association from store in establishment page

--- packages/front/src/routes/etablissement/[identifier]/+page.svelte
+++ packages/front/src/routes/etablissement/[identifier]/+page.svelte
@@ -1,39 +1,41 @@
 <script>
     import TabsEtab from "./components/TabsEtab.svelte";
+    import { EstablishmentController } from "./Establishment.controller";
     import DataNotFound from "$lib/components/DataNotFound.svelte";
     import ErrorAlert from "$lib/components/ErrorAlert.svelte";
     import InfosLegales from "$lib/components/InfosLegales/InfosLegales.svelte";
     import FullPageSpinner from "$lib/components/FullPageSpinner.svelte";
     import StructureTitle from "$lib/components/StructureTitle/StructureTitle.svelte";
-    import { siretToSiren } from "$lib/helpers/sirenHelper";
-    import associationService from "$lib/resources/associations/association.service";
-    import establishmentService from "$lib/resources/establishments/establishment.service";
+    import Alert from "$lib/dsfr/Alert.svelte";
 
     export let data;
     const { identifier: id } = data.params;
 
-    const titles = ["Tableau de bord", "Contacts", "Pièces administratives", "Informations bancaires"];
-    const associationPromise = associationService.getAssociation(siretToSiren(id));
-    const establishmentPromise = establishmentService.getBySiret(id);
-    const promises = Promise.all([associationPromise, establishmentPromise]).then(result => ({
-        association: result[0],
-        establishment: result[1],
-    }));
+    const controller = new EstablishmentController(id);
+    const { promises, titles } = controller;
 </script>
 
 <div class="fr-container">
     {#await promises}
         <FullPageSpinner description="Chargement de l'établissement {id} en cours ..." />
     {:then result}
-        <div class="fr-mb-3w">
-            <StructureTitle association={result.association} siret={id} />
-        </div>
-        <div class="fr-mb-6w">
-            <InfosLegales association={result.association} establishment={result.establishment} />
-        </div>
-        <div class="fr-mb-6w">
-            <TabsEtab establishment={result.establishment} {titles} identifier={id} />
-        </div>
+        {#if controller.isAssociation}
+            <div class="fr-mb-3w">
+                <StructureTitle siret={id} />
+            </div>
+            <div class="fr-mb-6w">
+                <InfosLegales association={result.association} establishment={result.establishment} />
+            </div>
+            <div class="fr-mb-6w">
+                <TabsEtab establishment={result.establishment} {titles} identifier={id} />
+            </div>
+        {:else}
+            <div class="fr-mb-3w">
+                <Alert type="warning" title="Attention">
+                    Il semblerait que vous cherchiez un établissement d'une entreprise et non d'une association
+                </Alert>
+            </div>
+        {/if}
     {:catch error}
         {#if error.request && error.request.status === 404}
             <DataNotFound />
