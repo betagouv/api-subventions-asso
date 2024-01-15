0823e48d8cef2b1433ec850d43ea52d2a9955005
alice.telescoop <alice@telescoop.fr>
2024-01-15 12:08:45 +0100

    test(api): integ test for rgpd cron

---
+++ packages/api/tests/interfaces/cron/Rgpd.cron.spec.ts
@@ -0,0 +1,66 @@
+import { RgpdCron } from "../../../src/interfaces/cron/Rgpd.cron";
+import userRepository from "../../../src/modules/user/repositories/user.repository";
+import { USER_DBO } from "../../../src/modules/user/__fixtures__/user.fixture";
+import brevoContactNotifyPipe from "../../../src/modules/notify/outPipes/BrevoContactNotifyPipe";
+import axios from "axios";
+
+describe("Rgpd Cron", () => {
+    const NOW_STR = "2024-01-02";
+    const NOW = new Date(NOW_STR);
+    let cron: RgpdCron;
+
+    beforeEach(() => {
+        cron = new RgpdCron();
+    });
+
+    describe("removeInactiveUsers()", () => {
+        beforeEach(async () => {
+            await Promise.all([
+                userRepository.create({
+                    ...USER_DBO,
+                    email: "old-user1@mail.com",
+                    signupAt: new Date("2020-12-12"),
+                    jwt: null,
+                }),
+                userRepository.create({
+                    ...USER_DBO,
+                    email: "old-user2@mail.com",
+                    signupAt: new Date("2020-12-12"),
+                    jwt: { ...USER_DBO.jwt, expirateDate: new Date("2020-12-12") },
+                }),
+                userRepository.create({
+                    ...USER_DBO,
+                    email: "new-user@mail.com",
+                    jwt: null,
+                    signupAt: NOW,
+                }),
+            ]);
+        });
+
+        it("should disable users that did not use the app for 2 years", async () => {
+            await cron.removeInactiveUsers();
+            const users = await userRepository.findAll();
+            users.map(user =>
+                expect(user).toMatchSnapshot({
+                    _id: expect.anything(),
+                    email: expect.anything(),
+                }),
+            );
+            users.map(user =>
+                expect(!user.disable || user.email === `${user._id}@deleted.datasubvention.beta.gouv.fr`),
+            );
+        });
+
+        it("should delete the users on brevo", async () => {
+            await cron.removeInactiveUsers();
+            // @ts-ignore
+            expect(brevoContactNotifyPipe.apiInstance.deleteContact).toHaveBeenCalledTimes(2);
+        });
+
+        it("should notify through mattermost", async () => {
+            await cron.removeInactiveUsers();
+            const actual = jest.mocked(axios.post).mock.calls;
+            expect(actual).toMatchSnapshot();
+        });
+    });
+});
