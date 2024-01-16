33835b17626496b1b9c12d52faed258338b01263
alice.telescoop <alice@telescoop.fr>
2024-01-16 15:49:55 +0100

    feat(api): sentry error on failed heartbeat

--- packages/api/src/shared/MongoConnection.ts
+++ packages/api/src/shared/MongoConnection.ts
@@ -1,4 +1,5 @@
 import * as mongoDB from "mongodb";
+import * as Sentry from "@sentry/node";
 import { MONGO_DBNAME, MONGO_URL, MONGO_PASSWORD, MONGO_USER } from "../configurations/mongo.conf";
 
 const mongoClient: mongoDB.MongoClient = new mongoDB.MongoClient(MONGO_URL, {
@@ -34,3 +35,5 @@ const events: string[] = [
 for (const eventName of events) {
     client.on(eventName, data => console.log(eventName, data));
 }
+
+client.on("serverHeartbeatFailed", data => Sentry.captureException(new Error("mongo failed heartbeat"), { data }));
