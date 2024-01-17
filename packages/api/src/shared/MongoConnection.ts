bad833b4f30e4e4496c42eb5e2e38ccb2432114e
alice-telescoop <94919029+alice-telescoop@users.noreply.github.com>
2024-01-17 15:50:50 +0100

    feat(api): adds logs to mongo connection (#2041)
    * feat(api): monitor (a lot) db connection
    
    * feat(api): sentry error on failed heartbeat

--- packages/api/src/shared/MongoConnection.ts
+++ packages/api/src/shared/MongoConnection.ts
@@ -1,4 +1,5 @@
 import * as mongoDB from "mongodb";
+import * as Sentry from "@sentry/node";
 import { MONGO_DBNAME, MONGO_URL, MONGO_PASSWORD, MONGO_USER } from "../configurations/mongo.conf";
 
 const mongoClient: mongoDB.MongoClient = new mongoDB.MongoClient(MONGO_URL, {
@@ -21,3 +22,18 @@ export const connectDB = () =>
 export const client = mongoClient;
 
 export default mongoClient.db(MONGO_DBNAME);
+
+const events: string[] = [
+    "serverOpening",
+    "serverClosed",
+    "serverDescriptionChanged",
+    "topologyOpening",
+    "topologyClosed",
+    "topologyDescriptionChanged",
+    "serverHeartbeatFailed",
+];
+for (const eventName of events) {
+    client.on(eventName, data => console.log(eventName, data));
+}
+
+client.on("serverHeartbeatFailed", data => Sentry.captureException(new Error("mongo failed heartbeat"), { data }));
