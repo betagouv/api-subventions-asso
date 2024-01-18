76a1e4f0169ad104b3e17fa0d7ca93fdbba758db
Maxime Silvère Meunier <mxmeunier@gmail.com>
2024-01-18 13:50:06 +0100

    feat(front): test identifierHelper

--- packages/front/src/lib/helpers/identifierHelper.test.ts
+++ packages/front/src/lib/helpers/identifierHelper.test.ts
@@ -1,25 +1,62 @@
 import * as IdentifierHelper from "./identifierHelper";
-vi.mock("./identifierHelper", async () => ({
-    original: await vi.importActual("./identifierHelper"),
-    isRna: vi.fn().mockReturnValue(false),
-    isSiren: vi.fn().mockReturnValue(false),
-    isSiret: vi.fn().mockReturnValue(false),
-    isIdentifier: vi.fn(),
-}));
-const mockedIdentifierHelper = vi.mocked(IdentifierHelper);
 
 describe("IdentifierHelper", () => {
-    describe("isIdentifier()", () => {
-        beforeAll(async () => {
-            // @ts-expect-error: add to mock
-            mockedIdentifierHelper.isIdentifier = mockedIdentifierHelper.original.isIdentifier;
-            mockedIdentifierHelper.isRna.mockReturnValue(true);
+    describe("isRna", () => {
+        it.each`
+            rna
+            ${"W000000000"}
+            ${"W0P0000000"}
+        `("should return true", ({ rna }) => {
+            const expected = true;
+            const actual = IdentifierHelper.isRna(rna);
+            expect(actual).toEqual(expected);
         });
 
-        it("should write test", () => {
-            expect(true).toBe(true);
+        it.each`
+            rna
+            ${"W00000000"}
+            ${"WP00000000"}
+            ${"W00P000000"}
+        `("should return false", ({ rna }) => {
+            const expected = false;
+            const actual = IdentifierHelper.isRna(rna);
+            expect(actual).toEqual(expected);
         });
-
-        // find a way to test isIdentifier but with mocked isRna and other named exported methods
     });
+
+    describe("isSiren", () => {
+        it("should return true", () => {
+            const validSiren = "125896475";
+            const expected = true;
+            const actual = IdentifierHelper.isSiren(validSiren);
+            expect(actual).toEqual(expected);
+        });
+
+        it("should return false", () => {
+            const validSiren = "A896475";
+            const expected = false;
+            const actual = IdentifierHelper.isSiren(validSiren);
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    describe("isSiret", () => {
+        it("should return true", () => {
+            const validSiret = "12589647500018";
+            const expected = true;
+            const actual = IdentifierHelper.isSiret(validSiret);
+            expect(actual).toEqual(expected);
+        });
+
+        it("should return false", () => {
+            const validSiret = "Z2589647500018";
+            const expected = false;
+            const actual = IdentifierHelper.isSiret(validSiret);
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    // find a way to test isIdentifier but with mocked isRna and other named exported methods
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    describe.skip("isIdentifier()", () => {});
 });
