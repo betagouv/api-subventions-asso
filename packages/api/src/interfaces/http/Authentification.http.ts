818ed0505848d4a548db3d855e4ee7030b44c061
Maxime Silvère Meunier <mxmeunier@gmail.com>
2024-01-25 16:00:52 +0100

    fix: disable secure cookie in dev mode

--- packages/api/src/interfaces/http/Authentification.http.ts
+++ packages/api/src/interfaces/http/Authentification.http.ts
@@ -48,7 +48,7 @@ export class AuthentificationHttp extends Controller {
         if (req.user) {
             // Successfully logged
             req.res?.cookie("token", req.user.jwt.token, {
-                secure: true,
+                secure: process.env.NODE_ENV === "production" ? true : false,
                 sameSite: "none",
                 domain: DOMAIN,
                 expires: req.user.jwt.expirateDate,
