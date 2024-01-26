c240ca5d8adbc0ecf76965e472e13d4d99d85c9e
Maxime Silvère Meunier <mxmeunier@gmail.com>
2024-01-26 14:49:06 +0100

    test(front): documents.service formatAndSortDocuments

--- packages/front/src/lib/services/documents.service.test.ts
+++ packages/front/src/lib/services/documents.service.test.ts
@@ -3,9 +3,27 @@ import documentService from "./documents.service";
 vi.mock("$env/static/public", () => ({ DATASUB_URL: "https://api.fr" }));
 
 describe("DocumentService", () => {
-    const DOC_URL = "/path";
+    const DOCUMENTS = [
+        { type: "RIB", url: "/rib" },
+        { type: "RAR", url: "/rar" },
+    ];
 
     describe("formatAndSortDocuments", () => {
-        documentService.formatAndSortDocuments([]);
+        it("should return formated and sorted documents", () => {
+            const expected = [
+                {
+                    label: "Rapport d'activité",
+                    type: "RAR",
+                    url: "https://api.fr/rar",
+                },
+                {
+                    label: "RIB",
+                    type: "RIB",
+                    url: "https://api.fr/rib",
+                },
+            ];
+            const actual = documentService.formatAndSortDocuments(DOCUMENTS);
+            expect(actual).toEqual(expected);
+        });
     });
 });
