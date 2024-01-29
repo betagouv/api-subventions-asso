c0f342cc8897f62e919d9d0f29682654b83d689f
alice.telescoop <alice@telescoop.fr>
2024-01-29 12:18:57 +0100

    docs(api): report api doc type meaning

--- packages/api/src/modules/providers/apiAsso/apiAsso.service.ts
+++ packages/api/src/modules/providers/apiAsso/apiAsso.service.ts
@@ -25,6 +25,7 @@ export class ApiAssoService
     extends ProviderCore
     implements AssociationsProvider, EtablissementProvider, DocumentProvider
 {
+    // API documented in part "contrat d'interface" https://lecompteasso.associations.gouv.fr/lapi-association/
     private requestCache = new CacheData<unknown>(CACHE_TIMES.ONE_DAY);
 
     constructor() {
@@ -124,7 +125,13 @@ export class ApiAssoService
     }
 
     private filterRnaDocuments(documents: StructureRnaDocumentDto[]) {
-        const acceptedType = ["MD", "LDC", "PV", "STC"];
+        const acceptedType = [
+            "MD", // récépissé de modification
+            "CR", //"Récépissé de création"
+            "LDC", // liste des dirigeants
+            "PV", // procès verbal
+            "STC", // statuts
+        ];
 
         const sortByYearAndTimeAsc = (a: StructureRnaDocumentDto, b: StructureRnaDocumentDto) => {
             return parseFloat(`${a.annee}.${a.time}`) - parseFloat(`${b.annee}.${b.time}`);
@@ -143,14 +150,14 @@ export class ApiAssoService
 
     private filterDacDocuments(documents: StructureDacDocumentDto[]) {
         const acceptedType = [
-            "RFA",
-            "BPA",
-            "RCA",
-            "RAR",
+            "RFA", // rapports financier ou moral
+            "BPA", // budget prévisionnel annuel
+            "RCA", // Rapport du commissaire aux compte
+            "RAR", // Rapport d'activité
             "CAP",
             "Jeunesse et Education Populaire (JEP)",
             "Education nationale",
-            "Formation",
+            "Formation", // L'habilitation d'organisme de formation
         ];
 
         const sortByTimeDepotAsc = (a: StructureDacDocumentDto, b: StructureDacDocumentDto) =>
@@ -190,7 +197,9 @@ export class ApiAssoService
                 // When api have one document, it is not an array but a single object
                 documents = [documents];
             } else {
-                const errorMessage = "API-ASSO structure do not contain documents or format is not supported. Structure identifier => " + structureIdentifier;
+                const errorMessage =
+                    "API-ASSO structure do not contain documents or format is not supported. Structure identifier => " +
+                    structureIdentifier;
                 Sentry.captureException(new Error(errorMessage));
                 console.error(errorMessage);
                 return [];
