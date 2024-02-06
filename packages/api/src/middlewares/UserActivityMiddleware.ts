e28fbee73e7b72006238917628cc8304eb296ccc
alice.telescoop <alice@telescoop.fr>
2024-02-06 12:29:44 +0100

    fix(api): lastActivityDate also on admins

--- packages/api/src/middlewares/UserActivityMiddleware.ts
+++ packages/api/src/middlewares/UserActivityMiddleware.ts
@@ -1,10 +1,9 @@
 import { NextFunction, Response } from "express";
-import { isRequestFromAdmin } from "../shared/helpers/HttpHelper";
 import { IdentifiedRequest } from "../@types";
 import userCrudService from "../modules/user/services/crud/user.crud.service";
 
 export default function UserActivityMiddleware(req: IdentifiedRequest, _res: Response, next: NextFunction) {
-    if (!req.user || isRequestFromAdmin(req)) return next();
+    if (!req.user) return next();
     const user = req.user;
     user.lastActivityDate = new Date();
 
