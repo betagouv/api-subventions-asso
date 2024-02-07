d467f227b2e7770543ed00d99bf9490693ba888b
alice.telescoop <alice@telescoop.fr>
2024-02-07 16:42:15 +0100

    fix(api): actual migration to fill $lastActivityDate

---
+++ packages/api/migrations/20240207101011-lastActivityDate-reviewed.js
@@ -0,0 +1,52 @@
+module.exports = {
+    async up(db) {
+        await db
+            .collection("log")
+            .aggregate([
+                { $group: { _id: "$meta.req.user.email", lastActivityDate: { $max: "$timestamp" } } },
+                { $out: "log-last-activity" },
+            ])
+            .toArray();
+
+        await db
+            .collection("users")
+            .aggregate([
+                {
+                    $lookup: {
+                        from: "log-last-activity",
+                        localField: "email",
+                        foreignField: "_id",
+                        as: "lastActivityLog",
+                    },
+                },
+                { $unwind: { path: "$lastActivityLog", preserveNullAndEmptyArrays: true } },
+                {
+                    $addFields: {
+                        lastActivityDate: {
+                            $ifNull: [
+                                "$lastActivityDate",
+                                {
+                                    $ifNull: [
+                                        "$lastActivityLog.lastActivityDate",
+                                        { $cond: { if: { $eq: ["$active", true] }, then: "$signupAt", else: null } },
+                                    ],
+                                },
+                            ],
+                        },
+                    },
+                },
+                { $project: { lastActivityLog: 0 } },
+                { $out: "users-last-activity" },
+            ])
+            .toArray();
+
+        await db.collection("users-last-activity").createIndex({ email: 1 }, { unique: true });
+        await db.renameCollection("users", "users-save");
+        await db.renameCollection("users-last-activity", "users");
+        await db.dropCollection("logs");
+    },
+
+    async down(db) {
+        await db.dropCollection("log-last-activity");
+    },
+};
