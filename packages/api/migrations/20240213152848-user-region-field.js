9dcd31a1c0ab2ad9b4fad857f485c3b434e9352a
alice.telescoop <alice@telescoop.fr>
2024-02-14 17:35:19 +0100

    fix(api): migration uses execution stack

--- packages/api/migrations/20240213152848-user-region-field.js
+++ packages/api/migrations/20240213152848-user-region-field.js
@@ -1,19 +1,28 @@
 const userProfileService = require("../build/src/modules/user/services/profile/user.profile.service").default;
 const geoService = require("../build/src/modules/providers/geoApi/geo.service").default;
 const { connectDB } = require("../build/src/shared/MongoConnection");
+const ExecutionSyncStack = require("../build/src/shared/ExecutionSyncStack");
 
 module.exports = {
     async up(db) {
+        // use task queue
         const collection = db.collection("users");
         await connectDB();
 
+        const updateContactsStack = new ExecutionSyncStack(
+            user => userProfileService.profileUpdate(user, user),
+            100, // brevo limits calls to /contacts to 10 per second so 100 per ms https://developers.brevo.com/docs/api-limits#general-rate-limiting
+        );
+
         await geoService.generateAndSaveEntities();
+        const promises = [];
 
         // deduces region for decentralized agents
         const usersCursor = collection.find({ disable: { $ne: true }, decentralizedTerritory: { $exists: true } });
         for await (const user of usersCursor) {
-            await userProfileService.profileUpdate(user, user);
+            promises.push(updateContactsStack.addOperation(user));
         }
+        await Promise.all(promises);
     },
 
     async down(db) {
