8a6e681e3a46b079c359e97f71f68008dac8d5c4
Maxime Silvère Meunier <mxmeunier@gmail.com>
2024-02-14 11:12:35 +0100

    feat(front): add filter for estabs on siret and postal code

--- packages/front/src/lib/components/SearchBar/SearchBar.svelte
+++ packages/front/src/lib/components/SearchBar/SearchBar.svelte
@@ -1,21 +1,42 @@
 <script>
-    export let value;
+    import { createEventDispatcher } from "svelte";
+    // build a unique id from timestamp if not given
+    export let id = Date.now().toString();
+    export let large = true;
+    export let placeholder = "Nom, n°RNA, n°SIREN ou SIRET";
+    export let disableIfEmpty = true;
+    export let value = undefined;
+    export let label = undefined;
+
+    const dispatch = createEventDispatcher();
+
+    async function handleReset() {
+        if (value === "") dispatch("reset");
+    }
+
+    function handleSubmit() {
+        dispatch("submit", value);
+    }
 </script>
 
 <div class="fr-grid-row fr-grid-row--center fr-grid-row--gutters">
     <div class="fr-col fr-col-lg-12">
-        <form on:submit|preventDefault>
-            <div class="fr-search-bar fr-search-bar--lg" id="search-input">
+        <form on:submit|preventDefault={handleSubmit}>
+            <div class="fr-search-bar" class:fr-search-bar--lg={large} id="search-input-{id}">
+                {#if label}
+                    <label class="fr-label" for="search-input-{id}">
+                        {label}
+                    </label>
+                {/if}
                 <input
                     class="fr-input"
-                    placeholder="Nom, n°RNA, n°SIREN ou SIRET"
+                    {placeholder}
                     type="search"
-                    id="search-input-input"
+                    id="search-input-{id}"
                     name="search-input"
-                    bind:value />
-                <button class="fr-btn" title="Rechercher" id="search-input-button" type="submit" disabled={!value}>
-                    Rechercher
-                </button>
+                    bind:value
+                    on:input={handleReset} />
+                <button class="fr-btn" title="Rechercher" disabled={!value && disableIfEmpty}>Rechercher</button>
             </div>
         </form>
     </div>
