024a319feb6951ff1238cd9d3083c80bb91134e1
Victor Serain <victor.serain@epitech.eu>
2024-02-15 10:01:54 +0100

    fix(api): fix redirect for post method

--- packages/api/src/middlewares/VersionMiddleware.ts
+++ packages/api/src/middlewares/VersionMiddleware.ts
@@ -1,9 +1,11 @@
-export const versionMiddleware = async (req, _res, next) => {
+import { Response } from "express";
+
+export const versionMiddleware = async (req, _res: Response, next) => {
     let version = req.url.split("/")[1];
     if (!["v1", "v2"].includes(version)) {
         console.log("redirecting to default API - V2");
         version = "v2";
-        _res.redirect(`/v2${req.url}`);
+        _res.redirect(307, `/v2${req.url}`);
     } else {
         req.apiVersion = version;
         console.log(req.url);
