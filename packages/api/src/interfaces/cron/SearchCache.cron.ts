3b07184094497728d88822853679021f302aa9a2
alice.telescoop <alice@telescoop.fr>
2024-02-16 15:22:15 +0100

    feat(api): cron empties cache each night

---
+++ packages/api/src/interfaces/cron/SearchCache.cron.ts
@@ -0,0 +1,13 @@
+import { AsyncCron } from "../../decorators/cronController.decorator";
+import { CronController } from "../../@types/cron";
+import searchService from "../../modules/search/search.service";
+
+export class SearchCacheCron implements CronController {
+    name = "searchCacheCron";
+
+    // every day at 11 PM UTC
+    @AsyncCron({ cronExpression: "0 23 * * *" })
+    removeInactiveUsers() {
+        return searchService.cleanCache();
+    }
+}
