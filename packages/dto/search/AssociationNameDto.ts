ccc3896eb1ad1fb744516820f90fee0e842eb1d1
alice.telescoop <alice@telescoop.fr>
2024-02-16 15:03:00 +0100

    fix(dto): also address & nbEtabs

--- packages/dto/search/AssociationNameDto.ts
+++ packages/dto/search/AssociationNameDto.ts
@@ -1,7 +1,9 @@
-import { Rna, Siren } from "../shared";
+import { Adresse, Rna, Siren } from "../shared";
 
 export interface AssociationNameDto {
     siren: Siren;
     name: string;
     rna?: Rna;
+    address?: Adresse;
+    nbEtabs?: number;
 }
