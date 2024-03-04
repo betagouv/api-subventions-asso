dc5ecbdc34a580587b90b7d23c7a9c8c675bcd07
alice.telescoop <alice@telescoop.fr>
2024-03-04 17:37:55 +0100

    test(front): search controller pagination

--- packages/front/src/routes/search/[name]/Search.controller.test.ts
+++ packages/front/src/routes/search/[name]/Search.controller.test.ts
@@ -1,29 +1,37 @@
+import type { SpyInstance } from "vitest";
+
 import SearchController from "./Search.controller";
 import * as IdentifierHelper from "$lib/helpers/identifierHelper";
 
 vi.mock("$lib/helpers/identifierHelper");
 const mockedIdentifierHelper = vi.mocked(IdentifierHelper);
 import associationService from "$lib/resources/associations/association.service";
+import { isSiret } from "$lib/helpers/identifierHelper";
+import { encodeQuerySearch } from "$lib/helpers/urlHelper";
 
 vi.mock("$lib/resources/associations/association.service");
 const mockedAssociationService = vi.mocked(associationService);
+vi.mock("$lib/helpers/urlHelper");
+vi.mock("$lib/helpers/identifierHelper");
 vi.mock("$app/navigation");
 
 describe("SearchController", () => {
-    describe("fetchAssociationFromName()", () => {
-        const RNA = "RNA";
-        const RNA_SIREN_1 = { rna: RNA, siren: "SIREN_1", name: "NOM" };
-        const RNA_SIREN_2 = { rna: RNA, siren: "SIREN_2", name: "NOM" };
-        const DUPLICATED_RESULTS = {
-            nbPages: 1,
-            page: 1,
-            results: [RNA_SIREN_1, RNA_SIREN_2],
-            total: 2,
-        };
+    const RNA = "RNA";
+    const RNA_SIREN_1 = { rna: RNA, siren: "SIREN_1", name: "NOM" };
+    const RNA_SIREN_2 = { rna: RNA, siren: "SIREN_2", name: "NOM" };
+    const DUPLICATED_RESULTS = {
+        nbPages: 1,
+        page: 1,
+        results: [RNA_SIREN_1, RNA_SIREN_2],
+        total: 2,
+    };
+    beforeAll(() => {
+        mockedAssociationService.search.mockResolvedValue(DUPLICATED_RESULTS);
+    });
 
+    describe("fetchAssociationFromName()", () => {
         it("should set duplicate store", async () => {
             const expected = [RNA_SIREN_1.siren, RNA_SIREN_2.siren];
-            mockedAssociationService.search.mockResolvedValueOnce(DUPLICATED_RESULTS);
             mockedIdentifierHelper.isRna.mockReturnValue(true);
             const controller = new SearchController(RNA);
             await new Promise(process.nextTick);
@@ -34,14 +42,65 @@ describe("SearchController", () => {
 
         it("should unset duplicate store on second search, with name", async () => {
             const expected = null;
-            mockedAssociationService.search.mockResolvedValueOnce(DUPLICATED_RESULTS);
             mockedIdentifierHelper.isRna.mockReturnValueOnce(true);
-            mockedAssociationService.search.mockResolvedValueOnce(DUPLICATED_RESULTS);
             const controller = new SearchController(RNA);
             await new Promise(process.nextTick);
             await controller.fetchAssociationFromName("name");
             const actual = controller.duplicatesFromIdentifier.value;
             expect(actual).toEqual(expected);
+        });
+
+        it("should call with required page", async () => {
+            const PAGE = 42;
+            const controller = new SearchController(RNA);
+            await new Promise(process.nextTick);
+            await controller.fetchAssociationFromName("name", PAGE);
+            expect(mockedAssociationService.search).toHaveBeenCalledWith("name", PAGE);
+        });
+
+        it("should call with default page", async () => {
+            const PAGE = 1;
+            const controller = new SearchController(RNA);
+            await new Promise(process.nextTick);
+            await controller.fetchAssociationFromName("name");
+            expect(mockedAssociationService.search).toHaveBeenCalledWith("name", PAGE);
+        });
+    });
+
+    describe("onSubmit", () => {
+        let controller: SearchController;
+        let goToEstabSpy: SpyInstance;
+        let fetchSpy: SpyInstance;
+
+        beforeAll(() => {
+            controller = new SearchController(RNA);
+            goToEstabSpy = vi.spyOn(controller, "gotoEstablishment");
+            // @ts-expect-error -- mock
+            fetchSpy = vi.spyOn(controller, "fetchAssociationFromName").mockResolvedValue(null);
+        });
+
+        afterAll(() => {
+            fetchSpy.mockRestore();
+        });
+
+        it("if input is siret, call goToEstablishment", () => {
+            vi.mocked(isSiret).mockReturnValueOnce(true);
+            controller.onSubmit(RNA);
+            expect(goToEstabSpy).toHaveBeenCalledWith(RNA);
+            expect(fetchSpy).not.toHaveBeenCalled();
+        });
+
+        it("if input is not siret, encode query search", () => {
+            vi.mocked(isSiret).mockReturnValueOnce(false);
+            controller.onSubmit(RNA);
+            expect(encodeQuerySearch).toHaveBeenCalledWith(RNA);
+        });
+
+        it("if input is not siret, fetch new result on page one", () => {
+            vi.mocked(isSiret).mockReturnValueOnce(false);
+            vi.mocked(encodeQuerySearch).mockReturnValueOnce("ENCODÉ");
+            controller.onSubmit(RNA);
+            expect(fetchSpy).toHaveBeenCalledWith("ENCODÉ", 1);
         });
     });
 });
