2bcb60a84432229ea33e02426b7f33e915757407
Maxime Silvère Meunier <mxmeunier@gmail.com>
2024-03-05 09:15:51 +0100

    refactor(front): remove custom banner color

--- packages/front/src/lib/dsfr/InformationBanner.svelte
+++ packages/front/src/lib/dsfr/InformationBanner.svelte
@@ -6,7 +6,7 @@
     const dispatch = createEventDispatcher();
 </script>
 
-<div class="fr-notice fr-notice--info override">
+<div class="fr-notice fr-notice--info">
     <div class="fr-container">
         <div class="fr-notice__body">
             <!-- eslint-disable-next-line svelte/no-at-html-tags ok because defined in component itself -->
@@ -21,9 +21,3 @@
         </div>
     </div>
 </div>
-
-<style>
-    .override {
-        color: var(--text-action-high-blue-france);
-    }
-</style>
