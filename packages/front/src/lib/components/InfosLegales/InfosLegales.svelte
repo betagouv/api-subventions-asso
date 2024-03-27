87438f32912b5072cc76cf4f49555a6724d72fd8
Maxime Silvère Meunier <mxmeunier@gmail.com>
2024-03-27 11:06:25 +0100

    feat(front): refactor legal info structure and add bordure bottom to action

--- packages/front/src/lib/components/InfosLegales/InfosLegales.svelte
+++ packages/front/src/lib/components/InfosLegales/InfosLegales.svelte
@@ -9,48 +9,61 @@
 </script>
 
 <div class="fr-grid-row fr-p-4w background-default">
-    <div class="fr-col fr-col-md-6 fr-pr-2w">
+    <div class="fr-col-md-6 fr-pr-2w">
         <p class="fr-text--lg fr-text--bold">Objet social</p>
         <p>{controller.objetSocial}</p>
     </div>
-    <div class="fr-col fr-col-md-6 fr-pl-2w">
-        <table class="fr-mb-1v">
-            <tr>
-                <td class="fr-pb-2w fr-text--lg fr-text--bold fr-pr-2w">{controller.siret.title}</td>
-                <td class="fr-pb-2w fr-text--sm">
-                    {controller.siret.value}
-                </td>
-            </tr>
-            <tr>
-                <td class="fr-pb-2w fr-text--lg fr-text--bold fr-pr-2w">{controller.address.title}</td>
-                <td class="fr-pb-2w fr-text--sm">
-                    {controller.address.value}
-                </td>
-            </tr>
-            <tr>
-                <td class="fr-pb-2w fr-text--lg fr-text--bold fr-pr-2w">Date d'immatriculation</td>
-                <td class="fr-pb-2w fr-text--sm">
-                    {controller.immatriculation}
-                </td>
-            </tr>
-            <tr>
-                <td class="fr-pb-2w fr-text--lg fr-text--bold fr-pr-2w">Date de modification</td>
-                <td class="fr-pb-2w fr-text--sm">
-                    {controller.modification}
-                </td>
-            </tr>
-        </table>
+    <div class="fr-col-md-6 fr-pl-2w">
+        <div class="fr-grid-row">
+            <div class="fr-col-6 fr-text--lg fr-text--bold">
+                {controller.siret.title} :
+            </div>
+            <div class="fr-col-6 fr-text--md">
+                {controller.siret.value}
+            </div>
+        </div>
+        <div class="fr-grid-row">
+            <div class="fr-col-6 fr-text--lg fr-text--bold">
+                {controller.address.title} :
+            </div>
+            <div class="fr-col-6 fr-text--md">
+                {controller.address.value}
+            </div>
+        </div>
+        <div class="fr-grid-row">
+            <div class="fr-col-6 fr-text--lg fr-text--bold">Date d'immatriculation :</div>
+            <div class="fr-col-6 fr-text--md">
+                {controller.immatriculation}
+            </div>
+        </div>
+        <div class="fr-grid-row">
+            <div class="fr-col-6 fr-text--lg fr-text--bold">Date de modification :</div>
+            <div class="fr-col-6 fr-text--md">
+                {controller.modification}
+            </div>
+        </div>
         {#if !establishment}
-            <Button
-                type="tertiary"
-                size="small"
-                icon="information-line"
-                iconPosition="left"
-                outline={false}
-                ariaControls="fr-modal"
-                on:click={() => controller.displayModal()}>
-                Voir les informations du RNA / Siren
-            </Button>
+            <!-- <div> -->
+            <div class="underline-button">
+                <Button
+                    type="tertiary"
+                    size="small"
+                    icon="information-line"
+                    iconPosition="left"
+                    outline={false}
+                    ariaControls="fr-modal"
+                    on:click={() => controller.displayModal()}>
+                    Plus de détails
+                </Button>
+            </div>
+            <!-- </div> -->
         {/if}
     </div>
 </div>
+
+<style>
+    .underline-button {
+        max-width: fit-content;
+        border-bottom: 1px blue solid;
+    }
+</style>
