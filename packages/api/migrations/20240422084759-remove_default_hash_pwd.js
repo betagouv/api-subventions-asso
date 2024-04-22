da4cedf849bec46f9575b2521df40ea255321417
alice.telescoop <alice@telescoop.fr>
2024-04-22 11:32:56 +0200

    feat(api): no default password

---
+++ packages/api/migrations/20240422084759-remove_default_hash_pwd.js
@@ -0,0 +1,30 @@
+const { DEFAULT_PWD } = require("../build/src/modules/user/user.constant");
+const { getHashPassword } = require("../build/src/modules/user/services/auth/user.auth.service");
+
+module.exports = {
+    async up(db) {
+        await db.collection("users").updateMany(
+            {
+                active: false,
+                disable: { $ne: true },
+                agentConnectId: { $ne: undefined },
+                lastActivityDate: undefined,
+            },
+            { $unset: "hashPassword" },
+        );
+    },
+
+    async down(db) {
+        const defaultHashPassword = await getHashPassword(DEFAULT_PWD);
+        await db.collection("users").updateMany(
+            {
+                active: false,
+                disable: { $ne: true },
+                hashPassowrd: undefined,
+                lastActivityDate: undefined,
+                agentConnectId: { $ne: undefined },
+            },
+            { $set: { hashPassword: defaultHashPassword } },
+        );
+    },
+};
