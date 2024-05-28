0c25211199a6e42bcb2061889893c209fe22199a
alice.telescoop <alice@telescoop.fr>
2024-05-28 18:08:01 +0200

    feat(front): label docs instead of filtering them

--- packages/front/src/lib/resources/document/document.service.ts
+++ packages/front/src/lib/resources/document/document.service.ts
@@ -2,13 +2,16 @@ import type { Siret } from "dto";
 import documentPort from "$lib/resources/document/document.port";
 import type { DocumentEntity } from "$lib/entities/DocumentEntity";
 
+export type LabeledDoc = DocumentEntity & { showByDefault: boolean };
+
 export class DocumentService {
     getAllDocs(identifier: string) {
         return documentPort.getAllDocs(identifier);
     }
 
-    filterAssoDocsBySiret(docs: DocumentEntity[], siret: Siret) {
-        return docs.filter(doc => !doc.__meta__.siret || doc.__meta__.siret === siret);
+    labelAssoDocsBySiret(docs: DocumentEntity[], siret: Siret): LabeledDoc[] {
+        docs.forEach(doc => ((doc as LabeledDoc).showByDefault = !doc.__meta__.siret || doc.__meta__.siret === siret));
+        return docs as LabeledDoc[];
     }
 }
 
