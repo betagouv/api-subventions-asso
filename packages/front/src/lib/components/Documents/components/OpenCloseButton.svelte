e475545719ad17c488dc3e127eceb6514b93108b
alice-telescoop <94919029+alice-telescoop@users.noreply.github.com>
2024-05-30 15:55:25 +0200

    feat(front): #2311 show all docs button (#2406)
    * refactor(front): #2356 redirect through localStorage (#2390)
    
    * refactor(front): redirect through localStorage
    
    * chore(front): clean no need for this promise
    
    * test(front): redirect through localStorage
    
    * fix(front): no need for JSON parsing/stringifying
    
    * chore(front): move redundant browser call
    
    * test(front): misc fixes
    
    * refactor(front): extract doc filtering
    
    * feat(front): accordeon dsfr component
    
    * feat(front): custom accordeon dsfr component for docs
    
    * feat(front): show only some docs or all other docs
    
    * test(front): show more behaviour
    
    * feat(front): label docs instead of filtering them
    
    * test(front): clean old skipped tests
    
    * chore(front): clean verbose methods and old #key
    
    * refactor(front): less repetition to organize docs
    
    * chore(front): fix types
    
    * feat(front): extract switch method to controller
    
    * refactor(front): simplify component
    
    * docs(front): clarify comment
    
    (cherry picked from commit 90d1215be5de2f1d493d7a59c8ff6471484820d7)

---
+++ packages/front/src/lib/components/Documents/components/OpenCloseButton.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import { nanoid } from "nanoid";
+
+    export let expanded: boolean;
+    export let id = nanoid(7);
+    export let toCloseLabel = "Masquer";
+    export let toOpenLabel = "Afficher plus";
+</script>
+
+<section class="fr-accordion">
+    <div class="fr-collapse fr-p-0" id="accordion-{id}">
+        <slot />
+    </div>
+    <h3 class="fr-accordion__title">
+        <button class="fr-accordion__btn" aria-expanded={expanded} aria-controls="accordion-{id}" on:click>
+            <div class="fr-ml-auto">
+                {#if expanded}
+                    {toCloseLabel}
+                {:else}
+                    {toOpenLabel}
+                {/if}
+            </div>
+        </button>
+    </h3>
+</section>
+
+<style>
+    .fr-accordion:before {
+        box-shadow: none;
+    }
+
+    .fr-accordion__btn {
+        display: flex;
+    }
+
+    .fr-accordion__btn:after {
+        margin-left: 0.5rem;
+        margin-right: auto;
+    }
+</style>
--- packages/front/src/lib/resources/document/document.service.ts
+++ packages/front/src/lib/resources/document/document.service.ts
@@ -1,8 +1,17 @@
+import type { Siret } from "dto";
 import documentPort from "$lib/resources/document/document.port";
+import type { DocumentEntity } from "$lib/entities/DocumentEntity";
+
+export type LabeledDoc = DocumentEntity & { showByDefault: boolean };
 
 export class DocumentService {
     getAllDocs(identifier: string) {
         return documentPort.getAllDocs(identifier);
+    }
+
+    labelAssoDocsBySiret(docs: DocumentEntity[], siret: Siret): LabeledDoc[] {
+        docs.forEach(doc => ((doc as LabeledDoc).showByDefault = !doc.__meta__.siret || doc.__meta__.siret === siret));
+        return docs as LabeledDoc[];
     }
 }
 
