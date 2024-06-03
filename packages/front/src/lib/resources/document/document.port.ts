c4a9b9e61120dbb0c583f4904b395ab86df6af70
alice.telescoop <alice@telescoop.fr>
2024-06-03 16:07:22 +0200

    feat(front): get some docs service port and adapter

--- packages/front/src/lib/resources/document/document.port.ts
+++ packages/front/src/lib/resources/document/document.port.ts
@@ -1,9 +1,15 @@
+import type { DocumentRequestDto } from "dto";
 import requestsService from "$lib/services/requests.service";
 
 class DocumentPort {
-    async getAllDocs(identifier): Promise<Blob> {
+    async getAllDocs(identifier: string): Promise<Blob> {
         const path = `document/downloads/${identifier}`;
         return (await requestsService.get(path, {}, { responseType: "blob" }))?.data;
+    }
+
+    async getSomeDocs(docRequests: DocumentRequestDto[]): Promise<Blob> {
+        const path = `document/downloads}`;
+        return (await requestsService.post(path, docRequests, { responseType: "blob" }))?.data;
     }
 }
 
