c4a1d588fa01277ad3069c68d8f0e75070edfdf6
alice.telescoop <alice@telescoop.fr>
2024-06-04 16:33:17 +0200

    feat(front): document card selection logic

--- packages/front/src/lib/components/Documents/components/DocumentCard.controller.ts
+++ packages/front/src/lib/components/Documents/components/DocumentCard.controller.ts
@@ -1,6 +1,16 @@
 import trackerService from "$lib/services/tracker.service";
+import Store from "$lib/core/Store";
+import type { DocumentEntity } from "$lib/entities/DocumentEntity";
 
 export class DocumentCardController {
+    public isSelected: Store<boolean>;
+    public checkBoxId: string;
+
+    constructor(document: DocumentEntity) {
+        this.isSelected = new Store(false);
+        this.checkBoxId = document.nom + document.date;
+    }
+
     getDateString(date) {
         if (date.getTime() === 0) return "Date de dépôt non disponible";
         return `Déposé le ${date.toLocaleDateString()}`;
--- packages/front/src/lib/components/Documents/components/DocumentCard.svelte
+++ packages/front/src/lib/components/Documents/components/DocumentCard.svelte
@@ -1,40 +1,91 @@
-<script>
+<script lang="ts">
     import { DocumentCardController } from "./DocumentCard.controller";
+    import type { DocumentEntity } from "$lib/entities/DocumentEntity";
 
-    export let document;
-    export let isSelected = false;
-    const controller = new DocumentCardController();
+    export let document: DocumentEntity;
+    export let value: DocumentEntity | undefined = undefined;
+
+    const controller = new DocumentCardController(document);
+    const { isSelected } = controller;
+    $: value = $isSelected ? document : undefined; // here because it would be very heavy to go through controller
 </script>
 
-<div class="fr-col-12">
-    <div class="fr-card fr-card--no-icon fr-card--horizontal fr-card--no-border" class:--active={isSelected}>
-        <div class="fr-card__body">
-            <div class="fr-card__content fr-p-2w">
-                <div>
-                    <h4 class="fr-card__title fr-h5">{document.label}</h4>
-                    <p class="fr-card__desc fr-hint-text fr-mt-2v fr-mb-0">
-                        {controller.getDateString(document.date)}
-                        ― Fourni par :
-                        <b>{document.provider}</b>
-                        <!-- TODO  ― {document.format} - {document.size} -->
-                    </p>
-                </div>
-                <div class="fr-ml-auto fr-my-auto">
-                    <a
-                        class="fr-link fr-links-group__title"
-                        href={document.url}
-                        title="Télécharger "
-                        target="_blank"
-                        rel="noopener noreferrer">
-                        <span class="fr-icon-file-download-line" />
-                    </a>
+<div class="card-container fr-grid-row fr-col-12" class:--selected={$isSelected}>
+    <div class="fr-grid-row fr-col-11 checkbox-wrapper">
+        <div class="fr-col-1 flex">
+            <div class="fr-mt-5v">
+                <div class="fr-checkbox-group document-selector">
+                    <input
+                        name="documents-to-download"
+                        id="documents-to-download-{controller.checkBoxId}"
+                        type="checkbox"
+                        bind:checked={$isSelected} />
+                    <label class="fr-label sr-only" for="documents-to-download-{controller.checkBoxId}">
+                        <!-- TODO infos document lecteur d'écran -->
+                    </label>
                 </div>
             </div>
+        </div>
+        <div class="fr-col-11">
+            <div class="fr-card fr-card--no-icon fr-card--horizontal fr-card--no-border">
+                <div class="fr-card__body">
+                    <div class="fr-card__content fr-p-2w">
+                        <div>
+                            <h4 class="fr-card__title fr-h5">{document.label}</h4>
+                            <p class="fr-card__desc fr-hint-text fr-mt-2v fr-mb-0">
+                                {controller.getDateString(document.date)}
+                                ― Fourni par :
+                                <b>{document.provider}</b>
+                                <!-- TODO  ― {document.format} - {document.size} -->
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="fr-col-1 flex download-wrapper">
+        <div class="fr-m-auto">
+            <a
+                class="fr-link fr-links-group__title"
+                href={$isSelected ? undefined : document.url}
+                title="Télécharger"
+                target="_blank"
+                rel="noopener noreferrer">
+                <span class="fr-icon-file-download-line" />
+            </a>
         </div>
     </div>
 </div>
 
 <style>
+    .checkbox-wrapper {
+        position: relative;
+    }
+
+    .document-selector {
+        opacity: 0;
+    }
+
+    .document-selector input ~ label::after {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: 10;
+    }
+
+    .card-container:hover .document-selector,
+    .card-container:focus-within .document-selector,
+    .card-container.--selected .document-selector {
+        opacity: 1;
+    }
+
+    /* card style */
     .fr-card__title {
         color: var(--text-title-blue-france);
     }
@@ -44,15 +95,21 @@
         flex-direction: row;
     }
 
-    .fr-card {
+    .fr-card,
+    .download-wrapper {
         border-bottom: 1px var(--border-default-grey) solid;
+        /*cursor: pointer;*/
     }
 
-    .fr-card:hover {
+    .card-container:hover .fr-card {
         background-color: var(--background-alt-grey);
     }
 
-    .fr-card.--active {
+    .card-container:focus-within .fr-card {
+        background-color: var(--background-alt-grey);
+    }
+
+    .card-container.--selected .fr-card {
         background-color: var(--background-contrast-blue-france);
     }
 
