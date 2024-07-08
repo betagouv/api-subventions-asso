5aeaa56c2be24a947ee0faa9e0717ed62a62779d
Maxime Meunier <mxmeunier@gmail.com>
2024-07-08 10:27:36 +0200

    Update packages/api/src/shared/errors/GrantError.ts
    Co-authored-by: alice-telescoop <94919029+alice-telescoop@users.noreply.github.com>

--- packages/api/src/shared/errors/GrantError.ts
+++ packages/api/src/shared/errors/GrantError.ts
@@ -2,6 +2,6 @@ import { Rna } from "dto";
 
 export class RnaOnlyError extends Error {
     constructor(rna: Rna) {
-        super(`We could not found any SIREN for the given RNA : ${rna}`);
+        super(`We could not find any SIREN for the given RNA : ${rna}`);
     }
 }
