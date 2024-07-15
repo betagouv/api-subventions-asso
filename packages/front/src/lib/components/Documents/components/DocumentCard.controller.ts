c5dbd625eb8052dfe3dbefb115d9adc471a63e09
alice.telescoop <alice@telescoop.fr>
2024-07-15 17:52:01 +0200

    fix(front): small mistake about doc selection

--- packages/front/src/lib/components/Documents/components/DocumentCard.controller.ts
+++ packages/front/src/lib/components/Documents/components/DocumentCard.controller.ts
@@ -29,6 +29,6 @@ export class DocumentCardController {
     }
 
     newValueOnCheck() {
-        return this.isSelected.value ? undefined : this.document;
+        return this.isSelected.value ? this.document : undefined;
     }
 }
