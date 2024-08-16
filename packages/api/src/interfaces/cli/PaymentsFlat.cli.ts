a6c21f254f7ed020cf1d11db33f874c528c378a5
Giulia Carra <giulia.carra@gmail.com>
2024-08-16 15:04:22 +0200

    feat(api): add PaymentsFlat.cli and its test

---
+++ packages/api/src/interfaces/cli/PaymentsFlat.cli.ts
@@ -0,0 +1,18 @@
+import { ObjectId } from "mongodb";
+import { StaticImplements } from "../../decorators/staticImplements.decorator";
+import { CliStaticInterface } from "../../@types";
+
+import paymentsFlatService from "../../modules/data-viz/paymentFlat/paymentFlat.service";
+
+@StaticImplements<CliStaticInterface>()
+export default class PaymentsFlatCli {
+    static cmdName = "payments-flat";
+
+    async resync(lastChorusObjectId: ObjectId = new ObjectId("000000000000000000000000")) {
+        // Default value is 1/1/1970 at midnight
+        const newLastChorusObjectId = await paymentsFlatService.updatePaymentsFlatCollection(lastChorusObjectId);
+        return newLastChorusObjectId;
+    }
+
+    // Il faudrait enregistrer newLastChorusObjectId quelques parts de façon que le cron puisse le récupérer et changer le paramètre de la fonction resync
+}
