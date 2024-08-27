edf3f649b28967160b5fb17d9893697fc7963196
Giulia Carra <giulia.carra@gmail.com>
2024-08-27 10:37:20 +0200

    feat(api): small modifs paymentsFlat

--- packages/api/src/interfaces/cli/PaymentsFlat.cli.test.ts
+++ packages/api/src/interfaces/cli/PaymentsFlat.cli.test.ts
@@ -1,33 +1,16 @@
 import PaymentsFlatCli from "./PaymentsFlat.cli";
 import paymentsFlatService from "../../modules/data-viz/paymentFlat/paymentFlat.service";
 jest.mock("../../modules/data-viz/paymentFlat/paymentFlat.service");
-import { ObjectId } from "mongodb";
+
 describe("PaymentsFlatCli", () => {
     let paymentsFlatCli: PaymentsFlatCli;
-    let mockLastChorusObjectId: ObjectId;
 
     beforeEach(() => {
         paymentsFlatCli = new PaymentsFlatCli();
-        mockLastChorusObjectId = new ObjectId("000000000000000000000000");
-        (paymentsFlatService.updatePaymentsFlatCollection as jest.Mock).mockResolvedValue(new ObjectId());
-    });
-    afterEach(() => {
-        jest.restoreAllMocks();
     });
 
-    it("should call updatePaymentsFlatCollection with default Object Value", async () => {
+    it("should call updatePaymentsFlatCollection", async () => {
         await paymentsFlatCli.resync();
-        expect(paymentsFlatService.updatePaymentsFlatCollection).toHaveBeenCalledWith(mockLastChorusObjectId);
-    });
-
-    it("should call updatePaymentsFlatCollection with new Object Value", async () => {
-        const newLastChorusObjectId = new ObjectId("5e0b3e4b0000000000000000");
-        await paymentsFlatCli.resync(newLastChorusObjectId);
-        expect(paymentsFlatService.updatePaymentsFlatCollection).toHaveBeenCalledWith(newLastChorusObjectId);
-    });
-
-    it("should return newLastChorusObjectId", async () => {
-        const newLastChorusObjectId = await paymentsFlatCli.resync();
-        expect(newLastChorusObjectId).toEqual(expect.any(ObjectId));
+        expect(paymentsFlatService.updatePaymentsFlatCollection).toHaveBeenCalledTimes(1);
     });
 });
--- packages/api/src/interfaces/cli/PaymentsFlat.cli.ts
+++ packages/api/src/interfaces/cli/PaymentsFlat.cli.ts
@@ -8,15 +8,8 @@ import paymentsFlatService from "../../modules/data-viz/paymentFlat/paymentFlat.
 export default class PaymentsFlatCli {
     static cmdName = "payments-flat";
 
-    async resync(lastChorusObjectId: ObjectId = new ObjectId("000000000000000000000000")) {
-        // Default value is 1/1/1970 at midnight
+    async resync() {
         console.log("Resyncing payments flat collection");
-        const newLastChorusObjectId = await paymentsFlatService.updatePaymentsFlatCollection(lastChorusObjectId);
-
-        // TO DO : modify updatePaymentsFlatCollection
-
-        return newLastChorusObjectId;
+        await paymentsFlatService.updatePaymentsFlatCollection();
     }
-
-    // Il faudrait enregistrer newLastChorusObjectId quelques parts de façon que le cron puisse le récupérer et changer le paramètre de la fonction resync
 }
