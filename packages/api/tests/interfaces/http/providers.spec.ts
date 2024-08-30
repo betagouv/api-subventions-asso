1d4893587eb9e846e7827c3fe34ab8f7fda9b329
alice.telescoop <alice@telescoop.fr>
2024-08-30 17:56:57 +0200

    test(api): clean imports

--- packages/api/tests/interfaces/http/providers.spec.ts
+++ packages/api/tests/interfaces/http/providers.spec.ts
@@ -1,41 +1,4 @@
-import { createAndGetAdminToken, createAndGetUserToken } from "../../__helpers__/tokenHelper";
-import osirisRequestRepository from "../../../src/modules/providers/osiris/repositories/osiris.request.repository";
-import fonjepSubventionRepository from "../../../src/modules/providers/fonjep/repositories/fonjep.subvention.repository";
-import {
-    SubventionEntity as FonjepSubventionFixture,
-    PaymentEntity as FonjepPaymentFixture,
-} from "../../modules/providers/fonjep/__fixtures__/entity";
 import request from "supertest";
-import OsirisRequestEntityFixture from "../../modules/providers/osiris/__fixtures__/entity";
-import { compareByValueBuilder } from "../../../src/shared/helpers/ArrayHelper";
-import statsService from "../../../src/modules/stats/stats.service";
-import { siretToSiren } from "../../../src/shared/helpers/SirenHelper";
-import { BadRequestError } from "../../../src/shared/errors/httpErrors";
-import associationsService from "../../../src/modules/associations/associations.service";
-import rnaSirenPort from "../../../src/dataProviders/db/rnaSiren/rnaSiren.port";
-import { Rna } from "dto";
-import { JoinedRawGrant, RawGrant } from "../../../src/modules/grant/@types/rawGrant";
-import demarchesSimplifieesDataRepository from "../../../src/modules/providers/demarchesSimplifiees/repositories/demarchesSimplifieesData.repository";
-import {
-    DATA_ENTITIES as DS_DATA_ENTITIES,
-    SCHEMAS as DS_SCHEMAS,
-} from "../../dataProviders/db/__fixtures__/demarchesSimplifiees.fixtures";
-import demarchesSimplifieesMapperRepository from "../../../src/modules/providers/demarchesSimplifiees/repositories/demarchesSimplifieesMapper.repository";
-import demarchesSimplifieesService from "../../../src/modules/providers/demarchesSimplifiees/demarchesSimplifiees.service";
-import caisseDepotsService from "../../../src/modules/providers/caisseDepots/caisseDepots.service";
-import { CAISSE_DES_DEPOTS_DTO } from "../../dataProviders/api/caisseDepots.fixtures";
-
-import miscScdlGrantRepository from "../../../src/modules/providers/scdl/repositories/miscScdlGrant.repository";
-import DEFAULT_ASSOCIATION, { LONELY_RNA } from "../../__fixtures__/association.fixture";
-import dauphinGisproRepository from "../../../src/modules/providers/dauphin/repositories/dauphin-gispro.repository";
-import { DAUPHIN_GISPRO_DBOS } from "../../dataProviders/db/__fixtures__/dauphinGispro.fixtures";
-import rnaSirenService from "../../../src/modules/rna-siren/rnaSiren.service";
-import FonjepEntityAdapter from "../../../src/modules/providers/fonjep/adapters/FonjepEntityAdapter";
-import { SCDL_GRANT_DBOS } from "../../dataProviders/db/__fixtures__/scdl.fixtures";
-import chorusLineRepository from "../../../src/modules/providers/chorus/repositories/chorus.line.repository";
-import { ChorusFixtures } from "../../dataProviders/db/__fixtures__/chorus.fixtures";
-import fonjepPaymentRepository from "../../../src/modules/providers/fonjep/repositories/fonjep.payment.repository";
-import FonjepSubventionEntity from "../../../src/modules/providers/fonjep/entities/FonjepSubventionEntity";
 import dataLogRepository from "../../../src/modules/data-log/repositories/dataLog.repository";
 
 jest.mock("../../../src/modules/provider-request/providerRequest.service");
