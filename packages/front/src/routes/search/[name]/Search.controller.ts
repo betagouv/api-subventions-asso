051fc89d249263fdc3b57b5bc33dc3cded035c39
alice.telescoop <alice@telescoop.fr>
2024-10-04 17:39:18 +0200

    feat(front): show company msg according to search error code

--- packages/front/src/routes/search/[name]/Search.controller.ts
+++ packages/front/src/routes/search/[name]/Search.controller.ts
@@ -1,12 +1,13 @@
 import type { PaginatedAssociationNameDto, Siret } from "dto";
+import { SearchCodeError } from "dto";
 import { goto } from "$app/navigation";
 import Store from "$lib/core/Store";
 import { returnInfinitePromise } from "$lib/helpers/promiseHelper";
 import { decodeQuerySearch, encodeQuerySearch } from "$lib/helpers/urlHelper";
 import { isRna, isSiren, isSiret } from "$lib/helpers/identifierHelper";
 import associationService from "$lib/resources/associations/association.service";
-import { isAssociation } from "$lib/resources/associations/association.helper";
 import { removeWhiteSpace } from "$lib/helpers/stringHelper";
+import { BadRequestError } from "$lib/errors";
 
 export default class SearchController {
     inputSearch: Store<string | undefined>;
@@ -26,28 +27,36 @@ export default class SearchController {
     async fetchAssociationFromName(rawName = "", page = 1) {
         const name = rawName.trim();
         this.isLastSearchCompany.set(false);
-        if (isSiret(name)) return this.gotoEstablishment(name);
-        const search = await associationService.search(name, page);
-        if ((isSiren(name) || isRna(name)) && search.total === 1) {
-            const asso = search.results[0];
-            if (isAssociation(asso)) return goto(`/association/${asso.siren || asso.rna}`, { replaceState: true });
-            else this.isLastSearchCompany.set(true);
-        } else {
-            // display alert if there are duplicates in rna-siren links
-            if (isSiren(name) || isRna(name)) {
-                this.duplicatesFromIdentifier.set(
-                    search.results
-                        .map(association =>
-                            [association.rna, association.siren].find(identifier => identifier !== name),
-                        )
-                        .filter(identifier => identifier) as string[],
-                );
-            } else this.duplicatesFromIdentifier.set(null);
+        try {
+            const search = await associationService.search(name, page);
 
-            this.associations.set(search);
-            this.currentPage.set(search.page);
-            // reload same page to save search in history
-            goto(`/search/${encodeQuerySearch(name)}`, { replaceState: true });
+            // search is an identifier
+            if (isSiret(name) && search.total === 1) return this.gotoEstablishment(name);
+            if ((isSiren(name) || isRna(name)) && search.total === 1) {
+                const asso = search.results[0];
+                return goto(`/association/${asso.siren || asso.rna}`, { replaceState: true });
+
+                // search is text
+            } else {
+                // display alert if there are duplicates in rna-siren links
+                if (isSiren(name) || isRna(name)) {
+                    this.duplicatesFromIdentifier.set(
+                        search.results
+                            .map(association =>
+                                [association.rna, association.siren].find(identifier => identifier !== name),
+                            )
+                            .filter(identifier => identifier) as string[],
+                    );
+                } else this.duplicatesFromIdentifier.set(null);
+
+                this.associations.set(search);
+                this.currentPage.set(search.page);
+                // reload same page to save search in history
+                goto(`/search/${encodeQuerySearch(name)}`, { replaceState: true });
+            }
+        } catch (e) {
+            if (e instanceof BadRequestError && e.data?.code === SearchCodeError.ID_NOT_ASSO)
+                this.isLastSearchCompany.set(true);
         }
     }
 
