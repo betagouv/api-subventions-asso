ec39dae38967ec710ccd8248c4bd418eb0e4b716
alice.telescoop <alice@telescoop.fr>
2024-10-17 18:22:46 +0200

    feat(front): gispro adapts dauphinId

--- packages/api/src/modules/providers/gispro/entities/gisproLineEntity.ts
+++ packages/api/src/modules/providers/gispro/entities/gisproLineEntity.ts
@@ -6,7 +6,13 @@ export default class GisproLineEntity implements Gispro {
 
     public static indexedInformationsPath: { [key: string]: ParserInfo } = {
         ej: { path: ["CHORUS -EJ"], adapter: value => value?.toString() },
-        dauphinId: { path: ["Action de demande - Code dossier"] },
+        dauphinId: {
+            path: ["Action de demande - Code dossier"],
+            adapter: value => {
+                if (typeof value !== "string") return;
+                return value.match(/(?:DA)?(\d{8})(?:-\d*)?/)?.[1];
+            },
+        },
         siret: { path: ["Code SIRET"] },
     };
 
