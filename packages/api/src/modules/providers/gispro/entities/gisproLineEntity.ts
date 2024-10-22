9fbc8726472d5f003318c4ef4d58a0cdf511e340
alice.telescoop <alice@telescoop.fr>
2024-10-22 16:20:35 +0200

    feat(api): add possible headers for gispro parser

--- packages/api/src/modules/providers/gispro/entities/gisproLineEntity.ts
+++ packages/api/src/modules/providers/gispro/entities/gisproLineEntity.ts
@@ -5,15 +5,27 @@ export default class GisproLineEntity implements Gispro {
     public provider = "Gispro";
 
     public static indexedInformationsPath: { [key: string]: ParserInfo } = {
-        ej: { path: ["CHORUS -EJ"], adapter: value => value?.toString() },
+        ej: {
+            path: [
+                [
+                    "CHORUS -EJ",
+                    "EJ CHORUS",
+                    "CHORUS EJ",
+                    "N° doc. Précédent\nn° EJ CHORUS",
+                    "EJ",
+                    "N° doc. précédent (EJ)",
+                ],
+            ],
+            adapter: value => value?.toString(),
+        },
         dauphinId: {
-            path: ["Action de demande - Code dossier"],
+            path: [["Action de demande - Code dossier", "Numéro De l'Action Prj", "Action - Code dossier"]],
             adapter: value => {
                 if (typeof value !== "string") return;
                 return value.match(/(?:DA)?(\d{8})(?:-\d*)?/)?.[1];
             },
         },
-        siret: { path: ["Code SIRET"] },
+        siret: { path: [["Code SIRET", "Code Siret"]] },
     };
 
     constructor(public ej: string, public dauphinId: string, public siret: string) {}
