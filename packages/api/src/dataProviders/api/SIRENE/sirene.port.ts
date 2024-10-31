1d01ed329e7e39f69ab0a8616b1d8cf61b6071c4
Maxime Silvère Meunier <mxmeunier@gmail.com>
2024-10-31 15:00:28 +0100

    feat(api): return promise to cli

--- packages/api/src/dataProviders/api/SIRENE/sirene.port.ts
+++ packages/api/src/dataProviders/api/SIRENE/sirene.port.ts
@@ -1,14 +1,9 @@
-import * as fs from "fs";
-import path from "path";
-import * as yauzl from "yauzl";
-import { relativeTimeRounding } from "moment";
-import * as unzipper from "unzipper";
 import ProviderRequestFactory, {
     ProviderRequestService,
 } from "../../../modules/provider-request/providerRequest.service";
 
 export class SirenePort {
-    private static URL = "https://files.data.gouv.fr/insee-sirene/StockUniteLegale_utf8.zip";
+    private URL = "https://files.data.gouv.fr/insee-sirene/StockUniteLegale_utf8.zip";
 
     private http: ProviderRequestService;
 
@@ -16,9 +11,8 @@ export class SirenePort {
         this.http = ProviderRequestFactory("sirene");
     }
 
-    async downloadAndExtractSireneZip() {
-        console.log("Downloading sirene zip file");
-        return await this.http.downloadAndExtractZip(SirenePort.URL, "/temp/sirene/");
+    getZip() {
+        return this.http.get(this.URL, { responseType: "stream" });
     }
 }
 
--- packages/api/src/interfaces/cli/poc.cli.ts
+++ packages/api/src/interfaces/cli/poc.cli.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import { CliStaticInterface } from "../../@types";
 import sirenePort from "../../dataProviders/api/SIRENE/sirene.port";
 import { StaticImplements } from "../../decorators/staticImplements.decorator";
@@ -8,8 +9,26 @@ export default class PocCli extends CliController {
     static cmdName = "poc";
 
     public async test() {
-        const url = "https://www.data.gouv.fr/fr/datasets/r/825f4199-cadd-486c-ac46-a65a8ea1a047";
+        const file = fs.createWriteStream("poc-zip.zip");
 
-        await sirenePort.downloadAndExtractSireneZip();
+        const response = await sirenePort.getZip();
+
+        return new Promise(resolve => {
+            response.data.pipe(file);
+
+            file.on("finish", () => {
+                console.log("finish");
+                file.close();
+            });
+
+            file.on("error", error => {
+                console.log("error", error);
+            });
+
+            file.on("close", () => {
+                resolve("ok google");
+                console.log("close");
+            });
+        });
     }
 }
