f5b8b387f000bb2bfb067324fb4fd1b324d465e0
alice.telescoop <alice@telescoop.fr>
2024-11-05 16:55:22 +0100

    fix(api): proper regex

--- packages/api/src/modules/documents/documents.service.ts
+++ packages/api/src/modules/documents/documents.service.ts
@@ -92,7 +92,7 @@ export class DocumentsService {
     }
 
     private sanitizeDocumentRequest(doc: DocumentRequestDto) {
-        const noPathTraversal = s => s.replace(/\/\\/, "");
+        const noPathTraversal = s => s.replace(/[/|\\]/, "");
         doc.type = noPathTraversal(doc.type);
         doc.nom = noPathTraversal(doc.nom);
         return doc;
