b2d7a5f2bdbcc5cc6f56fd09edb68691997cfeb9
Maxime Silvère Meunier <mxmeunier@gmail.com>
2024-11-11 12:39:58 +0100

    feat(front): flatten grants in asso and estab port

---
+++ packages/front/src/lib/resources/@types/FlattenGrant.ts
@@ -0,0 +1,122 @@
+import { ApplicationStatus, SiretDto } from "dto";
+
+export type FlatGrant = {
+    application: FlatApplication | null;
+    payments: FlatPayment[] | null;
+};
+
+export type FlatPayment = FlatChorusPayment | FlatFonjepPayment;
+
+interface FlatBasePayment {
+    versementKey: string;
+    siret: SiretDto;
+    amount: number;
+    dateOperation: Date;
+    programme: number;
+    libelleProgramme: string;
+}
+
+interface FlatChorusPayment extends FlatBasePayment {
+    ej: string;
+    centreFinancier: string;
+    domaineFonctionnel: string;
+    numeroDemandePaiement?: string;
+    numeroTier?: string;
+    activitee?: string;
+    compte?: string;
+    codeBranche?: string;
+    branche?: string;
+    type?: string;
+    /**
+     * Deprecated
+     */
+    bop: string;
+}
+
+interface FlatFonjepPayment extends FlatBasePayment {
+    codePoste: string;
+    periodeDebut: Date;
+    periodeFin: Date;
+    montantAPayer: number;
+    /**
+     * Deprecated
+     */
+    bop: number;
+}
+
+export interface FlatApplication {
+    service_instructeur: string;
+    siret: SiretDto;
+    dispositif?: string;
+    sous_dispositif?: string;
+    ej?: string;
+    versementKey?: string;
+    annee_demande?: number;
+    date_commision?: Date;
+    financeur_principal?: string;
+    creer_le?: Date;
+    transmis_le?: Date;
+    date_fin?: Date;
+    pluriannualite?: string;
+    plein_temps?: string;
+    contact?: {
+        email: string;
+        telephone?: string;
+    };
+    statut_label: ApplicationStatus;
+    status: string;
+    montants?: {
+        total?: number;
+        demande?: number;
+        propose?: number;
+        accorde?: number;
+    };
+    versement?: {
+        acompte: number;
+        solde: number;
+        realise: number;
+        compensation: {
+            "n-1": number;
+            reversement: number;
+        };
+    };
+    actions_proposee?: {
+        ej?: string;
+        rang?: number;
+        intitule: string;
+        objectifs?: string;
+        objectifs_operationnels?: string;
+        description?: string;
+        nature_aide?: string;
+        modalite_aide?: string;
+        modalite_ou_dispositif?: string;
+        indicateurs?: string;
+        cofinanceurs?: {
+            noms: string;
+            montant_demandes: number;
+        };
+        montants_versement?: {
+            total?: number;
+            demande?: number;
+            propose?: number;
+            accorde?: number;
+            attribue?: number;
+            realise?: number;
+            compensation?: number;
+        };
+    }[];
+    co_financement?: {
+        cofinanceur: string;
+        cofinanceur_email: string;
+        cofinanceur_siret?: string;
+        montants: number;
+    };
+    territoires?: {
+        status: string;
+        commentaire: string;
+    }[];
+    evaluation?: {
+        evaluation_resultat: string;
+        cout_total_realise: number;
+    };
+}
