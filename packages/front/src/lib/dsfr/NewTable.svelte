5d005d5062b44e7a57439b17e8a02e5894774c3c
Maxime Silvère Meunier <mxmeunier@gmail.com>
2024-11-14 09:27:10 +0100

    feat(front): implement new dsfr table

---
+++ packages/front/src/lib/dsfr/NewTable.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    export let title = "Titre du tableau (caption)";
+    export let size: "sm" | "md" | "lg" = "lg";
+    export let headers: string[];
+    export let rows: string[][];
+
+    const id = crypto.randomUUID();
+</script>
+
+<div class="fr-table--${size} fr-table fr-table" id="table-component-${id}">
+    <div class="fr-table__wrapper">
+        <div class="fr-table__container">
+            <div class="fr-table__content">
+                <table id="table-${id}">
+                    <caption>{title}</caption>
+                    <thead>
+                        <tr>
+                            {#each headers as header}
+                                <th scope="col">{header}</th>
+                            {/each}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {#each rows as [row, index]}
+                            <tr id="table-${size}-row-key-${index}" data-row-key={index}>
+                                {#each row as cell}
+                                    <td>{cell}</td>
+                                {/each}
+                            </tr>
+                        {/each}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
