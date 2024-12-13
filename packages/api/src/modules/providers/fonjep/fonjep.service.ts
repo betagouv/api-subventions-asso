8af664fd483f1b9102725a572991b513918c6592
Giulia Carra <giulia.carra@gmail.com>
2024-12-13 18:19:38 +0100

    feat(api): added createFonjepCollection to service

--- packages/api/src/modules/providers/fonjep/fonjep.service.ts
+++ packages/api/src/modules/providers/fonjep/fonjep.service.ts
@@ -1,6 +1,18 @@
 import { ProviderEnum } from "../../../@enums/ProviderEnum";
+import FonjepTypePosteDbo from "../../../dataProviders/db/providers/fonjep/dbo/fonjepTypePosteDbo";
+import FonjepVersementDbo from "../../../dataProviders/db/providers/fonjep/dbo/FonjepVersementDbo";
+import fonjepDispositifPort from "../../../dataProviders/db/providers/fonjep/fonjep.dispositif.port";
+import fonjepPostesPort from "../../../dataProviders/db/providers/fonjep/fonjep.postes.port";
+import fonjepTiersPort from "../../../dataProviders/db/providers/fonjep/fonjep.tiers.port";
+import fonjepTypePostePort from "../../../dataProviders/db/providers/fonjep/fonjep.typePoste.port";
+import fonjepVersementsPort from "../../../dataProviders/db/providers/fonjep/fonjep.versements.port";
 import ProviderCore from "../ProviderCore";
 import FonjepEntityAdapter from "./adapters/FonjepEntityAdapter";
+import FonjepDispositifEntity from "./entities/FonjepDispositifEntity";
+import FonjepPosteEntity from "./entities/FonjepPosteEntity";
+import FonjepTiersEntity from "./entities/FonjepTiersEntity";
+import FonjepTypePosteEntity from "./entities/FonjepTypePosteEntity";
+import FonjepVersementEntity from "./entities/FonjepVersementEntity";
 import FonjepParser from "./fonjep.parser";
 
 export class FonjepService extends ProviderCore {
@@ -14,28 +26,26 @@ export class FonjepService extends ProviderCore {
         });
     }
 
-    public fromFileToEntities(filePath: string) {
+    public fromFileToEntities(filePath: string): {
+        tierEntities: FonjepTiersEntity[];
+        posteEntities: FonjepPosteEntity[];
+        versementEntities: FonjepVersementEntity[];
+        typePosteEntities: FonjepTypePosteEntity[];
+        dispositifEntities: FonjepDispositifEntity[];
+    } {
         const { tiers, postes, versements, typePoste, dispositifs } = FonjepParser.parse(filePath);
 
-        const tierEntities = tiers.map(tier => {
-            FonjepEntityAdapter.toFonjepTierEntity(tier);
-        });
+        const tierEntities = tiers.map(tier => FonjepEntityAdapter.toFonjepTierEntity(tier));
 
-        const posteEntities = postes.map(poste => {
-            FonjepEntityAdapter.toFonjepPosteEntity(poste);
-        });
+        const posteEntities = postes.map(poste => FonjepEntityAdapter.toFonjepPosteEntity(poste));
 
-        const versementEntities = versements.map(versement => {
-            FonjepEntityAdapter.toFonjepVersementEntity(versement);
-        });
+        const versementEntities = versements.map(versement => FonjepEntityAdapter.toFonjepVersementEntity(versement));
 
-        const typePosteEntities = typePoste.map(typePoste => {
-            FonjepEntityAdapter.toFonjepTypePosteEntity(typePoste);
-        });
+        const typePosteEntities = typePoste.map(typePoste => FonjepEntityAdapter.toFonjepTypePosteEntity(typePoste));
 
-        const dispositifEntities = dispositifs.map(dispositif => {
-            FonjepEntityAdapter.toFonjepDispositifEntity(dispositif);
-        });
+        const dispositifEntities = dispositifs.map(dispositif =>
+            FonjepEntityAdapter.toFonjepDispositifEntity(dispositif),
+        );
 
         return { tierEntities, posteEntities, versementEntities, typePosteEntities, dispositifEntities };
     }
@@ -47,14 +57,33 @@ export class FonjepService extends ProviderCore {
      */
 
     useTemporyCollection(active: boolean) {
-        // TO DO GIULIA : créate une porte for each entity
-        fonjepSubventionPort.useTemporyCollection(active);
-        fonjepPaymentPort.useTemporyCollection(active);
+        fonjepDispositifPort.useTemporyCollection(active);
+        fonjepPostesPort.useTemporyCollection(active);
+        fonjepTiersPort.useTemporyCollection(active);
+        fonjepTypePostePort.useTemporyCollection(active);
+        fonjepVersementsPort.useTemporyCollection(active);
+    }
+
+    async createFonjepCollections(
+        tierEntities: FonjepTiersEntity[],
+        posteEntities: FonjepPosteEntity[],
+        versementEntities: FonjepVersementEntity[],
+        typePosteEntities: FonjepTypePosteEntity[],
+        dispositifEntities: FonjepDispositifEntity[],
+    ) {
+        await fonjepTiersPort.insertMany(tierEntities);
+        await fonjepPostesPort.insertMany(posteEntities);
+        await fonjepVersementsPort.insertMany(versementEntities);
+        await fonjepTypePostePort.insertMany(typePosteEntities);
+        await fonjepDispositifPort.insertMany(dispositifEntities);
     }
 
     async applyTemporyCollection() {
-        await fonjepSubventionPort.applyTemporyCollection();
-        await fonjepPaymentPort.applyTemporyCollection();
+        await fonjepDispositifPort.applyTemporyCollection();
+        await fonjepPostesPort.applyTemporyCollection();
+        await fonjepTiersPort.applyTemporyCollection();
+        await fonjepTypePostePort.applyTemporyCollection();
+        await fonjepVersementsPort.applyTemporyCollection();
     }
 }
 
