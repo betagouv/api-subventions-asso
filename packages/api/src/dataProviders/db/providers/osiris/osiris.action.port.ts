e76a90ce4e9005d7edb05cc0cfb62173cee1e422
alice.telescoop <alice@telescoop.fr>
2024-12-31 16:46:42 +0100

    feat(api): build and use per-exercise osiris action uniqueId

--- packages/api/src/dataProviders/db/providers/osiris/osiris.action.port.ts
+++ packages/api/src/dataProviders/db/providers/osiris/osiris.action.port.ts
@@ -24,22 +24,21 @@ export class OsirisActionPort extends MongoPort<OsirisActionEntityDbo> {
     public async update(osirisAction: OsirisActionEntity) {
         const options: FindOneAndUpdateOptions = { returnDocument: "after", includeResultMetadata: true };
         const { _id, ...actionWithoutId } = OsirisActionAdapter.toDbo(osirisAction);
-        const dbo =
-            (
-                await this.collection.findOneAndUpdate(
-                    { "indexedInformations.osirisActionId": osirisAction.indexedInformations.osirisActionId },
-                    { $set: actionWithoutId },
-                    options,
-                )
-            //@ts-expect-error -- mongo typing expects no metadata
-            )?.value;
+        const updateRes = await this.collection.findOneAndUpdate(
+            { "indexedInformations.uniqueId": osirisAction.indexedInformations.uniqueId },
+            { $set: actionWithoutId },
+            options,
+        );
+
+        //@ts-expect-error -- mongo typing expects no metadata
+        const dbo = updateRes?.value;
         if (!dbo) throw new MongoCnxError();
         return OsirisActionAdapter.toEntity(dbo);
     }
 
-    public async findByOsirisId(osirisId: string) {
+    public async findByUniqueId(uniqueId: string) {
         const dbo = await this.collection.findOne({
-            "indexedInformations.osirisActionId": osirisId,
+            "indexedInformations.uniqueId": uniqueId,
         });
         if (!dbo) return null;
         return OsirisActionAdapter.toEntity(dbo);
