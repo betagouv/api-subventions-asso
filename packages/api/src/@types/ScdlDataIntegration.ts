9581c11f9f5b86d8ccf992e01ca2c947b28b5b21
Romain Mercier <romain.mercier321@gmail.com>
2025-01-28 14:33:05 +0100

    feat(api): #3121 scdl integration script
    - type factorization

--- packages/api/src/@types/ScdlDataIntegration.ts
+++ packages/api/src/@types/ScdlDataIntegration.ts
@@ -1,11 +1,6 @@
-export type ScdlParseArgs = [producerSlug: string, exportDate: string, delimiter?: string, quote?: string];
-
-export type ScdlParseXlsArgs = [
-    producerSlug: string,
-    exportDate: string,
-    pageName?: string,
-    rowOffset?: number | string,
-];
+type BaseScdlParseArgs = [producerSlug: string, exportDate: string];
+export type ScdlParseArgs = BaseScdlParseArgs & [delimiter?: string, quote?: string];
+export type ScdlParseXlsArgs = BaseScdlParseArgs & [pageName?: string, rowOffset?: number | string];
 
 export interface ScdlFileProcessingConfig {
     name: string;
