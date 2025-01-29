6e9e94bbec14def0a6b09f1bb09a335e56d3d1e1
Romain Mercier <romain.mercier321@gmail.com>
2025-01-29 14:08:21 +0100

    feat(api): #3121 scdl integration script

--- packages/api/src/@types/ScdlDataIntegration.ts
+++ packages/api/src/@types/ScdlDataIntegration.ts
@@ -1,17 +1,6 @@
-export interface BaseScdlParseArgs {
-    producerSlug: string;
-    exportDate: string;
-}
-
-export interface ScdlParseArgs extends BaseScdlParseArgs {
-    delimiter?: string;
-    quote?: string;
-}
-
-export interface ScdlParseXlsArgs extends BaseScdlParseArgs {
-    pageName?: string;
-    rowOffset?: number | string;
-}
+type BaseScdlParseArgs = { producerSlug: string; exportDate: string };
+export type ScdlParseArgs = BaseScdlParseArgs & { delimiter?: string; quote?: string };
+export type ScdlParseXlsArgs = BaseScdlParseArgs & { pageName?: string; rowOffset?: number | string };
 
 export interface ScdlFileProcessingConfig {
     name: string;
