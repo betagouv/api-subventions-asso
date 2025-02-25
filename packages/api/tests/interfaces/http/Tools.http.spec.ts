71ec93bf53b467e65b4dc49055bdd71d8850d2a7
alice.telescoop <alice@telescoop.fr>
2025-02-25 17:09:31 +0100

    test(api): more integ tests on valid files

--- packages/api/tests/interfaces/http/Tools.http.spec.ts
+++ packages/api/tests/interfaces/http/Tools.http.spec.ts
@@ -5,7 +5,7 @@ const g = global as unknown as { app: unknown };
 
 describe("Tools http interface", () => {
     describe("/tools/scdl/validate", () => {
-        it("returns errors as csv", async () => {
+        it("returns empty string if csv file is correct", async () => {
             const response = await request(g.app)
                 .post(`/tools/scdl/validate`)
                 .set("x-access-token", await createAndGetAdminToken())
@@ -13,7 +13,30 @@ describe("Tools http interface", () => {
                 .field("delimiter", ",")
                 .attach("file", "tests/interfaces/http/fixtures/fixture.csv");
             expect(response.statusCode).toBe(200);
+            expect(response.body).toBe("");
+        });
+
+        it("returns errors as csv", async () => {
+            const response = await request(g.app)
+                .post(`/tools/scdl/validate`)
+                .set("x-access-token", await createAndGetAdminToken())
+                .field("type", "csv")
+                .field("delimiter", ",")
+                .attach("file", "tests/interfaces/http/fixtures/fixture-errors.csv");
+            expect(response.statusCode).toBe(200);
             expect(response.body).toMatchSnapshot();
+        });
+
+        it("returns empty string if excel file is correct", async () => {
+            const response = await request(g.app)
+                .post(`/tools/scdl/validate`)
+                .set("x-access-token", await createAndGetAdminToken())
+                .field("type", "excel")
+                .field("pageName", "fixture")
+                .field("rowOffset", "2")
+                .attach("file", "tests/interfaces/http/fixtures/fixture.xlsx");
+            expect(response.statusCode).toBe(200);
+            expect(response.body).toBe("");
         });
 
         it("returns errors as excel", async () => {
@@ -23,7 +46,7 @@ describe("Tools http interface", () => {
                 .field("type", "excel")
                 .field("pageName", "fixture")
                 .field("rowOffset", "2")
-                .attach("file", "tests/interfaces/http/fixtures/fixture.xlsx");
+                .attach("file", "tests/interfaces/http/fixtures/fixture-errors.xlsx");
             expect(response.statusCode).toBe(200);
             expect(response.body).toMatchSnapshot();
         });
