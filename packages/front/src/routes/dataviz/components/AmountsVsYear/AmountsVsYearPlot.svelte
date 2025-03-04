b619e5c5d483e11eb102685c022e067f605f9156
gcarra <36041873+gcarra@users.noreply.github.com>
2025-03-04 09:04:31 +0100

    feat(front): #3161 create graph Amount vs year (#3228)

---
+++ packages/front/src/routes/dataviz/components/AmountsVsYear/AmountsVsYearPlot.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import type { PartialAmountsVsProgramRegionDto } from "../../@types/AmountsVsYear.types";
+    import { updateChart } from "./AmountsVsYear.helper";
+
+    export let data_all: PartialAmountsVsProgramRegionDto[];
+    export let data_selected: PartialAmountsVsProgramRegionDto[];
+
+    let svg: SVGSVGElement;
+    const width = 800;
+    const height = 400;
+
+    const margin = { top: 80, right: 320, bottom: 40, left: 90 };
+
+    onMount(() => updateChart(svg, data_all, data_selected, width, height, margin));
+
+    $: updateChart(svg, data_all, data_selected, width, height, margin);
+</script>
+
+<svg bind:this={svg} />
