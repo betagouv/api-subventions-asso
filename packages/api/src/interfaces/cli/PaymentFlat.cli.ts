41442e2e86dbe3d120dd1397f4f956217a79844c
alice.telescoop <alice@telescoop.fr>
2025-03-13 16:41:06 +0100

    feat(api): logs regularly to not kill console

--- packages/api/src/interfaces/cli/PaymentFlat.cli.ts
+++ packages/api/src/interfaces/cli/PaymentFlat.cli.ts
@@ -8,15 +8,18 @@ import CliController from "../../shared/CliController";
 export default class PaymentFlatCli extends CliController {
     static cmdName = "payment-flat";
 
-    resyncExercice(exerciceBudgetaire: number) {
+    async resyncExercice(exerciceBudgetaire: number) {
+        const ticTacInterval = setInterval(() => console.log("TIC"), 60000);
         if (!exerciceBudgetaire) throw new Error("Exercice budgetaire is required");
 
         this.logger.logIC(`Resync payment flat collection for exercice ${exerciceBudgetaire}`);
-        return paymentsFlatService.updatePaymentsFlatCollection(exerciceBudgetaire);
+        await paymentsFlatService.updatePaymentsFlatCollection(exerciceBudgetaire);
+        clearInterval(ticTacInterval);
     }
 
     // should only be used once, then sync with resyncExercise
     async init() {
+        const ticTacInterval = setInterval(() => console.log("TIC"), 60000);
         if (await paymentsFlatService.isCollectionInitialized())
             throw new Error("DB already initialized, used resyncExercice instead");
         const START_YEAR = 2017;
@@ -25,6 +28,7 @@ export default class PaymentFlatCli extends CliController {
             this.logger.logIC(`init payment flat collection for exercice ${year}`);
             await paymentsFlatService.updatePaymentsFlatCollection(year);
         }
+        clearInterval(ticTacInterval);
 
         // this.logger.logIC("Create all payment flat collection");
         // return paymentsFlatService.init();
