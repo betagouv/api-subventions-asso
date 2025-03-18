028e323234e2d421e7255e9825ec8771c31fc7cc
alice.telescoop <alice@telescoop.fr>
2025-03-18 14:18:25 +0100

    fix(api): migration waits for rename to occur

--- packages/api/migrations/20250318095226-chorus_exercise_as_number.js
+++ packages/api/migrations/20250318095226-chorus_exercise_as_number.js
@@ -1,26 +1,32 @@
 module.exports = {
     async up(db) {
-        await db.collection("chorus-line").aggregate([
-            {
-                $addFields: {
-                    "indexedInformations.exercice": { $toInt: "$indexedInformations.exercice" },
+        await db
+            .collection("chorus-line")
+            .aggregate([
+                {
+                    $addFields: {
+                        "indexedInformations.exercice": { $toInt: "$indexedInformations.exercice" },
+                    },
                 },
-            },
-            { $out: "chorus-line-nb" },
-        ]);
+                { $out: "chorus-line-nb" },
+            ])
+            .toArray();
         await db.dropCollection("chorus-line");
         await db.renameCollection("chorus-line-nb", "chorus-line");
     },
 
     async down(db) {
-        await db.collection("chorus-line").aggregate([
-            {
-                $addFields: {
-                    "indexedInformations.exercice": { $toString: "$indexedInformations.exercice" },
+        await db
+            .collection("chorus-line")
+            .aggregate([
+                {
+                    $addFields: {
+                        "indexedInformations.exercice": { $toString: "$indexedInformations.exercice" },
+                    },
                 },
-            },
-            { $out: "chorus-line-str" },
-        ]);
+                { $out: "chorus-line-str" },
+            ])
+            .toArray();
         await db.dropCollection("chorus-line");
         await db.renameCollection("chorus-line-str", "chorus-line");
     },
