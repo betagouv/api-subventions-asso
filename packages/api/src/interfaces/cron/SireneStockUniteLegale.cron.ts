41e02c791e0c07660d9411b7803d431693deab41
alice.telescoop <alice@telescoop.fr>
2025-03-19 11:29:24 +0100

    refactor(api): uniformize names

--- packages/api/src/interfaces/cron/SireneStockUniteLegale.cron.ts
+++ packages/api/src/interfaces/cron/SireneStockUniteLegale.cron.ts
@@ -3,11 +3,11 @@ import { AsyncCron } from "../../decorators/cronController.decorator";
 import sireneStockUniteLegaleFileService from "../../modules/providers/sirene/stockUniteLegale/sireneStockUniteLegale.file.service";
 
 export class SireneStockUniteLegaleCron implements CronController {
-    name = "sireneStockUniteLegaleCron";
+    name = "sirene";
 
     // every month on day 2 (00:00)
     @AsyncCron({ cronExpression: "0 0 2 * *" })
-    async getAndParse() {
+    async import() {
         await sireneStockUniteLegaleFileService.getAndParse();
     }
 }
