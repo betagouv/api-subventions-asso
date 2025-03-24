d018fe0aa1f4630aba7d19e752dc9ba1e46c57a4
gcarra <36041873+gcarra@users.noreply.github.com>
2025-03-24 14:27:28 +0100

    feat(front): #3162 histo added to dataViz (#3251)

---
+++ packages/front/src/routes/dataviz/components/HistoAmountsVsRegionYear/HistoAmountsVsRegionYear.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import type { AmountsVsProgramRegionDto } from "dto";
+    import { createHistoAmountsVsRegionYear } from "./HistoAmountsVsRegionYear.helper";
+    import { HistoAmountsVsRegionYearController } from "./HistoAmountsVsRegionYear.controller";
+
+    export let elements: AmountsVsProgramRegionDto[];
+
+    let svg: SVGSVGElement;
+    const width = 1300;
+    const height = 500;
+
+    const margin = { top: 80, right: 320, bottom: 110, left: 90 };
+
+    const controller = new HistoAmountsVsRegionYearController(elements);
+    const { dataHisto } = controller;
+
+    onMount(() => createHistoAmountsVsRegionYear(svg, dataHisto, width, height, margin));
+
+    $: createHistoAmountsVsRegionYear(svg, dataHisto, width, height, margin);
+</script>
+
+<svg bind:this={svg} />
