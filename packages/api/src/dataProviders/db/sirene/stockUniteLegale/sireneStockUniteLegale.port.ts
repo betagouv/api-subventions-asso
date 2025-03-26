76585518240f5066cc2efddf577315ae53378895
alice.telescoop <alice@telescoop.fr>
2025-03-26 15:40:07 +0100

    fix(api): prevents overriding ObjectId

--- packages/api/src/dataProviders/db/sirene/stockUniteLegale/sireneStockUniteLegale.port.ts
+++ packages/api/src/dataProviders/db/sirene/stockUniteLegale/sireneStockUniteLegale.port.ts
@@ -1,18 +1,19 @@
 import MongoPort from "../../../../shared/MongoPort";
 import { SireneUniteLegaleDbo } from "../../../../modules/providers/sirene/stockUniteLegale/@types/SireneUniteLegaleDbo";
+import { SireneStockUniteLegaleEntity } from "../../../../entities/SireneStockUniteLegaleEntity";
 
-export class SireneUniteLegaleDbPort extends MongoPort<SireneUniteLegaleDbo> {
+export class SireneUniteLegaleDbPort extends MongoPort<SireneStockUniteLegaleEntity> {
     collectionName = "sirene";
 
     public async createIndexes(): Promise<void> {
         await this.collection.createIndex({ siren: 1 }, { unique: true });
     }
 
-    public insertMany(dbos: SireneUniteLegaleDbo[]) {
+    public insertMany(dbos: SireneUniteLegaleDbo[] | SireneStockUniteLegaleEntity[]) {
         return this.collection.insertMany(dbos);
     }
 
-    public insertOne(dbo: SireneUniteLegaleDbo) {
+    public insertOne(dbo: SireneStockUniteLegaleEntity | SireneUniteLegaleDbo) {
         return this.collection.insertOne(dbo);
     }
 
