ef46bb3d0890f03240168cb4dc7d48bd84219aa5
alice.telescoop <alice@telescoop.fr>
2025-04-04 10:06:12 +0200

    feat(api): application flat adapters can return null so that we can exclude incomplete/invalid data

--- packages/api/src/modules/applicationFlat/applicationFlat.helper.ts
+++ packages/api/src/modules/applicationFlat/applicationFlat.helper.ts
@@ -1,12 +1,14 @@
 import { Readable } from "stream";
-import { FindCursor } from "mongodb";
+import { FindCursor, Document } from "mongodb";
 import ApplicationFlatEntity from "../../entities/ApplicationFlatEntity";
 
 export function cursorToStream<T = Document>(
     findCursor: FindCursor<T>,
-    adapter: (entity: T) => ApplicationFlatEntity,
+    adapter: (entity: T) => ApplicationFlatEntity | null,
 ): ReadableStream {
     // there are two types of stream and we want more modern web streams
     // cf https://betterstack.com/community/guides/scaling-nodejs/nodejs-streams-vs-web-streams-api/#transitioning-from-node-js-streams-to-web-streams
-    return Readable.toWeb(findCursor.stream({ transform: entity => adapter(entity as T) })) as ReadableStream;
+    return Readable.toWeb(
+        findCursor.stream({ transform: entity => adapter(entity as T) as Document }),
+    ) as ReadableStream;
 }
