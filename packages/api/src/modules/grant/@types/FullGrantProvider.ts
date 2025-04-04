af0a088011f1318a410e4a27f7f286b0c0dd8789
alice.telescoop <alice@telescoop.fr>
2025-04-04 12:17:50 +0200

    feat(api): allow null until adaptation in grant service

--- packages/api/src/modules/grant/@types/FullGrantProvider.ts
+++ packages/api/src/modules/grant/@types/FullGrantProvider.ts
@@ -5,5 +5,5 @@ import GrantProvider from "./GrantProvider";
 // TODO: move this file somewhere else ?
 export interface FullGrantProvider<T> extends GrantProvider {
     isFullGrantProvider: boolean;
-    rawToGrant: (rawFullGrant: RawFullGrant<T>) => Grant;
+    rawToGrant: (rawFullGrant: RawFullGrant<T>) => Grant | null;
 }
