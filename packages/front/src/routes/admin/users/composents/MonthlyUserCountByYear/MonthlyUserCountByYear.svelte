e56e2d2ba42e4375329dfd43ede4200ebb28b2ce
alice.telescoop <alice@telescoop.fr>
2025-04-16 18:08:53 +0200

    chore(front): remove front stats

--- packages/front/src/routes/admin/users/composents/MonthlyUserCountByYear/MonthlyUserCountByYear.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import { MonthlyUserCountByYearController } from "./MonthlyUserCountByYear.controller";
-    import MonthlyGraph from "$lib/components/Stats/MonthlyGraph/MonthlyGraph.svelte";
-
-    const { loadData } = new MonthlyUserCountByYearController();
-</script>
-
-<MonthlyGraph title="Nombre d'utilisateurs" {loadData} withPreviousValue resourceName="Utilisateurs" />
