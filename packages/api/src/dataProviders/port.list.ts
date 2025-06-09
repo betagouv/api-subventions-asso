67b470370ea52dea24b3e68d900df8755521f0d8
alice.telescoop <alice@telescoop.fr>
2025-06-09 18:11:28 +0200

    chore(api): move port list to proper directory

---
+++ packages/api/src/dataProviders/port.list.ts
@@ -0,0 +1,58 @@
+import rnaSirenPort from "./db/rnaSiren/rnaSiren.port";
+import uniteLegalEntreprisePort from "./db/uniteLegalEntreprise/uniteLegalEntreprise.port";
+import uniteLegalNamePort from "./db/uniteLegalName/uniteLegalName.port";
+import configurationsPort from "./db/configurations/configurations.port";
+import logsPort from "./db/stats/logs.port";
+import statsAssociationsVisitPort from "./db/stats/statsAssociationsVisit.port";
+import userResetPort from "./db/user/user-reset.port";
+import userPort from "./db/user/user.port";
+import consumerTokenPort from "./db/user/consumer-token.port";
+import chorusLinePort from "./db/providers/chorus/chorus.line.port";
+import dauphinGisproPort from "./db/providers/dauphin/dauphin-gispro.port";
+import demarchesSimplifieesDataPort from "./db/providers/demarchesSimplifiees/demarchesSimplifieesData.port";
+import demarchesSimplifieesSchemaPort from "./db/providers/demarchesSimplifiees/demarchesSimplifieesSchema.port";
+import fonjepSubventionPort from "./db/providers/fonjep/fonjep.subvention.port.old";
+import fonjepPaymentPort from "./db/providers/fonjep/fonjep.payment.port.old";
+import fonjepVersementsPort from "./db/providers/fonjep/fonjep.versements.port";
+import fonjepPostesPort from "./db/providers/fonjep/fonjep.postes.port";
+import fonjepTiersPort from "./db/providers/fonjep/fonjep.tiers.port";
+import fonjepTypePostePort from "./db/providers/fonjep/fonjep.typePoste.port";
+import fonjepDispositifPort from "./db/providers/fonjep/fonjep.dispositif.port";
+import paymentFlatPort from "./db/paymentFlat/paymentFlat.port";
+import { osirisActionPort, osirisRequestPort } from "./db/providers/osiris";
+import miscScdlGrantPort from "./db/providers/scdl/miscScdlGrant.port";
+import miscScdlProducersPort from "./db/providers/scdl/miscScdlProducers.port";
+import sireneStockUniteLegalePort from "./db/sirene/stockUniteLegale/sireneStockUniteLegale.port";
+import amountsVsProgramRegionPort from "./db/dataViz/amountVSProgramRegion/amountsVsProgramRegion.port";
+import dataLogPort from "./db/data-log/dataLog.port";
+
+export const portsWithIndexes = [
+    userPort,
+    logsPort,
+    userResetPort,
+    osirisRequestPort,
+    osirisActionPort,
+    fonjepPaymentPort,
+    fonjepSubventionPort,
+    fonjepVersementsPort,
+    fonjepPostesPort,
+    fonjepTiersPort,
+    fonjepTypePostePort,
+    fonjepDispositifPort,
+    dauphinGisproPort,
+    miscScdlGrantPort,
+    miscScdlProducersPort,
+    configurationsPort,
+    consumerTokenPort,
+    chorusLinePort,
+    demarchesSimplifieesDataPort,
+    demarchesSimplifieesSchemaPort,
+    statsAssociationsVisitPort,
+    rnaSirenPort,
+    uniteLegalEntreprisePort,
+    uniteLegalNamePort,
+    paymentFlatPort,
+    sireneStockUniteLegalePort,
+    amountsVsProgramRegionPort,
+    dataLogPort,
+];
