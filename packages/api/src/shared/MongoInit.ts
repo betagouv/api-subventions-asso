224eae0c590026bb0aed7382392ca24e2e206c7c
alice.telescoop <alice@telescoop.fr>
2025-06-09 18:11:28 +0200

    chore(api): move port list to proper directory

--- packages/api/src/shared/MongoInit.ts
+++ packages/api/src/shared/MongoInit.ts
@@ -1,3 +1,3 @@
-import { portsWithIndexes } from "../modules/port.list";
+import { portsWithIndexes } from "../dataProviders/port.list";
 import { asyncForEach } from "./helpers/ArrayHelper";
 export const initIndexes = () => asyncForEach(portsWithIndexes, port => port.createIndexes());
