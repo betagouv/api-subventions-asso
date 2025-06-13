7cfef208cda2b9bf0c6e71542a17f5fd4b6e084a
alice.telescoop <alice@telescoop.fr>
2025-06-13 18:13:24 +0200

    feat(api): fix applicationFlatProvider typing

--- packages/api/src/modules/applicationFlat/@types/applicationFlatProvider.ts
+++ packages/api/src/modules/applicationFlat/@types/applicationFlatProvider.ts
@@ -2,7 +2,7 @@ import Provider from "../../providers/@types/IProvider";
 import { ApplicationFlatEntity } from "../../../entities/ApplicationFlatEntity";
 
 export default interface ApplicationFlatProvider extends Provider {
-    isApplicationFlatProvider: true;
+    isApplicationFlatProvider: boolean;
 
     saveFlatFromStream(stream: ReadableStream<ApplicationFlatEntity>): void;
     // is supposed to call flatService.saveFromStream
