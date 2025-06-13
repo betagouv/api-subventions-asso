8c3045c3622532e79ecf79b3971a2a4646011013
alice.telescoop <alice@telescoop.fr>
2025-06-13 18:13:24 +0200

    feat(api): fix applicationFlatProvider typing

--- packages/api/src/modules/applicationFlat/@types/applicationFlatProvider.ts
+++ packages/api/src/modules/applicationFlat/@types/applicationFlatProvider.ts
@@ -2,7 +2,7 @@ import Provider from "../../providers/@types/IProvider";
 import { ApplicationFlatEntity } from "../../../entities/ApplicationFlatEntity";
 
 export default interface ApplicationFlatProvider extends Provider {
-    isApplicationFlatProvider: true;
+    isApplicationFlatProvider: boolean;
 
     getApplicationFlatStream(START_YEAR: number, END_YEAR: number): ReadableStream<ApplicationFlatEntity>;
 }
