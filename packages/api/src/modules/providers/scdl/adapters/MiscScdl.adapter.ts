527cfa0d4047a67257f085971956ab75fec3bfea
alice.telescoop <alice@telescoop.fr>
2025-06-19 17:13:28 +0200

    feat(api): scdl to application flat adapter

--- packages/api/src/modules/providers/scdl/adapters/MiscScdl.adapter.ts
+++ packages/api/src/modules/providers/scdl/adapters/MiscScdl.adapter.ts
@@ -4,6 +4,9 @@ import { sameDateNextYear } from "../../../../shared/helpers/DateHelper";
 import MiscScdlGrantProducerEntity from "../entities/MiscScdlGrantProducerEntity";
 import { ScdlGrantEntity } from "../@types/ScdlGrantEntity";
 import { RawApplication } from "../../../grant/@types/rawGrant";
+import { ApplicationFlatEntity, ApplicationNature, PaymentCondition } from "../../../../entities/ApplicationFlatEntity";
+import { GenericAdapter } from "../../../../shared/GenericAdapter";
+import crypto from "crypto";
 
 export default class MiscScdlAdapter {
     public static toDemandeSubvention(entity: MiscScdlGrantProducerEntity): DemandeSubvention {
@@ -64,5 +67,81 @@ export default class MiscScdlAdapter {
     private static _status(entity: MiscScdlGrantProducerEntity | ScdlGrantEntity) {
         // not given by provider but this is the rule from Paris
         return entity.amount > 0 ? ApplicationStatus.GRANTED : ApplicationStatus.REFUSED;
+    }
+
+    private static normalizePaymentConditions(rawValue?: string): PaymentCondition | null {
+        if (!rawValue) return null;
+        if (rawValue.match(/unique/gi)) return PaymentCondition.UNIQUE;
+        if (rawValue.match(/[eé]chelonn[eé]/gi)) return PaymentCondition.PHASED;
+        return PaymentCondition.OTHER;
+    }
+
+    private static normalizePaymentNature(rawValue?: string): ApplicationNature | null {
+        if (!rawValue) return null;
+        if (rawValue.match(/(aide |versement )?(en )?num[ée]raire/gi)) return ApplicationNature.MONEY;
+        if (rawValue.match(/(aide )?(en )?nature/gi)) return ApplicationNature.NATURE;
+        return null;
+    }
+
+    private static getNormalizedPaymentDates(entity: MiscScdlGrantProducerEntity): Date[] {
+        const res: Date[] = [];
+        if (entity?.paymentStartDate) res.push(entity.paymentStartDate);
+        if (entity?.paymentEndDate) res.push(entity.paymentEndDate);
+        return res;
+    }
+
+    static toApplicationFlat(entity: MiscScdlGrantProducerEntity, lastUpdate: Date): ApplicationFlatEntity {
+        const dataHash = crypto.createHash("md5").update(JSON.stringify(entity)).digest("hex"); // TODO reather use same than in dbo to find back the raw data
+        const nature = MiscScdlAdapter.normalizePaymentNature(entity.paymentNature);
+
+        return {
+            requestYear: GenericAdapter.NOT_APPLICABLE_VALUE,
+            pluriannualYears: GenericAdapter.NOT_APPLICABLE_VALUE,
+
+            cofinancingRequested: GenericAdapter.NOT_APPLICABLE_VALUE,
+            paymentCondition: MiscScdlAdapter.normalizePaymentConditions(entity.paymentConditions),
+            conventionDate: entity.conventionDate,
+            decisionDate: GenericAdapter.NOT_APPLICABLE_VALUE,
+            depositDate: GenericAdapter.NOT_APPLICABLE_VALUE,
+            paymentPeriodDates: MiscScdlAdapter.getNormalizedPaymentDates(entity),
+            paymentConditionDesc: entity.paymentConditions, // potentially redundant but else info are missing
+            joinKeyDesc: GenericAdapter.NOT_APPLICABLE_VALUE,
+            scheme: entity.aidSystem,
+            ej: GenericAdapter.NOT_APPLICABLE_VALUE,
+            budgetaryYear: entity.exercice,
+            allocatorId: entity.allocatorSiret,
+            managingAuthorityId: null,
+            confinancersId: GenericAdapter.NOT_APPLICABLE_VALUE,
+            beneficiaryEstablishmentId: entity.allocatorSiret,
+            joinKeyId: GenericAdapter.NOT_APPLICABLE_VALUE,
+            idRAE: entity.idRAE,
+            applicationId: GenericAdapter.NOT_APPLICABLE_VALUE,
+            applicationProviderId: GenericAdapter.NOT_APPLICABLE_VALUE,
+            instructiveDepartementId: GenericAdapter.NOT_APPLICABLE_VALUE,
+            uniqueId: `${entity.producerSlug}-${entity.exercice}-${dataHash}`,
+            paymentId: GenericAdapter.NOT_APPLICABLE_VALUE,
+            grantedAmount: entity.amount, // TODO pourcentage subv ? I think not
+            requestedAmount: null,
+            totalAmount: entity.amount,
+            nature: nature,
+            allocatorName: entity.allocatorName,
+            managingAuthorityName: GenericAdapter.NOT_APPLICABLE_VALUE,
+            instructiveDepartmentName: GenericAdapter.NOT_APPLICABLE_VALUE,
+            cofinancersNames: GenericAdapter.NOT_APPLICABLE_VALUE,
+            ueNotification: entity.UeNotification,
+            object: entity.object,
+            pluriannual: GenericAdapter.NOT_APPLICABLE_VALUE,
+            subventionPercentage: entity.grantPercentage,
+            provider: `scdl-${entity.producerSlug}`,
+            decisionReference: entity.decisionReference,
+            subScheme: GenericAdapter.NOT_APPLICABLE_VALUE,
+            statusLabel: ApplicationStatus.GRANTED,
+            allocatorIdType: "siret",
+            managingAuthorityIdType: GenericAdapter.NOT_APPLICABLE_VALUE,
+            cofinancersIdType: GenericAdapter.NOT_APPLICABLE_VALUE,
+            instructiveDepartmentIdType: GenericAdapter.NOT_APPLICABLE_VALUE,
+            beneficiaryEstablishmentIdType: "siret",
+            updateDate: lastUpdate,
+        };
     }
 }
