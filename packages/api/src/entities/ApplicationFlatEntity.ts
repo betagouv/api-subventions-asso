cedc011e93f358098a8515830e6aac4375cbb041
alice.telescoop <alice@telescoop.fr>
2025-06-20 16:39:59 +0200

    feat(api): null not undefined

--- packages/api/src/entities/ApplicationFlatEntity.ts
+++ packages/api/src/entities/ApplicationFlatEntity.ts
@@ -23,50 +23,50 @@ type MandatoryApplicationFlatEntity = {
     applicationProviderId: string;
     provider: string;
     beneficiaryEstablishmentId: string;
-    beneficiaryEstablishmentIdType?: string;
+    beneficiaryEstablishmentIdType: string;
     budgetaryYear: number;
     statusLabel: ApplicationStatus;
     requestedAmount: number | null;
-    grantedAmount?: number;
+    grantedAmount: number | null;
+    updateDate: Date;
 };
 
 type ApplicationFlatEntityToEnableNA = {
-    joinKeyId?: string;
-    joinKeyDesc?: string;
-    allocatorName?: string;
-    allocatorIdType?: IdentifierIdName;
-    allocatorId?: string;
-    managingAuthorityName?: string;
-    managingAuthorityId?: string;
-    managingAuthorityIdType?: IdentifierIdName;
-    instructiveDepartmentName?: string;
-    instructiveDepartmentIdType?: IdentifierIdName;
-    instructiveDepartementId?: string;
-    pluriannual?: boolean;
-    pluriannualYears?: number[];
-    decisionDate?: Date;
-    conventionDate?: Date;
-    decisionReference?: string;
-    depositDate?: Date;
-    requestYear?: number;
-    scheme?: string; // dispositif
-    subScheme?: string; // sous dispositif
-    object?: string;
+    joinKeyId: string;
+    joinKeyDesc: string;
+    allocatorName: string;
+    allocatorIdType: IdentifierIdName;
+    allocatorId: string;
+    managingAuthorityName: string;
+    managingAuthorityId: string;
+    managingAuthorityIdType: IdentifierIdName;
+    instructiveDepartmentName: string;
+    instructiveDepartmentIdType: IdentifierIdName;
+    instructiveDepartementId: string;
+    pluriannual: boolean;
+    pluriannualYears: number[];
+    decisionDate: Date;
+    conventionDate: Date;
+    decisionReference: string;
+    depositDate: Date;
+    requestYear: number;
+    scheme: string; // dispositif
+    subScheme: string; // sous dispositif
+    object: string;
     nature: ApplicationNature;
-    totalAmount?: number;
-    ej?: string;
-    paymentId?: string;
-    paymentCondition?: PaymentCondition;
-    paymentConditionDesc?: string;
-    paymentPeriodDates?: Date[];
-    cofinancingRequested?: boolean;
-    cofinancersNames?: string[];
-    cofinancersIdType?: IdentifierIdName[];
-    confinancersId?: string[];
-    idRAE?: string;
-    ueNotification?: boolean;
-    subventionPercentage?: number;
-    updateDate: Date;
+    totalAmount: number;
+    ej: string;
+    paymentId: string;
+    paymentCondition: PaymentCondition;
+    paymentConditionDesc: string;
+    paymentPeriodDates: Date[];
+    cofinancingRequested: boolean;
+    cofinancersNames: string[];
+    cofinancersIdType: IdentifierIdName[];
+    confinancersId: string[];
+    idRAE: string;
+    ueNotification: boolean;
+    subventionPercentage: number;
 };
 
 export type ApplicationFlatEntity = MandatoryApplicationFlatEntity &
