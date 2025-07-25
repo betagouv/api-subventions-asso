8e71e477312149cfe2189a5744b4c12240ad89bc
alice.telescoop <alice@telescoop.fr>
2025-07-25 16:40:04 +0200

    feat(api): aggregation pipeline and adapter to create dauphin applicationFlat

--- packages/api/src/modules/providers/dauphin-gispro/dauphin.service.ts
+++ packages/api/src/modules/providers/dauphin-gispro/dauphin.service.ts
@@ -18,12 +18,17 @@ import GrantProvider from "../../grant/@types/GrantProvider";
 import dauphinPort from "../../../dataProviders/db/providers/dauphin/dauphin.port";
 import DauphinGisproDbo from "../../../dataProviders/db/providers/dauphin/DauphinGisproDbo";
 import DauphinSubventionDto from "./dto/DauphinSubventionDto";
-import DauphinDtoAdapter from "./adapters/DauphinDtoAdapter";
+import DauphinDtoAdapter, { InconsistentAggregationError } from "./adapters/DauphinDtoAdapter";
 import { StructureIdentifier } from "../../../identifierObjects/@types/StructureIdentifier";
+import ApplicationFlatProvider from "../../applicationFlat/@types/applicationFlatProvider";
+import { ReadableStream } from "stream/web";
+import { ApplicationFlatEntity } from "../../../entities/ApplicationFlatEntity";
+import applicationFlatService from "../../applicationFlat/applicationFlat.service";
+import { cursorToStream } from "../../applicationFlat/applicationFlat.helper";
 
 export class DauphinService
     extends ProviderCore
-    implements DemandesSubventionsProvider<DauphinGisproDbo>, DocumentProvider, GrantProvider
+    implements DemandesSubventionsProvider<DauphinGisproDbo>, DocumentProvider, GrantProvider, ApplicationFlatProvider
 {
     constructor() {
         super({
@@ -33,6 +38,52 @@ export class DauphinService
                 "Dauphin est un système d'information développé par MGDIS permettant aux associations de déposer des demandes de subvention dans le cadre de la politique de la ville et aux services instructeurs d'effectuer de la co-instruction.",
             id: "dauphin",
         });
+    }
+
+    /**
+     * |-------------------------|
+     * |   Flat Part          |
+     * |-------------------------|
+     */
+
+    isApplicationFlatProvider = true as const;
+
+    async generateTempJoinedCollection() {
+        await dauphinPort.createSimplifiedDauphinBeforeJoin();
+        await dauphinPort.joinGisproToSimplified();
+    }
+
+    async feedApplicationFlat() {
+        await this.generateTempJoinedCollection();
+        const cursor = dauphinPort.findAllTempCursor();
+        const errors: InconsistentAggregationError[] = [];
+        const stream = cursorToStream(cursor, simplified => {
+            try {
+                return DauphinDtoAdapter.simplifiedJoinedToApplicationFlat(simplified);
+            } catch (e) {
+                if (e instanceof InconsistentAggregationError) {
+                    errors.push(e);
+                    return null;
+                }
+                throw e;
+            }
+        });
+        await this.saveFlatFromStream(stream);
+
+        if (errors.length)
+            console.log(
+                "Les enregistrements suivants n'ont pas été sauvegardés car l'aggrégation était incohérente : ",
+                errors.map(
+                    e =>
+                        `dossier n°${e.codeDossier}\trefAdmin ${e.referenceAdministrative} ont pour le champ ${e.field} les valeurs ${e.valueList}`,
+                    "\nEn conséquence la collection temporaire n'a pas été vidée.",
+                ),
+            );
+        else await dauphinPort.cleanTempCollection();
+    }
+
+    async saveFlatFromStream(stream: ReadableStream<ApplicationFlatEntity>): Promise<void> {
+        await applicationFlatService.saveFromStream(stream);
     }
 
     /**
