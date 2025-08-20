b7c11dce33eedcbba78db4cf8ac721e3f124f504
alice.telescoop <alice@telescoop.fr>
2025-08-20 14:07:03 +0200

    test(api): services tests

--- packages/api/src/modules/providers/demarchesSimplifiees/demarchesSimplifiees.service.test.ts
+++ packages/api/src/modules/providers/demarchesSimplifiees/demarchesSimplifiees.service.test.ts
@@ -9,6 +9,9 @@ jest.mock("./adapters/DemarchesSimplifieesEntityAdapter");
 jest.mock("./adapters/DemarchesSimplifieesDtoAdapter");
 jest.mock("@inquirer/prompts");
 jest.mock("../../configurations/configurations.service");
+jest.mock("../../applicationFlat/applicationFlat.service");
+jest.mock("../../applicationFlat/applicationFlat.helper");
+jest.mock("stream/web");
 
 import configurationsService from "../../configurations/configurations.service";
 import DemarchesSimplifieesDtoAdapter from "./adapters/DemarchesSimplifieesDtoAdapter";
@@ -32,10 +35,186 @@ import EstablishmentIdentifier from "../../../identifierObjects/EstablishmentIde
 import { DemarchesSimplifieesSuccessDto } from "./dto/DemarchesSimplifieesDto";
 import DemarchesSimplifieesDataEntity from "./entities/DemarchesSimplifieesDataEntity";
 import { input } from "@inquirer/prompts";
+import applicationFlatService from "../../applicationFlat/applicationFlat.service";
+import { cursorToStream } from "../../applicationFlat/applicationFlat.helper";
+import { DefaultObject } from "../../../@types";
+import { ApplicationFlatEntity } from "../../../entities/ApplicationFlatEntity";
+import { FindCursor, WithId } from "mongodb";
+import { ReadableStream } from "stream/web";
 
 jest.mock("lodash");
 
 describe("DemarchesSimplifieesService", () => {
+    describe("flat part", () => {
+        const STREAM = {} as unknown as ReadableStream<ApplicationFlatEntity>;
+
+        describe("saveFlatFromStream", () => {
+            it("calls applicationFlatService", async () => {
+                await demarchesSimplifieesService.saveFlatFromStream(STREAM);
+                expect(applicationFlatService.saveFromStream).toHaveBeenCalledWith(STREAM);
+            });
+        });
+
+        describe("initApplicationFlat", () => {
+            const DEMARCHE_ID = 42;
+            const CURSOR = "CURSOR" as unknown as FindCursor<WithId<DemarchesSimplifieesDataEntity>>; // TODO
+            const SCHEMA = { demarcheId: DEMARCHE_ID } as unknown as DemarchesSimplifieesSchema;
+            const SCHEMAS_DICT: DefaultObject<DemarchesSimplifieesSchema> = { [DEMARCHE_ID]: SCHEMA };
+            const ADAPTED = {} as unknown as ApplicationFlatEntity;
+            const DBO = { demarcheId: DEMARCHE_ID };
+
+            let getSchemasByIdSpy: jest.SpyInstance; // TODO mock
+            let toFlatAndValidateSpy: jest.SpyInstance;
+            let saveFlatSpy: jest.SpyInstance;
+
+            beforeEach(() => {
+                getSchemasByIdSpy = jest
+                    // @ts-expect-error - spy private
+                    .spyOn(demarchesSimplifieesService, "getSchemasByIds")
+                    // @ts-expect-error - spy private
+                    .mockResolvedValue(SCHEMAS_DICT);
+                toFlatAndValidateSpy = jest
+                    .spyOn(demarchesSimplifieesService, "toFlatAndValidate")
+                    .mockReturnValue(ADAPTED);
+                saveFlatSpy = jest
+                    .spyOn(demarchesSimplifieesService, "saveFlatFromStream")
+                    .mockReturnValue(Promise.resolve());
+
+                jest.mocked(demarchesSimplifieesDataPort.findAllCursor).mockReturnValue(CURSOR);
+                jest.mocked(cursorToStream).mockReturnValue(STREAM);
+            });
+
+            afterEach(() => {
+                getSchemasByIdSpy.mockRestore();
+                toFlatAndValidateSpy.mockRestore();
+                saveFlatSpy.mockRestore();
+            });
+
+            it("finds cursor", async () => {
+                await demarchesSimplifieesService.initApplicationFlat();
+                expect(demarchesSimplifieesDataPort.findAllCursor).toHaveBeenCalled();
+            });
+
+            it("gets schemas", async () => {
+                await demarchesSimplifieesService.initApplicationFlat();
+                expect(getSchemasByIdSpy).toHaveBeenCalled();
+            });
+
+            it("calls helper's cursorToStream with cursor", async () => {
+                await demarchesSimplifieesService.initApplicationFlat();
+                expect(cursorToStream).toHaveBeenCalledWith(CURSOR, expect.any(Function));
+            });
+
+            describe("adaptation", () => {
+                it("calls saveFlatAndValidate with each proper schema", async () => {
+                    await demarchesSimplifieesService.initApplicationFlat();
+                    const adapter = jest.mocked(cursorToStream).mock.calls[0][1];
+                    const expected = ADAPTED;
+                    const actual = adapter(DBO);
+                    expect(toFlatAndValidateSpy).toHaveBeenCalledWith(DBO, SCHEMA);
+                    expect(actual).toEqual(expected);
+                });
+            });
+
+            it("calls saveFlatFromStream with stream from helper", async () => {
+                await demarchesSimplifieesService.initApplicationFlat();
+                expect(saveFlatSpy).toHaveBeenCalledWith(STREAM);
+            });
+        });
+
+        describe("toFlatAndValidate", () => {
+            const DBO = "DBO" as unknown as DemarchesSimplifieesDataEntity;
+            const SCHEMA = { flatSchema: {} } as DemarchesSimplifieesSchema;
+            let isDraftSpy: jest.SpyInstance;
+            const ADAPTED = { requestedAmount: 456 } as ApplicationFlatEntity;
+
+            beforeAll(() => {
+                // @ts-expect-error -- private method
+                isDraftSpy = jest.spyOn(demarchesSimplifieesService, "isDraft").mockReturnValue(false);
+                jest.mocked(DemarchesSimplifieesEntityAdapter.toFlat).mockReturnValue(ADAPTED);
+            });
+
+            afterAll(() => {
+                isDraftSpy.mockRestore();
+                jest.mocked(DemarchesSimplifieesEntityAdapter.toFlat).mockRestore();
+            });
+
+            it("returns null if no flat schema", () => {
+                const actual = demarchesSimplifieesService.toFlatAndValidate(DBO, {} as DemarchesSimplifieesSchema);
+                expect(actual).toBeNull();
+            });
+
+            it("checks if is draft", () => {
+                demarchesSimplifieesService.toFlatAndValidate(DBO, SCHEMA);
+                expect(isDraftSpy).toHaveBeenCalledWith(DBO);
+            });
+
+            it("returns null if draft", () => {
+                // TODO on devrait juste pas les stocker..?
+                isDraftSpy.mockReturnValueOnce(true);
+                const actual = demarchesSimplifieesService.toFlatAndValidate(DBO, SCHEMA);
+                expect(actual).toBeNull();
+            });
+
+            it("calls adapter", () => {
+                demarchesSimplifieesService.toFlatAndValidate(DBO, SCHEMA);
+            });
+
+            it("returns null if no requested amount", () => {
+                jest.mocked(DemarchesSimplifieesEntityAdapter.toFlat).mockReturnValueOnce({} as ApplicationFlatEntity);
+                const actual = demarchesSimplifieesService.toFlatAndValidate(DBO, SCHEMA);
+                expect(actual).toBeNull();
+            });
+
+            it("return res from adapter", () => {
+                const expected = ADAPTED;
+                const actual = demarchesSimplifieesService.toFlatAndValidate(DBO, SCHEMA);
+                expect(actual).toEqual(expected);
+            });
+        });
+
+        describe("bulkUpdateApplicationFlat", () => {
+            const ENTITIES = ["e1", "e2"] as unknown as DemarchesSimplifieesDataEntity[];
+            const SCHEMA = "SCHEMA" as unknown as DemarchesSimplifieesSchema;
+            const STREAM = "STREAM" as unknown as ReadableStream;
+            let saveSpy: jest.SpyInstance;
+            let adaptSpy: jest.SpyInstance;
+
+            beforeAll(() => {
+                saveSpy = jest
+                    .spyOn(demarchesSimplifieesService, "saveFlatFromStream")
+                    .mockReturnValue(Promise.resolve());
+                adaptSpy = jest
+                    .spyOn(demarchesSimplifieesService, "toFlatAndValidate")
+                    .mockImplementation(e => ((e as unknown as string) + "a") as unknown as ApplicationFlatEntity);
+                jest.mocked(ReadableStream.from).mockReturnValue(STREAM);
+            });
+            afterAll(() => {});
+
+            it("doesn't save anything if no schema", () => {
+                demarchesSimplifieesService.bulkUpdateApplicationFlat(ENTITIES, null);
+                expect(saveSpy).not.toHaveBeenCalled();
+            });
+
+            it("adapts each entity", () => {
+                demarchesSimplifieesService.bulkUpdateApplicationFlat(ENTITIES, SCHEMA);
+                expect(adaptSpy).toHaveBeenCalledWith(ENTITIES[0], SCHEMA);
+                expect(adaptSpy).toHaveBeenCalledWith(ENTITIES[1], SCHEMA);
+            });
+
+            it("creates stream from filtered adapted data", () => {
+                adaptSpy.mockReturnValueOnce(null);
+                demarchesSimplifieesService.bulkUpdateApplicationFlat(ENTITIES, SCHEMA);
+                expect(ReadableStream.from).toHaveBeenCalledWith(["e2a"]);
+            });
+
+            it("saves created stream", () => {
+                demarchesSimplifieesService.bulkUpdateApplicationFlat(ENTITIES, SCHEMA);
+                expect(saveSpy).toHaveBeenCalledWith(STREAM);
+            });
+        });
+    });
+
     describe("getSchemasByIds", () => {
         beforeAll(() => {
             // @ts-expect-error mock
@@ -292,10 +471,12 @@ describe("DemarchesSimplifieesService", () => {
     });
 
     describe("updateDataByFormId", () => {
-        const FORM_ID = 12345;
-
         let sendQueryMock: jest.SpyInstance;
         let toEntitiesMock: jest.SpyInstance;
+        let upsertFlatMock: jest.SpyInstance;
+
+        const FORM_ID = 12345;
+
         const lastModified = new Date();
         lastModified.setDate(lastModified.getDate() - 1);
         const DOSSIER = {
@@ -341,6 +522,7 @@ describe("DemarchesSimplifieesService", () => {
             },
         });
         const DEMARCHE = buildDemarche([DOSSIER]);
+        const SCHEMA = {} as DemarchesSimplifieesSchema;
 
         beforeAll(() => {
             demarchesSimplifieesService.lastModified = new Date();
@@ -352,6 +534,10 @@ describe("DemarchesSimplifieesService", () => {
             toEntitiesMock = jest.spyOn(DemarchesSimplifieesDtoAdapter, "toEntities").mockReturnValue([DOSSIER]);
             // @ts-expect-error mock
             demarchesSimplifieesDataPort.upsert.mockResolvedValue();
+            jest.mocked(demarchesSimplifieesSchemaPort.findById).mockResolvedValue(SCHEMA);
+            upsertFlatMock = jest
+                .spyOn(demarchesSimplifieesService, "bulkUpdateApplicationFlat")
+                .mockImplementation(jest.fn());
         });
 
         afterAll(() => {
@@ -359,6 +545,7 @@ describe("DemarchesSimplifieesService", () => {
             toEntitiesMock.mockRestore();
             // @ts-expect-error mock
             demarchesSimplifieesSchemaPort.upsert.mockRestore();
+            upsertFlatMock.mockRestore();
         });
 
         it("should call sendQuery", async () => {
@@ -377,7 +564,14 @@ describe("DemarchesSimplifieesService", () => {
             const expected = [DOSSIER];
             sendQueryMock.mockResolvedValueOnce(DEMARCHE);
             await demarchesSimplifieesService.updateDataByFormId(FORM_ID);
-            expect(demarchesSimplifieesDataPort.bulkUpsert).toBeCalledWith(expected);
+            expect(demarchesSimplifieesDataPort.bulkUpsert).toHaveBeenCalledWith(expected);
+        });
+
+        it("should upsert flat data", async () => {
+            const expected = [DOSSIER];
+            sendQueryMock.mockResolvedValueOnce(DEMARCHE);
+            await demarchesSimplifieesService.updateDataByFormId(FORM_ID);
+            expect(upsertFlatMock).toHaveBeenCalledWith(expected, SCHEMA);
         });
 
         it("skips unpublished demarches - does not adapt nor insert", async () => {
@@ -390,10 +584,16 @@ describe("DemarchesSimplifieesService", () => {
         it("should bulkUpsert two times for 10001 demandes", async () => {
             toEntitiesMock.mockReturnValueOnce(Array(2001).fill(DOSSIER));
             await demarchesSimplifieesService.updateDataByFormId(FORM_ID);
-            expect(demarchesSimplifieesDataPort.bulkUpsert).toBeCalledTimes(2);
+            expect(demarchesSimplifieesDataPort.bulkUpsert).toHaveBeenCalledTimes(2);
         });
 
-        it("skips unchanged demandes - not in bulk upsert", async () => {
+        it("should upsert flat two times for 10001 demandes", async () => {
+            toEntitiesMock.mockReturnValueOnce(Array(2001).fill(DOSSIER));
+            await demarchesSimplifieesService.updateDataByFormId(FORM_ID);
+            expect(upsertFlatMock).toHaveBeenCalledTimes(2);
+        });
+
+        it("skips unchanged demandes - not in bulk upsert nor flat upsert", async () => {
             const oldDossier = {
                 ...DOSSIER,
                 demande: { ...DOSSIER.demande, dateDerniereModification: new Date(0).toString() },
@@ -401,7 +601,8 @@ describe("DemarchesSimplifieesService", () => {
             toEntitiesMock.mockReturnValueOnce([oldDossier]);
             const expected = [];
             await demarchesSimplifieesService.updateDataByFormId(FORM_ID);
-            expect(demarchesSimplifieesDataPort.bulkUpsert).toBeCalledWith(expected);
+            expect(demarchesSimplifieesDataPort.bulkUpsert).toHaveBeenCalledWith(expected);
+            expect(upsertFlatMock).toHaveBeenCalledWith(expected, SCHEMA);
         });
     });
 
