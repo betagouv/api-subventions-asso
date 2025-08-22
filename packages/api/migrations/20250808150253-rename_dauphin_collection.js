ccd36fb6c68ddbbc4b5a0d9a0bbc4b0a8cbc7821
alice.telescoop <alice@telescoop.fr>
2025-08-22 14:50:12 +0200

    fix(api): make migrations more resilient

--- packages/api/migrations/20250808150253-rename_dauphin_collection.js
+++ packages/api/migrations/20250808150253-rename_dauphin_collection.js
@@ -1,9 +1,33 @@
 module.exports = {
     async up(db) {
-        await db.renameCollection("dauphin-gispro", "dauphin");
+        try {
+            await db.renameCollection("dauphin-gispro", "dauphin");
+        } catch (e) {
+            if (e.codeName === "NamespaceExists") {
+                console.log(JSON.stringify(e));
+                const saveName = `dauphin-save-${Date.now()}`;
+                console.log(
+                    `collection 'dauphin' existed already. Previous one renamed to '${saveName}'. You may want to delete that old one if it was not meant to be there`,
+                );
+                await db.renameCollection("dauphin", saveName);
+                await db.renameCollection("dauphin-gispro", "dauphin");
+            }
+        }
     },
 
     async down(db) {
-        await db.renameCollection("dauphin", "dauphin-gispro");
+        try {
+            await db.renameCollection("dauphin", "dauphin-gispro");
+        } catch (e) {
+            if (e.codeName === "NamespaceExists") {
+                console.log(JSON.stringify(e));
+                const saveName = `dauphin-gispro-save-${Date.now()}`;
+                console.log(
+                    `collection 'dauphin-gispro' existed already. Previous one renamed to '${saveName}'. You may want to delete that old one if it was not meant to be there`,
+                );
+                await db.renameCollection("dauphin-gispro", saveName);
+                await db.renameCollection("dauphin", "dauphin-gispro");
+            }
+        }
     },
 };
