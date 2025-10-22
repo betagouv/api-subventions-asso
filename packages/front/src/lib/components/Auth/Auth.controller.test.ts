d4c8ff813840a56b7cda928544863ee6d6fd78a0
Maxime Silvère Meunier <mxmeunier@gmail.com>
2025-10-22 10:23:34 +0200

    refactor(front): auth controller unit tests

--- packages/front/src/lib/components/Auth/Auth.controller.test.ts
+++ packages/front/src/lib/components/Auth/Auth.controller.test.ts
@@ -3,14 +3,19 @@ import AuthController from "$lib/components/Auth/Auth.controller";
 import authService from "$lib/resources/auth/auth.service";
 import AuthLevels from "$lib/resources/auth/authLevels";
 import depositLogService from "$lib/resources/deposit-log/depositLog.service";
+import type { Page } from "@sveltejs/kit";
 
+vi.mock("$lib/resources/deposit-log/depositLog.service");
 vi.mock("$lib/store/kit.store", () => ({ page: { subscribe: vi.fn() } }));
 vi.mock("$lib/resources/auth/auth.service");
 
 describe("AuthController", () => {
+    // @ts-expect-error: mock page value
+    const PAGE = { data: { authLevel: AuthLevels.ADMIN } } as Page<Record<string, string>>;
+    const mockSubscribe = vi.mocked(page.subscribe);
+
     describe("init", () => {
-        const authServiceMock = vi.spyOn(authService, "initUserInApp").mockImplementation(vi.fn);
-        vi.spyOn(depositLogService, "getDepositLog").mockImplementation(vi.fn());
+        const authServiceMock = vi.spyOn(authService, "initUserInApp").mockImplementation(vi.fn());
 
         let controller;
 
@@ -23,28 +28,28 @@ describe("AuthController", () => {
             expect(authServiceMock).toHaveBeenCalledTimes(1);
         });
 
+        it("gets deposits log", async () => {
+            await controller.init();
+            expect(vi.mocked(depositLogService.getDepositLog)).toHaveBeenCalledOnce();
+        });
+
         it("should subscribe page", async () => {
             await controller.init();
-            expect(page.subscribe).toHaveBeenCalledTimes(1);
+            expect(mockSubscribe).toHaveBeenCalledTimes(1);
         });
 
         it("page subscribe's callback calls controlAuth", async () => {
-            vi.mocked(authService.initUserInApp).mockReturnValueOnce();
             await controller.init();
-            const callback = vi.fn(vi.mocked(page.subscribe).mock.calls[0][0]);
-            const PAGE = { data: { authLevel: AuthLevels.ADMIN } };
-            // @ts-expect-error: mock page data
+            const callback = vi.mocked(mockSubscribe).mock.calls[0][0];
             callback(PAGE);
-            expect(authService.controlAuth).toHaveBeenCalledWith(2);
+            expect(authService.controlAuth).toHaveBeenCalledWith(AuthLevels.ADMIN);
         });
 
         it("page subscribe's callback sets result from controlAuth in show store", async () => {
-            vi.mocked(authService.initUserInApp).mockReturnValueOnce();
             vi.mocked(authService.controlAuth).mockReturnValueOnce(true);
             const showSetSpy = vi.spyOn(controller.show, "set");
             await controller.init();
-            const callback = vi.fn(vi.mocked(page.subscribe).mock.calls[0][0]);
-            const PAGE = { data: { authLevel: AuthLevels.ADMIN } };
+            const callback = vi.mocked(mockSubscribe).mock.calls[0][0];
             callback(PAGE);
             expect(showSetSpy).toHaveBeenCalledWith(true);
         });
