ffc68ac405c396567cf616f18999832877b72737
Romain Mercier <romain.mercier321@gmail.com>
2025-11-12 15:25:55 +0100

    feat(front, api): #3626 step 4 front
    - including back endpoint to generate csv string with existing scdl data

--- packages/front/src/lib/resources/deposit-log/depositLog.port.ts
+++ packages/front/src/lib/resources/deposit-log/depositLog.port.ts
@@ -5,6 +5,7 @@ class DepositLogPort {
     BASE_PATH = "/parcours-depot";
 
     getResource(resource?: string) {
+        console.log(`${this.BASE_PATH}${resource ? "/" + resource : ""}`);
         return requestsService.get(`${this.BASE_PATH}${resource ? "/" + resource : ""}`);
     }
 
