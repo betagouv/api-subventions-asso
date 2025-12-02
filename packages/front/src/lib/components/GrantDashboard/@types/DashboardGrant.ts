59690ceda06c0d5b3f3ee38a3787d06e39a5a528
Maxime Meunier <mxmeunier@gmail.com>
2025-12-02 12:46:32 +0100

    feat(api): #3678 use grants v2 dashboard (#3735)

--- packages/front/src/lib/components/GrantDashboard/@types/DashboardGrant.ts
+++ packages/front/src/lib/components/GrantDashboard/@types/DashboardGrant.ts
@@ -1,17 +1,16 @@
-import type { ApplicationStatus } from "dto";
+import type { ApplicationStatus, GrantFlatDto } from "dto";
 import type { TableCell } from "$lib/dsfr/TableCell.types";
-import type { FlatGrant } from "$lib/resources/@types/FlattenGrant";
 
 export type DashboardApplication = {
-    montantAccorde?: number;
-    montantDemande?: number;
+    montantAccorde: number | null;
+    montantDemande: number | null;
     date_demande: Date | undefined;
     postal_code?: string;
     service_instructeur: string;
     dispositif?: string;
     nomProjet: string;
     statut_label: ApplicationStatus;
-    annee_demande: number;
+    annee_demande?: number;
 };
 
 export type DashboardProgram =
@@ -39,5 +38,5 @@ export type SortableRow = {
     payment?: DashboardPayment | null;
     application?: DashboardApplication | null;
     granted: boolean;
-    flatGrant: FlatGrant;
+    grant: GrantFlatDto;
 };
--- packages/front/src/lib/components/GrantDashboard/Modals/ApplicationInfoModal.svelte
+++ packages/front/src/lib/components/GrantDashboard/Modals/ApplicationInfoModal.svelte
@@ -1,20 +1,20 @@
 <script>
     import { data } from "$lib/store/modal.store";
-    import { capitalizeFirstLetter } from "$lib/helpers/stringHelper";
     import { numberToEuro } from "$lib/helpers/dataHelper.js";
 </script>
 
+<!-- ApplicationFlatDto -->
 {#if $data.application}
     <section>
         <h4 class="fr-icon-arrow-right-line">Informations collectées</h4>
         <p class="fr-text--lead">
-            {#if $data.application.montants.accorde}
-                <span class="fr-text--bold">{numberToEuro($data.application.montants.accorde)}</span>
-                ont été accordés{#if $data.application.montants.demande}
-                    sur <span class="fr-text--bold">{numberToEuro($data.application.montants.demande)}</span>
+            {#if $data.application.montantAccorde}
+                <span class="fr-text--bold">{numberToEuro($data.application.montantAccorde)}</span>
+                ont été accordés {#if $data.application.montantDemande}
+                    sur <span class="fr-text--bold">{numberToEuro($data.application.montantDemande)}</span>
                     demandés{/if}.
-            {:else if $data.application.montants.demande}
-                <span class="fr-text--bold">{numberToEuro($data.application.montants.demande)}</span>
+            {:else if $data.application.montantDemande}
+                <span class="fr-text--bold">{numberToEuro($data.application.montantDemande)}</span>
                 ont été demandés.
             {/if}
         </p>
@@ -29,16 +29,11 @@
                 {$data.application.date_decision}
             </p>{/if}
     </section>
-    {#each $data.application?.actions_proposee || [] as action, index (index)}
+    {#if $data.application?.objet}
         <section>
-            <h4 class="fr-icon-arrow-right-line">{capitalizeFirstLetter(action.intitule)}</h4>
-            {#each action.objectifs?.split("\n") || [] as line, index (index)}
-                {#if line.length}
-                    <p>{line}</p>
-                {/if}
-            {/each}
+            <p>{$data.application.objet}</p>
         </section>
-    {/each}
+    {/if}
 {/if}
 
 <style>
@@ -58,16 +53,4 @@
     section h4:is([class*=" fr-fi-"], h4[class*=" fr-icon-"], h4[class^="fr-fi-"], h4[class^="fr-icon-"])::before {
         margin-right: 0.5rem;
     }
-
-    /* uncomment when showing establishment
-    .bigger-link {
-        position: relative;
-    }
-
-    .bigger-link > a::before {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        content: "";
-    }*/
 </style>
