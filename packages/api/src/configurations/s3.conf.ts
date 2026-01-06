351a4db4dec2492b2a0ec4b9fdee29e5b55b9f43
Romain Mercier <60876891+461OceanBd@users.noreply.github.com>
2026-01-06 15:26:56 +0100

    chore: #3754 dockerize local stack (#3761)

--- packages/api/src/configurations/s3.conf.ts
+++ packages/api/src/configurations/s3.conf.ts
@@ -2,4 +2,4 @@ export const S3_ENDPOINT = process.env.S3_ENDPOINT || "http://localhost:9000";
 export const S3_REGION = process.env.S3_REGION || "us-east-1";
 export const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY;
 export const S3_SECRET_KEY = process.env.S3_SECRET_KEY;
-export const S3_BUCKET = process.env.S3_BUCKET || "scdl-files";
+export const S3_BUCKET = process.env.S3_BUCKET || "data-sub-local-scdl";
