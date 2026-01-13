2c4902df100cc347ce8d4ef506b843419377d21d
Romain Mercier <romain.mercier321@gmail.com>
2026-01-13 14:22:28 +0100

    fix(api): #3773 error deposit log

--- packages/api/migrations/20260108132729-change-uploaded-file-infos-structure.js
+++ packages/api/migrations/20260108132729-change-uploaded-file-infos-structure.js
@@ -1,28 +1,34 @@
 module.exports = {
     async up(db) {
-        await db
+        const documentsToUpdate = await db
             .collection("deposit-log")
-            .aggregate([
-                {
-                    $match: {
-                        "uploadedFileInfos.errors": { $exists: true },
-                    },
-                },
-                {
-                    $addFields: {
-                        "uploadedFileInfos.errorStats": {
-                            count: { $size: "$uploadedFileInfos.errors" },
-                            errorSample: "$uploadedFileInfos.errors",
-                        },
-                    },
-                },
-                {
-                    $project: { "uploadedFileInfos.errors": 0 },
-                },
-                {
-                    $merge: { into: "deposit-log" },
-                },
-            ])
+            .find({
+                "uploadedFileInfos.errors": { $exists: true },
+            })
             .toArray();
+
+        let updatedCount = 0;
+        for (const doc of documentsToUpdate) {
+            const errorsArray = doc.uploadedFileInfos.errors;
+            const errorStats = {
+                count: errorsArray.length,
+                errorSample: errorsArray,
+            };
+
+            await db.collection("deposit-log").updateOne(
+                { _id: doc._id },
+                {
+                    $set: {
+                        "uploadedFileInfos.errorStats": errorStats,
+                    },
+                    $unset: {
+                        "uploadedFileInfos.errors": "",
+                    },
+                },
+            );
+            updatedCount++;
+        }
+
+        console.log(`${updatedCount} docs updated`);
     },
 };
