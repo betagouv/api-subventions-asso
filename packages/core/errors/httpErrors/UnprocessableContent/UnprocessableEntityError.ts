493509301bcfe285e44ac00df87af5ef2f539f62
Romain Mercier <romain.mercier321@gmail.com>
2026-01-26 18:18:03 +0100

    refactor(api): #0000 add doc & Record less strict

--- packages/core/errors/httpErrors/UnprocessableContent/UnprocessableEntityError.ts
+++ packages/core/errors/httpErrors/UnprocessableContent/UnprocessableEntityError.ts
@@ -3,10 +3,16 @@ import { HttpError } from "../HttpError";
 export const UnprocessableEntityErrorMessage = "Unprocessable Entity";
 export const UnprocessableEntityErrorCode = 422;
 
+/**
+ *  HTTP 422 Error - Unprocessable entity
+ *  Used when the server understood the content type of the request content,
+ *  and the syntax of the request content was correct,
+ *  but it was unable to process the contained instructions.
+ */
 export class UnprocessableEntityError extends HttpError {
     status = UnprocessableEntityErrorCode;
 
-    constructor(message = UnprocessableEntityErrorMessage, cause?: Record<string, string>, code?: number) {
+    constructor(message = UnprocessableEntityErrorMessage, cause?: Record<string, unknown>, code?: number) {
         super(message, { cause });
         this.code = code;
     }
