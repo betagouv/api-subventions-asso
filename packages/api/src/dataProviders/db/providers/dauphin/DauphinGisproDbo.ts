ec3b5e4638f1694f31e5806439ec1080e930c83e
Maxime Meunier <mxmeunier@gmail.com>
2026-02-23 08:44:36 +0100

    feat(api): #0000 fix simplified dauphin without beneficiary siret (#3827)
    feat(api): fix simplified dauphin without beneficiary siret

--- packages/api/src/dataProviders/db/providers/dauphin/DauphinGisproDbo.ts
+++ packages/api/src/dataProviders/db/providers/dauphin/DauphinGisproDbo.ts
@@ -1,6 +1,7 @@
 import GisproEntity from "../../../../modules/providers/dauphin-gispro/@types/GisproEntity";
 import DauphinSubventionDto from "../../../../modules/providers/dauphin-gispro/dto/DauphinSubventionDto";
 
+// @TODO: make it extends ProviderDataEntity
 export default interface DauphinGisproDbo {
     gispro?: GisproEntity;
     dauphin: DauphinSubventionDto;
