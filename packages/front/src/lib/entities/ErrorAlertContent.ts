44fb0f4f6384f61ba7073c40651329fbc2f967a8
Romain Mercier <romain.mercier321@gmail.com>
2026-02-24 16:59:25 +0100

    feat(front): #0000 hotfix wording deposit process

--- packages/front/src/lib/entities/ErrorAlertContent.ts
+++ packages/front/src/lib/entities/ErrorAlertContent.ts
@@ -4,7 +4,7 @@ export class ErrorAlertContent {
 
     constructor(
         title: string = "Un incident technique est survenu.",
-        message: string = "Merci de réessayer ultérieurement. Si le problème continue, vous pouvez recharger la page ou contacter notre support via Crisp.",
+        message: string = "Merci de réessayer ultérieurement. Si le problème persiste, vous pouvez recharger la page ou contacter notre support via la bulle de chat.",
     ) {
         this.title = title;
         this.message = message;
