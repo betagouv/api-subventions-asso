a84ef784cd73d1ef294ff263ebe4266393cf7ce8
Guillaume Lung <guillaume.lung@gmail.com>
2026-03-10 21:36:57 +0100

    docs(api): #3847 améliorer la description swagger de la route de recherche
    Ajoute un exemple de réponse, améliore les descriptions des paramètres
    et complète les codes de retour sur la route GET /search/associations/{input}.

--- packages/api/src/interfaces/http/Search.http.ts
+++ packages/api/src/interfaces/http/Search.http.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Response, Route, Security, Tags, Query, Path } from "tsoa";
-import { PaginatedAssociationNameDto } from "dto";
 import { HttpErrorInterface } from "core";
+import { PaginatedAssociationNameDto } from "dto";
+import { Controller, Example, Get, Path, Query, Response, Route, Security, SuccessResponse, Tags } from "tsoa";
 
 import searchService from "../../modules/search/search.service";
 
@@ -9,12 +9,32 @@ import searchService from "../../modules/search/search.service";
 @Tags("Search Controller")
 export class SearchHttp extends Controller {
     /**
-     * Recherche une association via son rna, siren et nom partiel ou complet
-     * @summary Recherche une association via son rna, siren et nom partiel ou complet
+     * Recherche des associations via rna, siren et nom partiel ou complet
+     * @summary Recherche des associations via rna, siren et nom partiel ou complet
      * @param input Identifiant RNA ou Identifiant Siren ou Nom d'une association (peut-être encodé via encodeURIComponent())
-     * @param page default to 1
+     * @param page Numéro de page des résultats (commence à 1, défaut : 1)
      */
+    @Example<PaginatedAssociationNameDto>({
+        results: [
+            {
+                siren: "123456789",
+                name: "Association Exemple",
+                rna: "W123456789",
+                address: {
+                    numero: "10",
+                    voie: "Rue de l'Exemple",
+                    code_postal: "75000",
+                    commune: "Paris",
+                },
+                nbEtabs: 2,
+            },
+        ],
+        nbPages: 3,
+        page: 1,
+        total: 27,
+    })
     @Get("/associations/{input}")
+    @SuccessResponse(200)
     @Response<HttpErrorInterface>("404", "Aucune association trouvée", {
         message: "Could not match any association with given input : ${input}",
     })
