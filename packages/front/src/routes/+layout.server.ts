782b92021581692905a12521d6720f82b636ca1f
Maxime Silvère Meunier <mxmeunier@gmail.com>
2026-04-07 12:27:36 +0200

    fix(front): use dynamic private var to avoid build error

--- packages/front/src/routes/+layout.server.ts
+++ packages/front/src/routes/+layout.server.ts
@@ -1,10 +1,10 @@
-import { MATOMO_URL, MATOMO_APP_ID } from "$env/static/private";
+import { env } from "$env/dynamic/private";
 
 export const load = () => {
     return {
         matomo: {
-            url: MATOMO_URL,
-            id: MATOMO_APP_ID,
+            url: env.MATOMO_URL,
+            id: env.MATOMO_APP_ID,
         },
     };
 };
