992ee8a889b6ecee2aa2a281c50890e43af4d2ca
Maxime Silvère Meunier <mxmeunier@gmail.com>
2026-04-21 12:23:46 +0200

    test(front): fix application.helper test

--- packages/front/src/lib/components/GrantDashboard/application.helper.test.ts
+++ packages/front/src/lib/components/GrantDashboard/application.helper.test.ts
@@ -1,21 +1,28 @@
 import * as ApplicationHelper from "./application.helper";
 
 describe("Application Helper", () => {
-    describe("isSCDL", () => {
+    describe("bypassInstructor", () => {
         const SCDL_PROVIDER = "scdl-75218622100012";
         const OTHER_PROVIDER = "osiris";
 
         it("returns true if fournisseur starting with scdl-", () => {
             const expected = true;
             // @ts-expect-error: partial application flat
-            const actual = ApplicationHelper.isSCDL({ fournisseur: SCDL_PROVIDER });
+            const actual = ApplicationHelper.bypassInstructor({ fournisseur: SCDL_PROVIDER });
+            expect(actual).toEqual(expected);
+        });
+
+        it("returns true if fournisseur is helios", () => {
+            const expected = true;
+            // @ts-expect-error: partial application flat
+            const actual = ApplicationHelper.bypassInstructor({ fournisseur: "helios" });
             expect(actual).toEqual(expected);
         });
 
         it("returns false if fournisseur does not start with scdl-", () => {
             const expected = false;
             // @ts-expect-error: partial application flat
-            const actual = ApplicationHelper.isSCDL({ fournisseur: OTHER_PROVIDER });
+            const actual = ApplicationHelper.bypassInstructor({ fournisseur: OTHER_PROVIDER });
             expect(actual).toEqual(expected);
         });
     });
