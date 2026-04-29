83da8b87e86a6d11e67cdcddaa46d7a7fa8de929
Signo <6769414+sign0@users.noreply.github.com>
2026-04-29 12:43:17 +0200

    fix(front): add a disclaimer notifying users that ProConnect is temporarily unavailable

--- packages/front/src/lib/components/Header/Header.svelte
+++ packages/front/src/lib/components/Header/Header.svelte
@@ -128,6 +128,16 @@
             <div class="fr-header__menu-links" />
         </div>
     </div>
+
+    <div class="fr-header__menu status-proconnect-down" id="status-proconnect" aria-labelledby="header__menu__button">
+        <div class="fr-container">
+            <div>
+                ⚠️ ProConnect indisponible – La connexion à Data.Subvention via ProConnect est actuellement impossible.
+                Notre équipe technique est entièrement mobilisée pour rétablir le service dans les plus brefs délais.
+                Merci de votre compréhension.
+            </div>
+        </div>
+    </div>
 </header>
 
 <style>
@@ -147,5 +157,12 @@
     }
     .app-logo a {
         display: flex;
+    }
+
+    .status-proconnect-down {
+        padding: 16px 16px 16px 16px;
+        background-color: #fcbfbf;
+        color: #5e2a2b;
+        font-weight: 700;
     }
 </style>
