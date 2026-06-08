f244172123ebce94fffa98fcb0de69df18f9c2d5
Signo <6769414+sign0@users.noreply.github.com>
2026-06-08 16:08:40 +0200

    fix(api): #0000 reactivate index of osiris-requests and osiris-actions

--- packages/api/src/adapters/outputs/db/providers/osiris/osiris.request.adapter.ts
+++ packages/api/src/adapters/outputs/db/providers/osiris/osiris.request.adapter.ts
@@ -11,11 +11,11 @@ export class OsirisRequestAdapter extends MongoAdapter<OsirisRequestEntity> impl
     collectionName = "osiris-requests";
 
     async createIndexes() {
-        // TODO: reactivate this after MEPP/MEP
-        // await this.collection.createIndex({ "dossier.osirisId": 1, "dossier.exerciceBudgetaire": 1 }, { unique: true });
-        // await this.collection.createIndex({ "dossier.osirisId": 1 });
-        // await this.collection.createIndex({ "association.rna": 1 });
-        // await this.collection.createIndex({ "association.siret": 1 });
+        // TODO: desactivate/activate this before/after (pre-)production migration
+        await this.collection.createIndex({ "dossier.osirisId": 1, "dossier.exerciceBudgetaire": 1 }, { unique: true });
+        await this.collection.createIndex({ "dossier.osirisId": 1 });
+        await this.collection.createIndex({ "association.rna": 1 });
+        await this.collection.createIndex({ "association.siret": 1 });
     }
 
     joinIndexes = {
