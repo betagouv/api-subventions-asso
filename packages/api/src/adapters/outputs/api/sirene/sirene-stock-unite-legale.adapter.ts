a31d7be004f4af641267f873367b0795c171c02c
Maxime Silvère Meunier <mxmeunier@gmail.com>
2026-07-02 09:02:51 +0200

    fix(api): updates sirene stock unite legale url to use stable link

--- packages/api/src/adapters/outputs/api/sirene/sirene-stock-unite-legale.adapter.ts
+++ packages/api/src/adapters/outputs/api/sirene/sirene-stock-unite-legale.adapter.ts
@@ -4,7 +4,7 @@ import ProviderRequestFactory, {
 import { SireneStockUniteLegalePort } from "./sirene-stock-unite-legale.port";
 
 export class SireneStockUniteLegaleApiAdapter implements SireneStockUniteLegalePort {
-    private URL = "https://object.files.data.gouv.fr/data-pipeline-open/siren/stock/StockUniteLegale_utf8.zip";
+    private URL = "https://www.data.gouv.fr/api/1/datasets/r/825f4199-cadd-486c-ac46-a65a8ea1a047";
 
     private http: ProviderRequestService;
 
