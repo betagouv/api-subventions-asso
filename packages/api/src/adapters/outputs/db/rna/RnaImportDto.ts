e24bc69603c1d6ad5283dcf1ea17c5045310d9fd
Maxime Silvère Meunier <mxmeunier@gmail.com>
2026-07-06 13:44:17 +0200

    feat(api): create rna adapter part

---
+++ packages/api/src/adapters/outputs/db/rna/RnaImportDto.ts
@@ -0,0 +1,41 @@
+export interface RnaImportDto {
+    id: string;
+    id_ex: string;
+    siret: string;
+    rup_mi: string;
+    gestion: string;
+    date_creat: Date;
+    date_decla: Date;
+    date_publi: Date;
+    date_disso: Date;
+    nature: string;
+    groupement: string;
+    titre: string;
+    titre_court: string;
+    objet: string;
+    objet_social1: string;
+    objet_social2: string;
+    adrs_complement: string;
+    adrs_numvoie: string;
+    adrs_repetition: string;
+    adrs_typevoie: string;
+    adrs_libvoie: string;
+    adrs_distrib: string;
+    adrs_codeinsee: string;
+    adrs_codepostal: string;
+    adrs_libcommune: string;
+    adrg_declarant: string;
+    adrg_complemid: string;
+    adrg_complemgeo: string;
+    adrg_libvoie: string;
+    adrg_distrib: string;
+    adrg_codepostal: string;
+    adrg_achemine: string;
+    adrg_pays: string;
+    dir_civilite: string;
+    siteweb: string;
+    publiweb: string;
+    observation: string;
+    position: string;
+    maj_time: Date; // timestamp d'après la doc, à vérifier
+}
