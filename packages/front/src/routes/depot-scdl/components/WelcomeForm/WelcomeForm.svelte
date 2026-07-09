36e9fc19bae5d6b96d813b62a4e9a065a7136414
Maxime Silvère Meunier <mxmeunier@gmail.com>
2026-07-09 14:31:29 +0200

    feat(front): update scdl deposit welcome page

--- packages/front/src/routes/depot-scdl/components/WelcomeForm/WelcomeForm.svelte
+++ packages/front/src/routes/depot-scdl/components/WelcomeForm/WelcomeForm.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
     import TargetBlankLink from "$lib/components/TargetBlankLink.svelte";
+    import Alert from "$lib/dsfr/Alert.svelte";
 
     const dispatch = createEventDispatcher<{ start: void }>();
 </script>
 
 <div class="fr-col-12 fr-col-lg-9">
     <h1 class="text-center fr-mb-10v">Bienvenue sur le parcours de dépôt de données de Data.Subvention</h1>
+
     <div class="fr-text--bold fr-text--lead fr-mb-7v">Déposez vos données de subventions en toute simplicité</div>
     <div class="fr-text">
         <p class="fr-mb-7v">
@@ -17,20 +19,22 @@
             <li>Vérifier la conformité de vos fichiers avec le format SCDL</li>
             <li>Importer facilement vos données</li>
         </ul>
-        <p class="fr-mb-7v">
-            👉 Avant de commencer, vous pouvez vous reporter à la documentation officielle pour plus d'informations :
-            <TargetBlankLink href="https://schema.data.gouv.fr/scdl/subventions/">
-                voir la documentation
-            </TargetBlankLink>
-            .
-            <br />
-            👉 Vous pouvez également vous inscrire à un webinaire d'accompagnement au format SCDL :
-            <TargetBlankLink
-                href="https://datasubvention.beta.gouv.fr/permanence-scdl-creer-structurer-et-deposer-vos-donnees/">
-                s'inscrire
-            </TargetBlankLink>
-        </p>
     </div>
+
+    <Alert type="info">
+        Vous souhaitez en savoir plus sur le SCDL ou sur le dépôt de vos données ? Rejoignez notre webinaire
+        d'information et d'accompagnement pour découvrir le SCDL, comprendre les étapes de création et de dépôt de vos
+        données, et poser vos questions à notre équipe. <TargetBlankLink
+            href="https://datasubvention.beta.gouv.fr/permanence-scdl-creer-structurer-et-deposer-vos-donnees/">
+            S'inscrire.
+        </TargetBlankLink>
+        <br />
+        <br />
+        Vous pouvez également vous reporter à la <TargetBlankLink href="https://schema.data.gouv.fr/scdl/subventions/">
+            documentation officielle
+        </TargetBlankLink> pour plus d'informations.
+    </Alert>
+
     <div class="text-center">
         <button on:click={() => dispatch("start")} class="fr-btn" type="button">Débuter</button>
     </div>
